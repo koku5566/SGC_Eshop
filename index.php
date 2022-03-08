4fa364df542954febfe7d7615002c73b31620768
Maverick <maverickleeweilin88@gmail.com>
2022-03-08 16:20:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     }
 ?>
                 <!-- Begin Page Content -->
-                <div class="container-fluid">
+                <div class="container-fluid" style="width:80%">
 
                     <!-- Content Row - Slidebar and SlideShow -->
                     <div class="row">
@@ -62,45 +62,15 @@
                                     <div class="item-image-container">
                                     <img class="item-image" src="https://png.pngtree.com/background/20210714/original/pngtree-vibrant-green-red-and-yellow-low-poly-abstract-banner-background-picture-image_1238020.jpg" />
                                     </div>
-                                    <!-- Staggered Header Elements -->
-                                    <div class="item-header">
-                                    <span class="vertical-part"><b>DEMO 1</b></span>
-                                    </div>
-                                    <!-- Staggered Description Elements -->
-                                    <div class="item-description">
-                                    <span class="vertical-part">
-                                        <b>This is a demo description, This is a demo description, This is a demo description, This is a demo description</b>
-                                    </span>
-                                    </div>
                                 </div>
                                 <div class="item">
                                     <div class="item-image-container">
                                     <img class="item-image" src="https://png.pngtree.com/thumb_back/fw800/background/20201113/pngtree-abstract-triangle-shapes-design-banner-image_469727.jpg" />
                                     </div>
-                                    <!-- Staggered Header Elements -->
-                                    <div class="item-header">
-                                    <span class="vertical-part"><b>DEMO 2</b></span>
-                                    </div>
-                                    <!-- Staggered Description Elements -->
-                                    <div class="item-description">
-                                    <span class="vertical-part">
-                                        <b>This is a demo description, This is a demo description, This is a demo description, This is a demo description</b>
-                                    </span>
-                                    </div>
                                 </div>
                                 <div class="item">
                                     <div class="item-image-container">
                                     <img class="item-image" src="https://png.pngtree.com/background/20210714/original/pngtree-yellow-red-green-and-blue-color-low-poly-abstract-banner-design-picture-image_1238027.jpg" />
-                                    </div>
-                                    <!-- Staggered Header Elements -->
-                                    <div class="item-header">
-                                    <span class="vertical-part"><b>DEMO 3</b></span>
-                                    </div>
-                                    <!-- Staggered Description Elements -->
-                                    <div class="item-description">
-                                    <span class="vertical-part">
-                                        <b>This is a demo description, This is a demo description, This is a demo description, This is a demo description</b>
-                                    </span>
                                     </div>
                                 </div>
                                 </div>
@@ -586,69 +556,44 @@
     // Master DOManipulator v2 ------------------------------------------------------------
     const items = document.querySelectorAll('.item'),
     controls = document.querySelectorAll('.control'),
-    headerItems = document.querySelectorAll('.item-header'),
-    descriptionItems = document.querySelectorAll('.item-description'),
     activeDelay = .76,
     interval = 5000;
 
     let current = 0;
 
     const slider = {
-    init: () => {
-        controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
-        controls[current].classList.add('active');
-        items[current].classList.add('active');
-    },
-    nextSlide: () => { // Increment current slide and add active class
-        slider.reset();
-        if (current === items.length - 1) current = -1; // Check if current slide is last in array
-        current++;
-        controls[current].classList.add('active');
-        items[current].classList.add('active');
-        slider.transitionDelay(headerItems);
-        slider.transitionDelay(descriptionItems);
-    },
-    clickedControl: (e) => { // Add active class to clicked control and corresponding slide
-        slider.reset();
-        clearInterval(intervalF);
+        init: () => {
+            controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
+            controls[current].classList.add('active');
+            items[current].classList.add('active');
+        },
+        nextSlide: () => { // Increment current slide and add active class
+            slider.reset();
+            if (current === items.length - 1) current = -1; // Check if current slide is last in array
+            current++;
+            controls[current].classList.add('active');
+            items[current].classList.add('active');
+        },
+        clickedControl: (e) => { // Add active class to clicked control and corresponding slide
+            slider.reset();
+            clearInterval(intervalF);
 
-        const control = e.target,
-        dataIndex = Number(control.dataset.index);
+            const control = e.target,
+            dataIndex = Number(control.dataset.index);
 
-        control.classList.add('active');
-        items.forEach((item, index) => { 
-        if (index === dataIndex) { // Add active class to corresponding slide
-            item.classList.add('active');
-        }
-        })
-        current = dataIndex; // Update current slide
-        slider.transitionDelay(headerItems);
-        slider.transitionDelay(descriptionItems);
-        intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
-    },
-    reset: () => { // Remove active classes
-        items.forEach(item => item.classList.remove('active'));
-        controls.forEach(control => control.classList.remove('active'));
-    },
-    transitionDelay: (items) => { // Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
-        let seconds;
-        
-        items.forEach(item => {
-        const children = item.childNodes; // .vertical-part(s)
-        let count = 1,
-            delay;
-        
-        item.classList.value === 'item-header' ? seconds = .015 : seconds = .007;
-
-        children.forEach(child => { // iterate through .vertical-part(s) and style b element
-            if (child.classList) {
-            item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
-            child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
-            count++;
+            control.classList.add('active');
+            items.forEach((item, index) => { 
+            if (index === dataIndex) { // Add active class to corresponding slide
+                item.classList.add('active');
             }
-        })
-        })
-    },
+            })
+            current = dataIndex; // Update current slide
+            intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+        },
+        reset: () => { // Remove active classes
+            items.forEach(item => item.classList.remove('active'));
+            controls.forEach(control => control.classList.remove('active'));
+        }
     }
 
     let intervalF = setInterval(slider.nextSlide, interval);
