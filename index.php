3c53a63984d84ada7f8bd384a9199c321b1910bf
Maverick <maverickleeweilin88@gmail.com>
2022-03-09 16:40:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
                         
                         <!-- Slideshow -->
                         <div class="col-xl-10">
-                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <div id="carouselExampleIndicators" class="carousel slide mb-3" data-ride="carousel">
                             <ol class="carousel-indicators">
                                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -393,11 +393,8 @@
     }
 
     .browse-menus .browse-menu{
-        position: absolute;
-        top: 100%;
-        left: 0;
         width: 100%;
-        heigh:auto;
+        height:auto;
         z-index:0;
         background-color:#ffffff;
     }
