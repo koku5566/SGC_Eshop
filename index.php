d0d742edaa7c797f0fc80f9b81c6c7aa4c217bcd
Maverick <maverickleeweilin88@gmail.com>
2022-03-09 17:01:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -78,15 +78,15 @@
                                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                             </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                <img class="d-block w-100 img-fluid" src="https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=" alt="First slide">
+                            <div class="carousel-inner" style="height:20rem;">
+                                <div class="carousel-item active" style="height:inherit;">
+                                <img class="d-block w-100 img-div" src="https://media.istockphoto.com/photos/freedom-chains-that-transform-into-birds-charge-concept-picture-id1322104312?b=1&k=20&m=1322104312&s=170667a&w=0&h=VQyPkFkMKmo0e4ixjhiOLjiRs_ZiyKR_4SAsagQQdkk=" alt="First slide">
                                 </div>
-                                <div class="carousel-item">
-                                <img class="d-block w-100 img-fluid" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="Second slide">
+                                <div class="carousel-item" style="height:inherit;">
+                                <img class="d-block w-100 img-div" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="Second slide">
                                 </div>
-                                <div class="carousel-item">
-                                <img class="d-block w-100 img-fluid" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="Third slide">
+                                <div class="carousel-item" style="height:inherit;">
+                                <img class="d-block w-100 img-div" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="Third slide">
                                 </div>
                             </div>
                             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -456,4 +456,8 @@
     display: block;
     }
 
+    .img-div{
+        max-width:100%;
+        height:100%;
+    }
 </style>
