78b5752c15782d50366893c15d05e3fd0a30d2e6
Maverick <maverickleeweilin88@gmail.com>
2022-03-11 16:41:56 +0800

    Update classic.js

--- js/classic.js
+++ js/classic.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('load', function () {
-  document.getElementsByClassName("loader-wrapper")[0].style.animation = "fadeOut 0.4s";
+  document.getElementsByClassName("loader-wrapper")[0].style.animation = "fadeOut 1s";
   setTimeout(() => {
       document.getElementsByClassName("loader-wrapper")[0].style.display = "none";
   }, 1000);
