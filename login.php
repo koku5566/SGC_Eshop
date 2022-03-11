ede0f577496cc76ea65a66331d9846f6dacf1bad
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-11 23:27:29 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php require __DIR__ . '/header.php' ?>
 <!DOCTYPE html>
-<html lang="en">
-<!--
+<!--html lang="en">
+
 <head>
 
     <meta charset="utf-8">
@@ -12,13 +12,13 @@
 
     <title>SB Admin 2 - Login</title>
 
-    <!-- Custom fonts for this template--
+     Custom fonts for this template--
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template--
+     Custom styles for this template--
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
