dbe956d436ea43b545caa08b3cc8f3a097ff85c5
Maverick <maverickleeweilin88@gmail.com>
2022-03-11 15:38:26 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -44,6 +44,7 @@
 
     <script>
           /*An array containing all the product name list for search purpose:*/
+          <?php echo json_encode($productArray); ?>
             var json_data = <?php echo json_encode($productArray); ?>;
 
             var obj = JSON.parse(json_data);
