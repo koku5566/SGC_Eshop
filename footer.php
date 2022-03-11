828468ea4cf169c43828e62f118be6fdbe6337d9
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-12 01:16:09 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,10 +2,59 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
+            <footer class=""><div class="kingster-footer-wrapper "><div class="kingster-footer-container kingster-container clearfix"><div class="kingster-footer-column kingster-item-pdlr kingster-column-15"><div id="text-2" class="widget widget_text kingster-widget">			<div class="textwidget"><p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">About Us</span></p>
+<p><a href="https://colleges.segi.edu.my/penang/principal-greetings/">Principal’s Message</a><br>
+<a href="https://colleges.segi.edu.my/penang/governance/">Governance</a></p>
+<p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Our Vibrant Campuses</span></p>
+<p><a href="https://colleges.segi.edu.my/subangjaya/" target="_blank" rel="noopener">SEGi College Subang Jaya</a><br>
+<a href="https://colleges.segi.edu.my/kualalumpur/" target="_blank" rel="noopener">SEGi College Kuala Lumpur</a><br>
+<a href="https://colleges.segi.edu.my/penang/" target="_blank" rel="noopener">SEGi College Penang</a><br>
+<a href="https://colleges.segi.edu.my/sarawak/" target="_blank" rel="noopener">SEGi College Sarawak</a><br>
+<a href="https://segicollegekd.edu.my/">SEGi College Kota Damansara</a><br>
+<a href="https://university.segi.edu.my/" target="_blank" rel="noopener">SEGi University Kota Damansara</a></p>
+<p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Other Locations </span></p>
+<p><a href="https://www.segi.edu.my/institutions/segi-regional-centre-johor-bahru/" target="_blank" rel="noopener">SEGi University Regional Centre Johor Bahru</a></p>
+</div>
+		</div></div><div class="kingster-footer-column kingster-item-pdlr kingster-column-15"><div id="text-37" class="widget widget_text kingster-widget">			<div class="textwidget"><p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Fields of Interest</span></p>
+<p><a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=american-degree-program&amp;campus=segi-college-penang&amp;level-of-study=">American Degree Program</a><br>
+<a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=business-accounting&amp;campus=segi-college-penang&amp;level-of-study=">Business and Accounting</a><br>
+<a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=early-childhood-care-education&amp;campus=segi-college-penang&amp;level-of-study=">Early Childhood Care and Education</a><br>
+<a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=engineering&amp;campus=segi-college-penang&amp;level-of-study=">Engineering</a><br>
+<a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=hospitality-tourism&amp;campus=segi-college-penang&amp;level-of-study=">Hospitality and Tourism</a><br>
+<a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=information-technology&amp;campus=segi-college-penang&amp;level-of-study=">Information Technology</a><br>
+<a href="https://colleges.segi.edu.my/penang/course-search/?course-keywords=&amp;field-of-interest=psychology&amp;campus=segi-college-penang&amp;level-of-study=">Psychology</a></p>
+<p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Explore</span></p>
+<p><a href="https://kuula.co/share/collection/7lzw2?fs=1&amp;vr=1&amp;zoom=0&amp;thumbs=-1&amp;chromeless=1&amp;logo=1&amp;logosize=200">Virtual Tour</a><br>
+<a href="https://colleges.segi.edu.my/penang/ir-4-0/">IR 4.0</a><br>
+<a href="https://colleges.segi.edu.my/penang/news/">News</a><br>
+<a href="https://colleges.segi.edu.my/penang/events-pg/">Events</a><br>
+<a href="https://colleges.segi.edu.my/penang/industry/valued-corporate-partner/?name_directory_startswith=A">Valued Corporate Partners</a></p>
+</div>
+		</div></div><div class="kingster-footer-column kingster-item-pdlr kingster-column-15"><div id="text-38" class="widget widget_text kingster-widget">			<div class="textwidget"><p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Future Students</span></p>
+<p><a href="https://colleges.segi.edu.my/penang/courses-and-careers/">Programmes</a><br>
+<a href="https://colleges.segi.edu.my/penang/scholarships-and-financial-aids/">Scholarships &amp; Financial Aids</a><br>
+<a href="https://colleges.segi.edu.my/penang/local-students/">Local Students</a><br>
+<a href="https://colleges.segi.edu.my/penang/international-students/">International Students</a></p>
+<p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Staff and Students</span></p>
+<p><a href="https://colleges.segi.edu.my/penang/careers/">Careers at SEGi</a><br>
+<a href="https://segipenang.edu.my/scpg-library/">Library</a><br>
+<a href="https://segi.edu.my/jompay/index_payment.html" target="_blank" rel="noopener">SEGi Online Payment</a></p>
+</div>
+		</div></div><div class="kingster-footer-column kingster-item-pdlr kingster-column-15"><div id="text-39" class="widget widget_text kingster-widget">			<div class="textwidget"><p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Contacts and Directories</span></p>
+<p><i class="fa fa-phone" style="color: white;"></i> +604 263 3888<br>
+<i class="fa fa-phone" style="color: white;"></i> +6013 629 4880 (WhatsApp/Call)</p>
+<p>infopg@segi.edu.my</p>
+<p><span style="color: #a31f34; font-size: 16px; font-weight: 600;">Covid-19 / CMCO InfoDesk</span><br>
++013-629 4880<br>
+ohphaikcheng@segi.edu.my<br>
+scpgsao@segi.edu.my (Student Affairs)<br>
+infopg@segi.edu.my</p>
+</div>
+		</div></div></div></div><div class="kingster-copyright-wrapper"><div class="kingster-copyright-container kingster-container clearfix"><div class="kingster-copyright-left kingster-item-pdlr">© 2021 SEGi College Penang 198901010318 (187620-W). All Rights Reserved. Privacy Policy | Privacy Notice</div><div class="kingster-copyright-right kingster-item-pdlr"><div class="gdlr-core-social-network-item gdlr-core-item-pdb  gdlr-core-none-align" style="padding-bottom: 0px ;"><a href="https://www.facebook.com/SEGiCollegePenang/" target="_blank" class="gdlr-core-social-network-icon" title="facebook"><i class="fa fa-facebook"></i></a><a href="https://www.linkedin.com/school/segi-college-penang/" target="_blank" class="gdlr-core-social-network-icon" title="linkedin"><i class="fa fa-linkedin"></i></a><a href="https://www.youtube.com/channel/UCmsSUqFo6etShr_P690ilMw" target="_blank" class="gdlr-core-social-network-icon" title="youtube"><i class="fa fa-youtube"></i></a><a href="https://instagram.com/segicollegepenang/" target="_blank" class="gdlr-core-social-network-icon" title="instagram"><i class="fa fa-instagram"></i></a></div></div></div></div></footer>
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>&copy; 2021 SEGi College Penang 198901010318 (187620-W). All Rights Reserved. Privacy Policy | Privacy Notice</span>
                     </div>
                 </div>
             </footer>
