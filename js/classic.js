404b857784e9ccc726c6fc3d4b65944f6a46a5c0
Maverick <maverickleeweilin88@gmail.com>
2022-03-11 16:16:17 +0800

    1

--- js/classic.js
+++ js/classic.js
@@ -1,14 +1,13 @@
-$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
-$(window).on('load', function(){
-  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
-});
-function removeLoader(){
-    $( "#loadingDiv" ).fadeOut(500, function() {
-      // fadeOut complete. Remove the loading div
-      $( "#loadingDiv" ).remove(); //makes page more lightweight 
-  });  
+var myVar;
+
+function Loader() {
+  myVar = setTimeout(showPage, 3000);
 }
 
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
 
 function autocomplete(inp, arr) {
     /*the autocomplete function takes two arguments,
