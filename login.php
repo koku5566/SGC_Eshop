2be8b2412dec52b30d8c30f9a2974801a58a49b3
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-13 00:28:23 +0800

    Update login.php

--- login.php
+++ login.php
@@ -50,12 +50,12 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-lg-6 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-left">
                                         <h1 class="h4 text-gray-900 mb-4">Login</h1>
@@ -81,7 +81,7 @@
                                                 <label class="custom-control-label" for="customCheck">Remember Me</label>
                                             </div>
                                         </div>
-                                        
+
                                         <a href="index.html" class="btn btn-primary btn-user btn-block">Login</a>
 
                                         <div class="text-left">
