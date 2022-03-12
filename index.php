7ede29fac33d4c3ec27a1705c0f94a6b92262933
Maverick <maverickleeweilin88@gmail.com>
2022-03-12 09:24:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php
 
-    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         if(isset($_POST['restaurant']))
