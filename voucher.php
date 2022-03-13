f3bdd620177ae44d3d7be6e0b5421c799f471da2
easonying0723 <ey73210723@gmail.com>
2022-03-13 22:12:12 +0800

    voucher

--- voucher.php
+++ voucher.php
@@ -1,4 +1,7 @@
 
+<?php
+    require __DIR__ . '/header.php'
+?>
 
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -102,4 +105,9 @@
 
 </body>
 
+<?php
+    require __DIR__ . '/footer.php'
+?>
+
 </html>
+
