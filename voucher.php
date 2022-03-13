4c7519fc24b5acc8c01715a40ee09a0e62af537c
easonying0723 <ey73210723@gmail.com>
2022-03-14 04:55:35 +0800

    30

--- voucher.php
+++ voucher.php
@@ -1,78 +1,54 @@
 <link href="/css/voucher.css" rel="stylesheet" type="text/css">
 
-<div class="card" style="width: 20rem; hight: 10rem;">
-   <div class="row">
-     <div class="col-md-3">
-       <img src="../img/segilogo.png" class="img-fluid rounded-start p-1 voucherlogo">
-     </div>
-     <div class="col-md-7">
-       <div class="card-body m-1">
-         <h6 class="card-title"><strong>SEGi College Kuala Lumpur</strong></h6>
-         <h5 class="card-subtitle text-muted">RM10 off</h5>
-          <small>Validity : 30 SEP ~ 05 OCT</small><br>
-          <a class="" data-bs-toggle="modal" data-bs-target="#exampleModal">
-             <small>T&C applied.</small>
-          </a>
-       </div>
-     </div>
+<div class="card m-2" id="cardlogo">
+   <div class="container">
+      <img class="m-4" src="../img/segilogo.png" id="voucherlogo">
+   </div>
+   <div class="card-body">
+      <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
+      <h5 class="card-subtitle text-muted">RM1 off</h5>
+      <small>Used : 08 AUG ~ 18 AUG</small><br>
+      <u>
+         <a type="" class="" data-toggle="modal" data-target="#exampleModal">
+         T&C applied.
+         </a>
+      </u>
    </div>
 </div>
 
-
-    <!-- Modal -->
-   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-         <div class="modal-content">
-            <div class="modal-header">
-               <h5 class="modal-title" id="exampleModalLabel">Terms and Conditions.</h5>
-               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body d-flex justify-content-center">
-               <div class="card card-sm" style="max-width: 350px;">
-                  <div class="row g-0">
-                     <div class="col-md-3">
-                        <img src="../img/segilogo.png" class="img-fluid rounded-start p-1 voucherlogo">
-                     </div>
-                     <div class="col-md-7">
-                        <div class="card-body m-1">
-                           <h6 class="card-title"><strong>SEGi College Kuala Lumpur</strong></h6>
-                           <h5 class="card-subtitle text-muted">RM10 off</h5>
-                           <small>Validity : 30 SEP ~ 05 OCT</small><br>
-                           <a class="text-muted" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                              <small>T&C applied.</small>
-                           </a>
-                        </div>
-                     </div>
-                     <div class="col-md-2 buttonsec" style="background-color: brown;">
-                     </div>
-                  </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Terms and Conditions.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <div class="d-flex justify-content-center">
+            <div class="card m-2" id="cardlogo">
+               <div class="container">
+                  <img class="m-4" src="../img/apple.jpg" id="voucherlogo">
+               </div>
+               <div class="card-body">
+                  <h6 class="card-title"><strong>Apple Authorised Reseller</strong></h6>
+                  <h5 class="card-subtitle text-muted">22% off</h5>
+                  <small>Used : 01 FEB ~ 29 FEB</small><br>
                </div>
             </div>
-            <div class="tnccontainer">
-               <strong>Product</strong>
-               <p>All products</p>
-               <strong>More Details</strong>
-               <p>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-               </p>
-               <strong>Usage Period</strong>
-               <p>12/07/2021 15:00:24 - 31/12/2021 23:59:59</p>
-            </div>
-            <br>
          </div>
       </div>
-   </div>
-
-   
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-
+      <div class="tnccontainer">
+         <strong>Product</strong>
+         <p>All products</p>
+         <strong>More Details</strong>
+         <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         </p>
+         <strong>Usage Period</strong>
+         <p>12/07/2021 15:00:24 - 31/12/2021 23:59:59</p>
+      </div>
+    </div>
+  </div>
---
+++ voucherRun.php
@@ -0,0 +1,145 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+
+<link href="/css/voucher.css" rel="stylesheet" type="text/css">
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-3">
+               <div class="card m-2" id="cardlogo">
+                  <div class="container">
+                     <img class="m-4" src="../img/segilogo.png" id="voucherlogo">
+                  </div>
+                  <div class="card-body">
+                     <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
+                     <h5 class="card-subtitle text-muted">RM1 off</h5>
+                     <small>Used : 30 FEB 2022</small><br>
+                     <u>
+                        <a type="" class="" data-toggle="modal" data-target="#exampleModal">
+                        T&C applied.
+                        </a>
+                     </u>
+                     <hr/>
+                     <button type="button" class="btn btn-warning btn-sm" style="float: right" data-toggle="modal" data-target="#alert">CLAIM</button>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-3">
+               <div class="card m-2" id="cardlogo">
+                  <div class="container">
+                     <img class="m-4" src="../img/sony.png" id="voucherlogo">
+                  </div>
+                  <div class="card-body">
+                     <h6 class="card-title"><strong>SONY Malaysia</strong></h6>
+                     <h5 class="card-subtitle text-muted">RM188 off</h5>
+                     <small>Used : 08 AUG ~ 18 AUG 2022</small><br>
+                     <u>
+                        <a type="" class="" data-toggle="modal" data-target="#exampleModal">
+                        T&C applied.
+                        </a>
+                     </u>
+                     <hr/>
+                     <button type="button" class="btn btn-warning btn-sm" style="float: right" data-toggle="modal" data-target="#alert">CLAIM</button>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-3">
+               <div class="card m-2" id="cardlogo">
+                  <div class="container">
+                     <img class="m-4" src="../img/ttracing.png" id="voucherlogo">
+                  </div>
+                  <div class="card-body">
+                     <h6 class="card-title"><strong>SEGi College Kuala Lumpur</strong></h6>
+                     <h5 class="card-subtitle text-muted">RM10 off</h5>
+                     <small>Used : 30 SEP ~ 05 OCT</small><br>
+                     <u>
+                        <a type="" class="" data-toggle="modal" data-target="#exampleModal">
+                        T&C applied.
+                        </a>
+                     </u>
+                     <hr/>
+                     <button type="button" class="btn btn-warning btn-sm" style="float: right" data-toggle="modal" data-target="#alert">CLAIM</button>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-3">
+               <div class="card m-2" id="cardlogo">
+                  <div class="container">
+                     <img class="m-4" src="../img/apple.jpg" id="voucherlogo">
+                  </div>
+                  <div class="card-body">
+                     <h6 class="card-title"><strong>Apple Authorised Reseller</strong></h6>
+                     <h5 class="card-subtitle text-muted">22% off</h5>
+                     <small>Used : 01 FEB ~ 29 FEB</small><br>
+                     <u>
+                        <a type="" class="" data-toggle="modal" data-target="#exampleModal">
+                        T&C applied.
+                        </a>
+                     </u>
+                     <hr/>
+                     <button type="button" class="btn btn-warning btn-sm" style="float: right" data-toggle="modal" data-target="#alert">CLAIM</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      
+    </div>
+    <div class="carousel-item">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Terms and Conditions.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <div class="d-flex justify-content-center">
+            <div class="card m-2" id="cardlogo">
+               <div class="container">
+                  <img class="m-4" src="../img/apple.jpg" id="voucherlogo">
+               </div>
+               <div class="card-body">
+                  <h6 class="card-title"><strong>Apple Authorised Reseller</strong></h6>
+                  <h5 class="card-subtitle text-muted">22% off</h5>
+                  <small>Used : 01 FEB ~ 29 FEB</small><br>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="tnccontainer">
+         <strong>Product</strong>
+         <p>All products</p>
+         <strong>More Details</strong>
+         <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         </p>
+         <strong>Usage Period</strong>
+         <p>12/07/2021 15:00:24 - 31/12/2021 23:59:59</p>
+      </div>
+    </div>
+  </div>
+</div>
+
