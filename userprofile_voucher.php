f88f9ccaa250c12f44600924f0a91a1022f07b9e
easonying0723 <ey73210723@gmail.com>
2022-03-14 00:17:33 +0800

    6

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -5,14 +5,25 @@
 <br>
 <div class="container">
    <div class="row">
-         <?php
-         require __DIR__ . '/userprofilenav.php'
-         ?>
+      <?php
+      require __DIR__ . '/userprofilenav.php'
+      ?>
+      <br>
       <div class="col-xl-9">
-         <?php
-         require __DIR__ . '/voucher.php'
-         ?>
+         <div class="row g-0">
+            <div class="col-md-6">
+               <?php
+               require __DIR__ . '/voucher.php'
+               ?>
+            </div>
+            <div class="col-md-6">
+               <?php
+               require __DIR__ . '/voucher.php'
+               ?>
+            </div>
+         </div>
       </div>
+      <br>
    </div>
 </div>
 
