43c6b60fcaef20cc18a225f36b005be78337db92
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-14 01:32:53 +0800

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,100 @@
+<?php require __DIR__ . '/header.php' ?>
+
+<?php
+	if($_SESSION['isLogin'] == false)
+	{
+		echo "<script>alert('Logout to continue');
+			window.location.href='Login.php';</script>";
+    }
+?>
+<?php
+if(isset($_POST['update']))
+	{
+		$UID = $_SESSION['id'];
+		$name = $_POST['name'];
+		$password = md5($_POST['password']);
+		$contact = $_POST['contact'];
+		$address = $_POST['address'];
+
+		$sql_u = "SELECT * FROM user WHERE userID = '$UID'";
+
+		$stmt_u = mysqli_query($conn, $sql_u);
+
+		if (mysqli_num_rows($stmt_u) > 0) {	
+		
+			if($_POST['password'] != ""){
+				$sql = "UPDATE user SET name='$name', password='$password', contact='$contact', address='$address' WHERE userID='$UID'";
+			}
+			else{
+				$sql = "UPDATE user SET name='$name', contact='$contact', address='$address' WHERE userID='$UID'";
+			}
+			
+			if (mysqli_query($conn, $sql)) {
+				$_SESSION['Update'] = true;
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+		}
+		else
+		{
+			echo("<script>alert('Error');</script>");
+		}
+	}
+	
+	if(isset($_POST['toOrder']))
+	{
+		echo("<script>window.location.href='Account-Order.php';</script>");
+	}
+?>
+
+<div id="title"><h2>My Profile</h2></div>
+<div id="Account">
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" enctype="multipart/form-data">
+<?php
+	$UID = $_SESSION["id"];
+	$sql = "SELECT * FROM user WHERE userID = '$UID'";
+
+	$res_data = mysqli_query($conn,$sql);
+	if (mysqli_num_rows($res_data) > 0){
+		while($row = mysqli_fetch_array($res_data)){
+			echo("
+				<p id=\"label\">Name</p>
+				<input required type=\"text\" name=\"name\" maxlength=\"50\" value=\"".$row["name"]."\"/>
+				
+				<p id=\"label\">Email Address</p>
+				<input disabled type=\"email\" name=\"email\" maxlength=\"50\" placeholder=\"xxxxx@xxx.xxx\" value=\"".$row["email"]."\" style=\"border: 1px solid #1d1e1e; background-color: lightgray;\"/>
+				
+				<p id=\"label\">Password</p>
+				<input type=\"password\" name=\"password\" pattern=\".{8,}\" maxlength=\"50\" title=\"Must be at least 8 characters long\"/>
+				
+				<p id=\"label\">Contact</p>
+				<input required type=\"tel\" name=\"contact\" pattern=\"[0-9]{3}-[0-9]{7-8}\" maxlength=\"12\" placeholder=\"000-0000000\" value=\"".$row["contact"]."\"/>
+				
+				<p id=\"label\">Address</p>
+				<textarea required type=\"text\" name=\"address\" maxlength=\"999\">".$row["address"]."</textarea><br><br>
+				
+				<button name=\"toOrder\">My Order</button>
+				<button type=\"submit\" name=\"update\">Update</button>
+				");
+		}
+	}
+	/*<input required type=\"password\" name=\"password\" pattern=\".{8,}\" maxlength=\"50\" value=\"".$row["password"]."\"/>*/
+?>
+</form>
+<?php
+if(isset($_SESSION['Update']))
+	{
+		if($_SESSION['Update'] == true)
+		{
+			echo "<script>alert('Details Updated');</script>";
+		}
+		else if($_SESSION['Update'] == false && $_SESSION['Error'] != "")
+		{
+			echo "<script>alert('".$_SESSION['Error']."');</script>";
+		}
+		$_SESSION['Update'] = NULL;
+	}
+?>
+</div>
+
+<?php require __DIR__ . '/footer.php' ?>
