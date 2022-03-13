d64ab001f5dff8685d36782408270331149a8c9c
easonying0723 <ey73210723@gmail.com>
2022-03-14 00:37:15 +0800

    20

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -43,22 +43,22 @@
                   ?>
                </div>
             </div>
+            <br>
+            <nav aria-label="Page navigation example">
+               <ul class="pagination justify-content-end">
+               <li class="page-item disabled">
+                  <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+               </li>
+               <li class="page-item"><a class="page-link" href="#">1</a></li>
+               <li class="page-item"><a class="page-link" href="#">2</a></li>
+               <li class="page-item"><a class="page-link" href="#">3</a></li>
+               <li class="page-item">
+                  <a class="page-link" href="#">Next</a>
+               </li>
+               </ul>
+            </nav>
+            <br>
          </div>
-         <br>
-         <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-end">
-            <li class="page-item disabled">
-               <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-               <a class="page-link" href="#">Next</a>
-            </li>
-            </ul>
-         </nav>
-         <br>
       </div>
    </div>
 </div>
