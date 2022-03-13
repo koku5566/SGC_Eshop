4c06831546595e3c035ae32901c1cc17ed72e3fa
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-03-13 13:30:55 +0800

    Update facilityrental.php

--- facilityrental.php
+++ facilityrental.php
@@ -8,9 +8,9 @@
                     <!-- List All Product -->
                     <div class="row">
                         <!--Product List -->
-                        <div class="col-xl-12 col-lg-12">
+                        <div class="w3-display-middle">
                             <div class="card shadow mb-4">
-                                <div class="card-header py-3" class="w3-display-middle">
+                                <div class="card-header py-3">
                                     <h5 class="m-0 font-weight-bold text-primary ">CHOOSE A CAMPUS</h5>
                                 </div>
                                 <!-- Card Body -->
