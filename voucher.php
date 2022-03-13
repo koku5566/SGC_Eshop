9ff28e78d57987015e34462efe222c3cfb5c615c
easonying0723 <ey73210723@gmail.com>
2022-03-14 02:52:59 +0800

    29

--- voucher.php
+++ voucher.php
@@ -1,31 +1,25 @@
+<link href="/css/voucher.css" rel="stylesheet" type="text/css">
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    <link href="/css/voucher.css" rel="stylesheet" type="text/css">
-
-      <div class="card card-sm" style="max-width: 350px;">
-         <div class="row g-0">
-           <div class="col-md-3">
-             <img src="../img/segilogo.png" class="img-fluid rounded-start p-1 voucherlogo">
-           </div>
-           <div class="col-md-7">
-             <div class="card-body m-1">
-               <h6 class="card-title"><strong>SEGi College Kuala Lumpur</strong></h6>
-               <h5 class="card-subtitle text-muted">RM10 off</h5>
-                <small>Validity : 30 SEP ~ 05 OCT</small><br>
-                <a class="" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                   <small>T&C applied.</small>
-                </a>
-             </div>
-           </div>
-           <div class="col-md-2 buttonsec">
-            <a><button class="btn btn-outline-light bg-transparent btn-lg">CLAIM</button></a>
-           </div>
-         </div>
-      </div>
+<div class="card" style="width: 10rem; hight: 10rem;">
+   <div class="row">
+     <div class="col-md-3">
+       <img src="../img/segilogo.png" class="img-fluid rounded-start p-1 voucherlogo">
+     </div>
+     <div class="col-md-7">
+       <div class="card-body m-1">
+         <h6 class="card-title"><strong>SEGi College Kuala Lumpur</strong></h6>
+         <h5 class="card-subtitle text-muted">RM10 off</h5>
+          <small>Validity : 30 SEP ~ 05 OCT</small><br>
+          <a class="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+             <small>T&C applied.</small>
+          </a>
+       </div>
+     </div>
+     <div class="col-md-2 buttonsec">
+      <a><button class="btn btn-outline-light bg-transparent btn-lg">CLAIM</button></a>
+     </div>
+   </div>
+</div>
 
 
     <!-- Modal -->
