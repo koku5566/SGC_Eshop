c07fbe2afe26804502c240f7c4d4b49a71968dde
easonying0723 <ey73210723@gmail.com>
2022-03-14 00:11:44 +0800

    4

--- userprofilenav.php
+++ userprofilenav.php
@@ -4,10 +4,10 @@
    <div class="browse-menus">
       <div class="browse-menu active">
          <ul class="list-group" style="">
-            <li class="list-group-item"><a href="" class=""><img src="./img/myprofile.png" id="uprofileside" style="width: 50px; height: 50px;">My Profile</a></li>
-            <li class="list-group-item"><a href="" class=""><img src="./img/myaddressbook.png" id="uprofileside" style="width: 50px; height: 50px;">My Address Book</a></li>
-            <li class="list-group-item"><a href="" class=""><img src="./img/paymentoption.png" id="uprofileside" style="width: 50px; height: 50px;">My Payment Option</a></li>
-            <li class="list-group-item"><a href="" class=""><img src="./img/myvoucher.png" id="uprofileside" style="width: 50px; height: 50px;">My Voucher</a></li>
+            <li class="list-group-item"><a href="" class="list-group-item list-group-item-action"><img src="./img/myprofile.png" id="uprofileside" style="width: 50px; height: 50px;">My Profile</a></li>
+            <li class="list-group-item"><a href="" class="list-group-item list-group-item-action"><img src="./img/myaddressbook.png" id="uprofileside" style="width: 50px; height: 50px;">My Address Book</a></li>
+            <li class="list-group-item"><a href="" class="list-group-item list-group-item-action"><img src="./img/paymentoption.png" id="uprofileside" style="width: 50px; height: 50px;">My Payment Option</a></li>
+            <li class="list-group-item"><a href="" class="list-group-item list-group-item-action"><img src="./img/myvoucher.png" id="uprofileside" style="width: 50px; height: 50px;">My Voucher</a></li>
          </ul>
       </div>
    </div>
