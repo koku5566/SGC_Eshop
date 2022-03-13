9aa20fa3b5aa8370f91f2a957d29b6b33400bd1c
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-03-13 15:42:35 +0800

    Update facilityrental.php

--- facilityrental.php
+++ facilityrental.php
@@ -14,7 +14,7 @@
                                     <h5 class="w3-display-middle">CHOOSE A CAMPUS</h5>
                                 </div>
                                 <!-- Card Body -->
-                                <div class="card-body">
+                                
                                     <div class="row">
                                         <div class="col-xl-3 col-lg-4 col-sm-6" style="padding-bottom: .625rem;">
                                             <a data-sqe="link" href="product.php?id=a">
@@ -22,11 +22,11 @@
                                                     <div class="image-container">
                                                         <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
                                                     </div>
-                                                    <div class="card-body">
+                                                    
                                                         <div class="Name">
                                                             <p class="card-text campus-name">SUBANG JAYA</p>
                                                         </div>            
-                                                    </div>
+                                                    
                                                 </div>   
                                             </a>
                                         </div>
@@ -73,7 +73,7 @@
                                             </a>
                                         </div>
                                     </div>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -122,14 +122,7 @@
         overflow:hidden;
         text-align: center;
     }
-    .name{
-        background-color: black;
-
-    }
     .card-body{
        background-color: #A31F37;
     }
-
-
-
 </style>
