f8d769c5709c6b40dcbfc3904c8f29e0d6f12164
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-13 17:42:47 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,5 @@
 <?php require __DIR__ . '/header.php' ?>
 <?php
-	if(!isset($_SESSION)){
-		session_start();
-	}
-
 	if (isset($_SESSION['login']) && $_SESSION['login']){
 		?><script>window.location.href = window.location.origin + "/Main.php/";</script><?php
         header('location: Main.php');
