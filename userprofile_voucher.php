25fa6e1ab64a6e87f8b1b0e8a5013038cc3f5239
easonying0723 <ey73210723@gmail.com>
2022-03-13 23:58:06 +0800

    update

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -1,12 +1,21 @@
 <?php
-    require_once __DIR__ . '/mysqli_connect.php'
+    require_once __DIR__ . '/header.php'
 ?>
 
 <?php
     require __DIR__ . '/userprofilenav.php'
 ?>
 
-
+<br>
+<div class="container">
+   <div class="row">
+   <?php
+    require __DIR__ . '/userprofilenav.php'
+   ?>
+      <div class="col-xl-9">
+      </div>
+   </div>
+</div>
 
 <?php
     require __DIR__ . '/footer.php'
--- userprofilenav.php
+++ userprofilenav.php
@@ -1,20 +1,14 @@
 
 <br>
-<div class="container">
-   <div class="row">
-      <div class="col-xl-3 col-lg-3 col-0">
-         <div class="browse-menus">
-            <div class="browse-menu active">
-               <ul class="list-group" style="">
-                  <li class="list-group-item"><a href="" class=""><img src="./img/myprofile.png" id="uprofileside" style="width: 50px; height: 50px;">My Profile</a></li>
-                  <li class="list-group-item"><a href="" class=""><img src="./img/myaddressbook.png" id="uprofileside" style="width: 50px; height: 50px;">My Address Book</a></li>
-                  <li class="list-group-item"><a href="" class=""><img src="./img/paymentoption.png" id="uprofileside" style="width: 50px; height: 50px;">My Payment Option</a></li>
-                  <li class="list-group-item"><a href="" class=""><img src="./img/myvoucher.png" id="uprofileside" style="width: 50px; height: 50px;">My Voucher</a></li>
-               </ul>
-            </div>
-         </div>
-      </div>
-      <div class="col-xl-9">
+<div class="col-xl-3 col-lg-3 col-0">
+   <div class="browse-menus">
+      <div class="browse-menu active">
+         <ul class="list-group" style="">
+            <li class="list-group-item"><a href="" class=""><img src="./img/myprofile.png" id="uprofileside" style="width: 50px; height: 50px;">My Profile</a></li>
+            <li class="list-group-item"><a href="" class=""><img src="./img/myaddressbook.png" id="uprofileside" style="width: 50px; height: 50px;">My Address Book</a></li>
+            <li class="list-group-item"><a href="" class=""><img src="./img/paymentoption.png" id="uprofileside" style="width: 50px; height: 50px;">My Payment Option</a></li>
+            <li class="list-group-item"><a href="" class=""><img src="./img/myvoucher.png" id="uprofileside" style="width: 50px; height: 50px;">My Voucher</a></li>
+         </ul>
       </div>
    </div>
 </div>
