0f1a60ba080e23678519b24e1b03bfa52a96e8ae
easonying0723 <ey73210723@gmail.com>
2022-03-15 04:46:33 +0800

    test7

--- voucher2.php
+++ voucher2.php
@@ -16,11 +16,49 @@
             <h5 class="card-subtitle text-muted">RM1 off</h5>
             <small>Used : 30 FEB 2022</small><br>
             <u>
-               <a type="" class="" data-toggle="modal" data-target="#termsModal">
+               <a type="" class="" data-toggle="modal" data-target="#termsv2Modal">
                T&C applied.
                </a>
             </u>
          </div>
       </div>
    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="termsv2Modal" tabindex="-1" role="dialog" aria-labelledby="termsv2ModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="termsv2ModalLongTitle">Terms and Conditions.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <div class="d-flex justify-content-center">
+            <div class="card m-2" id="termsvouchercard">
+               <div class="container">
+                  <img class="m-4" src="../img/apple.jpg" id="voucherlogo">
+               </div>
+               <div class="card-body">
+                  <h6 class="card-title"><strong>Apple Authorised Reseller</strong></h6>
+                  <h5 class="card-subtitle text-muted">22% off</h5>
+                  <small>Used : 01 FEB ~ 29 FEB</small><br>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="tnccontainer">
+         <strong>Product</strong>
+         <p>All products</p>
+         <strong>More Details</strong>
+         <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         </p>
+         <strong>Usage Period</strong>
+         <p>12/07/2021 15:00:24 - 31/12/2021 23:59:59</p>
+      </div>
+    </div>
+  </div>
 </div>
