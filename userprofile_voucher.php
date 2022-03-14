2cd1d4a3397ab0b86c0abfbbbb5732a041a0a966
easonying0723 <ey73210723@gmail.com>
2022-03-15 04:54:00 +0800

    test11

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -16,28 +16,13 @@
       <br>
       <div class="col-xl-9">
          <div class="container p-4" style="background-color: #ffffff">
-            <!-- <div class="card" id="vouchercard2">
-               <div class="card-body">
-                  <div class="row">
-                     <div class="col-mb-3 m-2">
-                        <img class="m-2" src="../img/segilogo.png" id="voucherlogo">
-                     </div>
-                     <div class="col-mb-7 m-2">
-                        <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
-                        <h5 class="card-subtitle text-muted">RM1 off</h5>
-                        <small>Used : 30 FEB 2022</small><br>
-                        <u>
-                           <a type="" class="" data-toggle="modal" data-target="#termsModal">
-                           T&C applied.
-                           </a>
-                        </u>
-                     </div>
-                  </div>
+            <div class="row row-cols-2">
+               <div class="col-6">
+                  <?php
+                  require __DIR__ . '/voucher2.php'
+                  ?>
                </div>
-            </div> -->
-            <?php
-            require __DIR__ . '/voucher2.php'
-            ?>
+            </div>
          </div>
          <br>
          <div class="container">
