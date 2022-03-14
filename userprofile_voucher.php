78d21b63703fbe3ea678947b8152eef0903928f6
easonying0723 <ey73210723@gmail.com>
2022-03-15 04:51:09 +0800

    test10

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -2,6 +2,11 @@
     require_once __DIR__ . '/header.php'
 ?>
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+
 <br>
 <div class="container">
    <div class="row">
@@ -59,3 +64,54 @@
 <?php
     require __DIR__ . '/footer.php'
 ?>
+
+<style>
+   #voucherlogo{
+   height: 100px !important;
+   width: 100px !important;
+}
+
+#vouchercard{
+   width: 11.5rem;
+   height: 22rem;
+}
+
+#termsvouchercard{
+   width: 11.5rem;
+   height: 20rem;
+}
+
+.tnccontainer{
+   margin: 10px 30px 30px 30px;
+   padding: 15px 18px 15px 18px;
+   border-radius: 10px;
+   border: dashed;
+}
+
+.selectvoucher{
+   width: 40px;
+   height: 28px;
+}
+
+#vouchercard2{
+   width: 25rem;
+   height: 10.5rem;
+}
+
+/* -------------------- Category Scrollbar----------------------- */
+
+/* width */
+::-webkit-scrollbar {
+   width: 5px;
+   height: 5px;
+ }
+
+ .scrolling-wrapper{
+	overflow-x: auto;
+}
+
+ .scrolling-wrapper2{
+	overflow-y: auto;
+   max-height: 580px;
+}
+</style>
