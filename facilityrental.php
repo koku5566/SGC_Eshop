5bad2e5b9eb2f0f9e5b212a8fa2155cac17fbd97
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-03-14 10:24:31 +0800

    Update facilityrental.php

--- facilityrental.php
+++ facilityrental.php
@@ -7,8 +7,8 @@
                 <div class="container-fluid" style="width:80%">
                     <!-- List All Product -->
                     <div class="card-body">
-                                <div class="w3-display-middle">
-                                    <h5 >CHOOSE A CAMPUS</h5>
+                                <div>
+                                    <h5 style = "text-align:center">CHOOSE A CAMPUS</h5>
                                 </div>
                     <div class="row">
                                 <!-- Card Body -->
@@ -76,7 +76,7 @@
                         </div>
                     <br>
                     <div class="card-header py-3">
-                                    <h5 class="w3-display-middle">EXPLORE SEGI FACILITIES</h5>
+                                    <h5>EXPLORE SEGI FACILITIES</h5>
                                 </div>
                     <!-- Slideshow -->
                     <div class="w3-display-middle" style="width:100%">
