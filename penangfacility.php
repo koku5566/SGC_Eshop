6c3fc668d053a797cf5459e23de4a3f700f37228
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-03-14 11:35:08 +0800

    Update penangfacility.php

--- penangfacility.php
+++ penangfacility.php
@@ -6,77 +6,9 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid" style="width:80%">
                     <!-- List All Product -->
-                    <div class="card-body">
-                                <div>
-                                    <h5 style ="text-align:center">CHOOSE A CAMPUS</h5>
-                                </div>
-                    <div class="row">
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-xl-3 col-lg-4 col-sm-6" style="padding-bottom: .625rem;">
-                                            <a data-sqe="link" href="product.php?id=a">
-                                                <div class="card">
-                                                    <div class="image-container">
-                                                        <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
-                                                    </div>
-                                                    <div class="card-body-text">
-                                                        <div class="Name">
-                                                            <p class="card-text campus-name">SUBANG JAYA</p>
-                                                        </div>            
-                                                    </div>
-                                                </div>   
-                                            </a>
-                                        </div>
-                                        <div class="col-xl-3 col-lg-4 col-sm-6" style="padding-bottom: .625rem;">
-                                            <a data-sqe="link" href="#">
-                                                <div class="card">
-                                                    <div class="image-container">
-                                                        <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
-                                                    </div>
-                                                    <div class="card-body-text">
-                                                        <div class="Name">
-                                                            <p class="card-text campus-name">PENANG</p>
-                                                        </div>                                                       
-                                                    </div>
-                                                </div>   
-                                            </a>
-                                        </div>
-                                        <div class="col-xl-3 col-lg-4 col-sm-6" style="padding-bottom: .625rem;">
-                                            <a data-sqe="link" href="#">
-                                                <div class="card">
-                                                    <div class="image-container">
-                                                        <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
-                                                    </div>
-                                                    <div class="card-body-text">
-                                                        <div class="Name">
-                                                            <p class="card-text campus-name">SARAWAK</p>
-                                                        </div>                                                                                                              
-                                                    </div>
-                                                </div>   
-                                            </a>
-                                        </div>
-                                        <div class="col-xl-3 col-lg-4 col-sm-6" style="padding-bottom: .625rem;">
-                                            <a data-sqe="link" href="#">
-                                                <div class="card">
-                                                    <div class="image-container">
-                                                        <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
-                                                    </div>
-                                                    <div class="card-body-text">
-                                                        <div class="Name">
-                                                            <p class="card-text campus-name">KUALA LUMPUR</p>
-                                                        </div>                                                                                                          
-                                                    </div>
-                                                </div>   
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>  
-                            </div>
-                        </div>
-                    <br>
+                    
                     <div class="card-header py-3">
-                                    <h5>EXPLORE SEGI FACILITIES</h5>
+                                    <h5>SEGI PENANG FACILITIES</h5>
                                 </div>
                     <!-- Slideshow -->
                     <div class="w3-display-middle" style="width:100%">
@@ -114,7 +46,9 @@
 
                    
                     <br>
-
+                    <div class="card-header py-3">
+                                    <h5>BOOK YOUR FACILITIES</h5>
+                                </div>
                     <!-- List All Product -->
                     <div class="row">
                         <!--Product List -->
