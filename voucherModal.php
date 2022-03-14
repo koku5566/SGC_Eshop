ae588fd005c16c58d0f588a2d2f9e91011360a1b
easonying0723 <ey73210723@gmail.com>
2022-03-15 04:24:03 +0800

    update voucherModal

--- voucherModal.php
+++ voucherModal.php
@@ -142,9 +142,9 @@
                            <div class="col-mb-7 m-2">
                               <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
                               <h5 class="card-subtitle text-muted">RM1 off</h5>
-                              <small>Used : 30 FEB 2022</small><br>
+                              <p>Used : 30 FEB 2022</p><br>
                               <u>
-                                 <a type="" class="" data-toggle="modal" data-target="#termsModal">
+                                 <a type="" class="" data-toggle="modal" data-target="#termsModal2">
                                  T&C applied.
                                  </a>
                               </u>
@@ -159,6 +159,44 @@
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-warning">Apply</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="termsModal2" tabindex="-1" role="dialog" aria-labelledby="termsModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="termsModalLongTitle">Terms and Conditions.</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <div class="d-flex justify-content-center">
+            <div class="card m-2" id="termsvouchercard">
+               <div class="container">
+                  <img class="m-4" src="../img/apple.jpg" id="voucherlogo">
+               </div>
+               <div class="card-body">
+                  <h6 class="card-title"><strong>Apple Authorised Reseller</strong></h6>
+                  <h5 class="card-subtitle text-muted">22% off</h5>
+                  <small>Used : 01 FEB ~ 29 FEB</small><br>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="tnccontainer">
+         <strong>Product</strong>
+         <p>All products</p>
+         <strong>More Details</strong>
+         <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         </p>
+         <strong>Usage Period</strong>
+         <p>12/07/2021 15:00:24 - 31/12/2021 23:59:59</p>
       </div>
     </div>
   </div>
