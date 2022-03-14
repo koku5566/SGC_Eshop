8c146e28166977d886da1d6b2de56fd9eeae9e58
Maverick <maverickleeweilin88@gmail.com>
2022-03-14 21:22:37 +0800

    a

---
+++ blank.php
@@ -0,0 +1,19 @@
+<?php
+    require __DIR__ . '/header.php'
+?>
+
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid" style="width:80%">
+
+
+                </div>
+                <!-- /.container-fluid -->
+
+<?php
+    require __DIR__ . '/footer.php'
+?>
+
+<style>
+
+</style>
