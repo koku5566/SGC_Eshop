c3dc6ec817743d8d95c569650e0ef78d53d6ff01
easonying0723 <ey73210723@gmail.com>
2022-03-15 04:34:55 +0800

    view voucher user profile test 1

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__ . '/header.php'
 ?>
+<link href="/css/voucher.css" rel="stylesheet" type="text/css">
 
 <br>
 <div class="container">
@@ -10,7 +11,27 @@
       ?>
       <br>
       <div class="col-xl-9">
-         
+         <div class="container">
+            <div class="card" id="vouchercard2">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-mb-3 m-2">
+                        <img class="m-2" src="../img/segilogo.png" id="voucherlogo">
+                     </div>
+                     <div class="col-mb-7 m-2">
+                        <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
+                        <h5 class="card-subtitle text-muted">RM1 off</h5>
+                        <small>Used : 30 FEB 2022</small><br>
+                        <u>
+                           <a type="" class="" data-toggle="modal" data-target="#termsModal">
+                           T&C applied.
+                           </a>
+                        </u>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
          <br>
          <div class="container">
             <nav aria-label="Page navigation example">
