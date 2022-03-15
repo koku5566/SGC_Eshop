8d05536bd5dfd4274aeef52b35a4d45a5f028743
Maverick <maverickleeweilin88@gmail.com>
2022-03-15 11:12:26 +0800

    a

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -48,10 +48,13 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-xl-2 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
+                                        <div class="col-xl-10 col-lg-8 col-sm-4" style="padding-bottom: .625rem;">
                                             <button type="button" class="btn btn-primary">Search</button>
                                         </div>
-                                        <div class="col-xl-2 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
+                                        <div class="col-xl-1 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
+                                            <button type="button" class="btn btn-primary">Search</button>
+                                        </div>
+                                        <div class="col-xl-1 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
                                             <button type="button" class="btn btn-outline-dark">Reset</button>
                                         </div>
                                         
@@ -77,21 +80,45 @@
                                             <nav id="myTab" class="nav nav-tabs" role="tablist">
                                                 <a class="nav-item nav-link active" id="nav-all-tab" data-toggle="tab" href="#nav-all" role="tab" aria-controls="nav-all" aria-selected="true">All</a>
                                                 <a class="nav-item nav-link" id="nav-published-tab" data-toggle="tab" href="#nav-published" role="tab" aria-controls="nav-published" aria-selected="false">Published</a>
-                                                <a class="nav-item nav-link" id="nav-sold-tab" data-toggle="tab" href="#nav-sold" role="tab" aria-controls="nav-sold" aria-selected="false">Sold</a>
-                                                <a class="nav-item nav-link" id="nav-violation-tab" data-toggle="tab" href="#nav-violation" role="tab" aria-controls="nav-violation" aria-selected="false">Violation</a>
-                                                <a class="nav-item nav-link" id="nav-unpublish-tab" data-toggle="tab" href="#nav-unpublish" role="tab" aria-controls="nav-unpublish" aria-selected="false">Haven't Publish</a>
+                                                <a class="nav-item nav-link" id="nav-sold-tab" data-toggle="tab" href="#nav-sold" role="tab" aria-controls="nav-sold" aria-selected="false">Out of Stock</a>
+                                                <a class="nav-item nav-link" id="nav-violation-tab" data-toggle="tab" href="#nav-violation" role="tab" aria-controls="nav-violation" aria-selected="false">Banned</a>
+                                                <a class="nav-item nav-link" id="nav-unpublish-tab" data-toggle="tab" href="#nav-unpublish" role="tab" aria-controls="nav-unpublish" aria-selected="false">Unpublished</a>
                                             </nav>
 
                                             <div class="tab-content" id="nav-tabContent">
-                                                <div class="tab-pane active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                                                 <!-- All Product -->
+                                                <div class="tab-pane active" id="nav-all" role="tabpanel" aria-labelledby="nav-all-tab">
+                                                    <!-- Header Bar -->
+                                                    <div class="row">
+                                                        <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
+                                                            <div>
+                                                                <h5>10 Products
+                                                            </div>
+                                                            <div class="progress">
+                                                                <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                                            </div>
+                                                            <p data-bs-toggle="tooltip" data-bs-placement="bottom" title="Number of upload product available = 1000 - Number of current product">You can still upload 990 products</p>
+                                                        </div>
+                                                    </div>
                                                 </div>
 
-                                                <div class="tab-pane" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                                 <!-- Published -->
+                                                <div class="tab-pane" id="nav-published" role="tabpanel" aria-labelledby="nav-published-tab">
                                                     type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
                                                 </div>
 
-                                                <div class="tab-pane" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                                                 <!-- Out of Stock -->
+                                                <div class="tab-pane" id="nav-sold" role="tabpanel" aria-labelledby="nav-sold-tab">
+                                                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                                                </div>
+
+                                                 <!-- Banned -->
+                                                <div class="tab-pane" id="nav-violation" role="tabpanel" aria-labelledby="nav-violation-tab">
+                                                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                                                </div>
+
+                                                 <!-- Unpublished -->
+                                                <div class="tab-pane" id="nav-unpublish" role="tabpanel" aria-labelledby="nav-unpublish-tab">
                                                     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
                                                 </div>
                                             </div>
@@ -102,6 +129,20 @@
                         </div>
                     </div>
 
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-end">
+                            <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+
 </div>
 <!-- /.container-fluid -->
 
@@ -110,6 +151,11 @@
 ?>
 
 <style>
+
+    .nav-link{
+
+    }
+
     .tab-pane.active {
     animation: slide-down 0.5s ease-out;
     }
