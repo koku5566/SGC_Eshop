dc05d414030216637c8a04967a2a9bcdfec91dbd
Maverick <maverickleeweilin88@gmail.com>
2022-03-15 15:13:00 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -239,7 +239,7 @@
 
                                                                         //Fetch each product information
                                                                         $id = $row['product_id'];
-                                                                        $sql_1 = "SELECT DISTINCT A.product_id, A.product_name,A.product_cover_picture,A.product_variation,A.product_price,A.product_stock,A.product_sold,A.product_status,
+                                                                        $sql_1 = "SELECT DISTINCT A.product_id, A.product_name,A.product_cover_picture,A.product_variation,A.product_price,A.product_stock,A.product_sold,
                                                                         C.max_price,D.min_price,E.total_sold,F.total_stock FROM `product` AS A 
                                                                         LEFT JOIN variation AS B ON A.product_id = B.product_id 
                                                                         LEFT JOIN (SELECT product_id,product_price AS max_price FROM `variation` WHERE product_id = '$id' ORDER BY product_price DESC LIMIT 1) AS C ON A.product_id = C.product_id 
@@ -273,23 +273,17 @@
 
                                                                                     echo("
                                                                                                     </div>
-                                                                                                    <div class=\"row\">
-                                                                                                        <div class=\"col\">
-                                                                                                            <p>Stock ".$row_1['total_stock']."</p>
-                                                                                                        </div>
-                                                                                                        <div class=\"col\">
-                                                                                                            <p>Sold ".$row_1['total_sold']."</p>
-                                                                                                        </div>
+                                                                                                    <div class=\"Rating\">
+                                                                                                        <i class=\"fa fa-star\"></i>
+                                                                                                        <i class=\"fa fa-star\"></i>
+                                                                                                        <i class=\"fa fa-star-half-alt\"></i>
+                                                                                                        <i class=\"fa fa-star\" style=\"font-weight:normal;\"></i>
+                                                                                                        <i class=\"fa fa-star\" style=\"font-weight:normal;\"></i>
                                                                                                     </div>
-                                                                                    ");
-                                                                                    echo("
-                                                                                                    <div class=\"row\">
-                                                                                                        <div class=\"col-xl-12\">
-                                                                                                            <button value=".$row_1['product_id'].">Edit</button>
-                                                                                                        </div>
-                                                                                                        <div class=\"col-xl-12\">
-                                                                                                            <button value=".$row_1['product_id'].">Unpublish</button>
-                                                                                                        </div>
+                                                                                                    <div class=\"Location\">
+                                                                                                    <span style=\"font-size: 10pt; color:grey;\" >Subang Jaya</span>
+                                                                                                    </div>
+                                                                                                        
                                                                                                     </div>
                                                                                                 </div>   
                                                                                             </a>
