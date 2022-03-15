5ac86796890521f8402dd79e7a8aca8e2218f565
Maverick <maverickleeweilin88@gmail.com>
2022-03-15 17:12:15 +0800

    Update header.php

--- header.php
+++ header.php
@@ -30,11 +30,9 @@
             $searchTerm = test_input($_POST["search"]);
             // check if name only contains letters and whitespace
             if (preg_match("/^[a-zA-Z-' ]*$/",$searchTerm)) {
-                echo($searchTerm);
                 ?>
                     <script type="text/javascript">
-                    console.log(echo($searchTerm));
-                    //window.location.href = window.location.origin + "/search.php?search=<?php echo($searchTerm)?>";
+                        window.location.href = window.location.origin + "/search.php?search=<?php echo($searchTerm)?>";
                     </script>
                 <?php
 
