d44890d472e4a4e75310542f7b9fcf7b44b9620f
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-16 10:34:14 +0800

    Update PurchaseHistory.php

--- PurchaseHistory.php
+++ PurchaseHistory.php
@@ -9,8 +9,9 @@
     <div style="padding-left: 59px;padding-top: 40px;">
         <div><button class="btn btn-primary" type="button" style="width: 89.5px;padding-left: 6px;margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;border-color: var(--bs-body-bg);text-decoration: underline;"><i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);background: rgba(255,255,255,0);"></i>Back</button><br /><br /></div>
     </div>
-    <div style="height: 249px;width: 1117.2px;margin-left: 100px;box-shadow: 4px 4px rgba(108,117,125,0.3);border: 1.5px solid var(--bs-gray-600);"><img style="height: 86px;padding-top: 0px;margin-left: 11px;margin-top: 0px;width: 431.1625px;" src="segi kl.png" />
-        <div style="height: 159px;"><img src="iphone-grey.png" style="height: 102px;margin-left: 19px;" />
+    <div style="height: 249px;width: 1117.2px;margin-left: 100px;box-shadow: 4px 4px rgba(108,117,125,0.3);border: 1.5px solid var(--bs-gray-600);">
+    <img style="height: 86px;padding-top: 0px;margin-left: 11px;margin-top: 0px;width: 431.1625px;" src="img/segi kl.png" />
+        <div style="height: 159px;"><img src="/img/product/iphone-grey.png" style="height: 102px;margin-left: 19px;" />
             <p style="width: 316px;margin-left: 124px;margin-top: -78px;font-size: 25px;">Iphone 10 Pro Max</p>
             <p style="width: 66px;font-size: 25px;margin-left: 474px;margin-top: -56px;">Grey</p>
             <p style="font-size: 25px;width: 56px;margin-left: 631px;margin-top: -54px;">x1</p>
