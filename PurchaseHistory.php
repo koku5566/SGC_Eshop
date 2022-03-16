f4361ddd90d599862f4d1067f203806d9f00159a
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-16 14:21:54 +0800

    Update PurchaseHistory.php

--- PurchaseHistory.php
+++ PurchaseHistory.php
@@ -4,25 +4,43 @@
 
 
                 <!-- Begin Page Content -->
-                
-    <h1 style="width: 315px;margin-left: 561px;color: #c71526;">Purchase History</h1>
+                <h1 style="width: 315px;margin-left: 561px;color: #c71526;">Purchase History</h1>
     <div style="padding-left: 59px;padding-top: 40px;">
-        <div><button class="btn btn-primary" type="button" style="width: 89.5px;padding-left: 6px;margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;border-color: var(--bs-body-bg);text-decoration: underline;"><i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);background: rgba(255,255,255,0);"></i>Back</button><br /><br /><br /></div>
+        <div>
+            <button class="btn btn-primary" type="button" style="width: 89.5px;padding-left: 6px;
+            margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;
+            border-color: var(--bs-body-bg);text-decoration: underline;">
+            <i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);
+            background: rgba(255,255,255,0);">
+            </i>Back</button><br /><br /><br /></div>
     </div>
-    <div style="height: 249px;width: 1117.2px;margin-left: 135px;border: 1.5px solid var(--bs-gray-600);margin-top: 13px;"><img style="height: 86px;padding-top: 0px;margin-left: 11px;margin-top: 0px;width: 431.1625px;" src="segi kl.png" />
-        <div style="height: 159px;"><img src="iphone-grey.png" style="height: 102px;margin-left: 19px;" />
-            <p style="width: 316px;margin-left: 124px;margin-top: -78px;font-size: 25px;">Iphone 10 Pro Max</p>
+    <div style="height: 249px;width: 1117.2px;margin-left: 135px;
+    border: 1.5px solid var(--bs-gray-600);margin-top: 13px;">
+    <img style="height: 86px;padding-top: 0px;margin-left: 11px;margin-top: 6px;width: 431.1625px;" src="segi kl.png" />
+    <br />
+    <br />
+        <div style="height: 159px;">
+          <img src="iphone-grey.png" style="height: 102px;margin-left: 19px; margin-top: 10px" />
+            <p style="width: 316px;margin-left: 140px;margin-top: -78px;font-size: 25px;">Iphone 10 Pro Max</p>
             <p style="width: 66px;font-size: 25px;margin-left: 474px;margin-top: -56px;">Grey</p>
             <p style="font-size: 25px;width: 56px;margin-left: 631px;margin-top: -54px;">x1</p>
             <p style="font-size: 25px;width: 126px;margin-left: 748px;margin-top: -51px;">RM3500.00</p>
             <p style="width: 166px;font-size: 25px;margin-left: 948px;margin-top: -55px;color: #ba1525;">RM3500.00</p>
         </div>
     </div>
-    <div style="width: 1117.2px;height: 89px;margin-left: 135px;border: 1.5px solid var(--bs-gray-600);"><button class="btn btn-primary" type="button" style="height: 49px;width: 121.5px;margin-left: 30px;margin-top: 19px;background: #1A2C42;">Order Status</button><button class="btn btn-primary" type="button" style="width: 121.5px;margin-left: 15px;margin-top: 19px;height: 49px;background: #1A2C42;">Order Again</button>
+    <div style="width: 1117.2px;height: 89px;margin-left: 135px;
+    border: 1.5px solid var(--bs-gray-600);">
+    <button class="btn btn-primary" type="button" style="height: 49px;width: 121.5px;
+    margin-left: 30px;margin-top: 19px;background: #1A2C42;">Order Status</button>
+    <button class="btn btn-primary" type="button" style="width: 121.5px;margin-left: 15px;
+    margin-top: 19px;height: 49px;background: #1A2C42;">Order Again</button>
         <p style="font-size: 25px;margin-left: 744px;margin-top: -41px;width: 74.2px;">Total:</p>
         <p style="font-size: 25px;margin-left: 948px;margin-top: -57px;">RM3500.00</p>
     </div>
-      
+    <br />
+<br />
+<br />
+              
 <!----------<div style="height: 125px;">----------->
 <!-----------------Other Content------------------>
 </div>
