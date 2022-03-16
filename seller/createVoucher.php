a4f50c9c3f8506a712131f3a03c20f541a6e16e6
easonying0723 <ey73210723@gmail.com>
2022-03-17 03:58:39 +0800

    asd

--- seller/createVoucher.php
+++ seller/createVoucher.php
@@ -1,13 +1,23 @@
 <?php
-    require_once __DIR__ . '/header.php'
+    require __DIR__ . '/header.php'
 ?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+<script charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
 
 <!-- Page Content -->
 <div class="container-fluid" style="background-color: #FFFFFF; width:80%;">
-   <h2 class="">Create Voucher</h2>
+   <h2 class="mt-5 mb-2">Create Voucher</h2>
    <form>
        <div class="container m-2">
-         <h5>Basic Information</h5>
+         <h5 class="mt-1 mb-1">Basic Information</h5>
          <div class="row mt-2">
             <label for="">Voucher Code</label>
             <div class="col-12">
@@ -107,11 +117,44 @@
                <input type="text" aria-label="First name" class="form-control" placeholder="Enter your product category">
             </div>
          </div>
-         <div class="row mt-2">
-            <label for="">Product Category</label>
+         <div class="row mt-2" style="float: right;">
             <div class="col-12">
                <button type="button" class="btn btn-light" data-dismiss="modal">Reset</button>
                <button type="button" class="btn btn-danger" data-dismiss="modal">Search</button>
+            </div>
+         </div>
+         <div class="row container p-3">
+            <div class="col-12">
+               <table class="table">
+                  <thead>
+                     <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product Image</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">SKU</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                     </tr>
+                     <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                     </tr>
+                     <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                     </tr>
+                  </tbody>
+               </table>
             </div>
          </div>
       </div>
--- voucherRun.php
+++ voucherRun.php
@@ -1,15 +1,7 @@
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
-<!-- Custom styles for this template-->
-<link href="css/sb-admin-2.min.css" rel="stylesheet">
-<link href="css/classic.css" rel="stylesheet">
 
 <link href="/css/voucher.css" rel="stylesheet" type="text/css">
 
