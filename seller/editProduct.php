76efe177744841995800c33eca16e682c0b094a7
Maverick <maverickleeweilin88@gmail.com>
2022-03-16 11:36:00 +0800

    Update editProduct.php

--- seller/editProduct.php
+++ seller/editProduct.php
@@ -7,7 +7,7 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Product Details</h1>
+        <h1 class="h3 mb-0 text-gray-800">Edit Product Details</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                 class="fas fa-download fa-sm text-white-50"></i> Update</a>
     </div>
@@ -26,15 +26,19 @@
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
-                                            <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                    <select class="form-select" aria-label="SearchBy">
-                                                        <option selected value="name">Product Name</option>
-                                                        <option value="mainSku">Main Product SKU</option>
-                                                        <option value="sku">Product SKU</option>
-                                                    </select>
-                                                </div>
-                                                <input type="text" class="form-control" placeholder="Enter ..." aria-label="SearchKeyword">
+                                            <div class="drag-list">
+                                                <div class="drag-item" draggable="true">A</div>
+                                                <div class="drag-item" draggable="true">B</div>
+                                                <div class="drag-item" draggable="true">C</div>
+                                                <div class="drag-item" draggable="true">D</div>
+                                                <div class="drag-item" draggable="true">E</div>
+                                                <div class="drag-item" draggable="true">F</div>
+                                                <div class="drag-item" draggable="true">G</div>
+                                                <div class="drag-item" draggable="true">H</div>
+                                                <div class="drag-item" draggable="true">I</div>
+                                                <div class="drag-item" draggable="true">J</div>
+                                                <div class="drag-item" draggable="true">K</div>
+                                                <div class="drag-item" draggable="true">L</div>
                                             </div>
                                         </div>
                                     </div>
@@ -60,6 +64,128 @@
 
 </div>
 <!-- /.container-fluid -->
+
+<style>
+
+    @import "nib";
+
+    [draggable] {
+    user-select: none;
+    }
+    .drag-list {
+    overflow: hidden;
+    margin: 10px auto;
+    width: 500px;
+    border: 1px solid #ccc;
+    }
+    .drag-item {
+    float: left;
+    padding: 50px 20px;
+    width: 25%;
+    text-align: center;
+    color: #555;
+    background: #ddd;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    transition: 0.25s;
+    }
+    .drag-start {
+    opacity: 0.8;
+    }
+    .drag-enter {
+    opacity: 0.5;
+    transform: scale(0.9);
+    }
+</style>
+
+<script>
+    function DragNSort (config) {
+  this.$activeItem = null;
+  this.$container = config.container;
+	this.$items = this.$container.querySelectorAll('.' + config.itemClass);
+  this.dragStartClass = config.dragStartClass;
+  this.dragEnterClass = config.dragEnterClass;
+}
+
+DragNSort.prototype.removeClasses = function () {
+	[].forEach.call(this.$items, function ($item) {
+		$item.classList.remove(this.dragStartClass, this.dragEnterClass);
+  }.bind(this));
+};
+
+DragNSort.prototype.on = function (elements, eventType, handler) {
+	[].forEach.call(elements, function (element) {
+    element.addEventListener(eventType, handler.bind(element, this), false);
+  }.bind(this));
+};
+
+DragNSort.prototype.onDragStart = function (_this, event) {
+  _this.$activeItem = this;
+
+  this.classList.add(_this.dragStartClass);
+  event.dataTransfer.effectAllowed = 'move';
+  event.dataTransfer.setData('text/html', this.innerHTML);
+};
+
+DragNSort.prototype.onDragEnd = function (_this) {
+	this.classList.remove(_this.dragStartClass);
+};
+
+DragNSort.prototype.onDragEnter = function (_this) {
+	this.classList.add(_this.dragEnterClass);
+};
+
+DragNSort.prototype.onDragLeave = function (_this) {
+	this.classList.remove(_this.dragEnterClass);
+};
+
+DragNSort.prototype.onDragOver = function (_this, event) {
+  if (event.preventDefault) {
+  event.preventDefault();
+  }
+
+  event.dataTransfer.dropEffect = 'move';
+
+  return false;
+};
+
+DragNSort.prototype.onDrop = function (_this, event) {
+	if (event.stopPropagation) {
+    event.stopPropagation();
+  }
+
+  if (_this.$activeItem !== this) {
+    _this.$activeItem.innerHTML = this.innerHTML;
+    this.innerHTML = event.dataTransfer.getData('text/html');
+  }
+
+  _this.removeClasses();
+
+  return false;
+};
+
+DragNSort.prototype.bind = function () {
+	this.on(this.$items, 'dragstart', this.onDragStart);
+	this.on(this.$items, 'dragend', this.onDragEnd);
+	this.on(this.$items, 'dragover', this.onDragOver);
+	this.on(this.$items, 'dragenter', this.onDragEnter);
+	this.on(this.$items, 'dragleave', this.onDragLeave);
+	this.on(this.$items, 'drop', this.onDrop);
+};
+
+DragNSort.prototype.init = function () {
+	this.bind();
+};
+
+// Instantiate
+var draggable = new DragNSort({
+	container: document.querySelector('.drag-list'),
+  itemClass: 'drag-item',
+  dragStartClass: 'drag-start',
+  dragEnterClass: 'drag-enter'
+});
+draggable.init();
+</script>
 
 <?php
     require __DIR__ . '/footer.php'
