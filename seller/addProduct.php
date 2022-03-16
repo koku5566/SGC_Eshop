5bdab952782c7ecb5b42aa37b56e9e3eca95499c
Maverick <maverickleeweilin88@gmail.com>
2022-03-16 20:52:14 +0800

    Update addProduct.php

--- seller/addProduct.php
+++ seller/addProduct.php
@@ -18,7 +18,7 @@
                                     <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
                                         <div class="input-group mb-3">
                                             <div class="input-group-prepend">
-                                                <span class="input-group-text" >Product Name</span>
+                                                <span class="input-group-text">Product Name</span>
                                             </div>
                                             <input type="text" pattern="{20,100}" value="<?php echo(isset($_POST['productName']) ? $_POST['productName'] : "sad");?>" class="form-control" name="productName" placeholder="Enter ..." aria-label="SearchKeyword" required>
                                         </div>
@@ -28,33 +28,40 @@
                                 <div class="row">
                                     <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
                                         <div class="input-group mb-3">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" >Main Category</span>
+                                            <div class="row">
+                                                <div class="col-xl-4 col-lg-4 col-sm-4" style="padding-bottom: .625rem;">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text" >Main Category</span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-xl-4 col-lg-4 col-sm-4" style="padding-bottom: .625rem;">
+                                                    <select class="form-select" onchange='this.form.submit()' name="mainCategoryId" aria-label="mainCategory" style="color:currentColor;width: 100%;background-color: white;border: 1px solid;border-radius: 0 5px 5px 0;" required>
+                                                        <option value="">Please Select a Category</option>
+                                                            <?php
+                                                            //Main Category
+                                                            $sql = "SELECT * FROM mainCategory";
+                                                            $result = mysqli_query($conn, $sql);
+
+                                                            if (mysqli_num_rows($result) > 0) {
+                                                                while($row = mysqli_fetch_assoc($result)) {
+                                                                    $categoryId = $row["main_category_id"];
+                                                                    $categoryName = $row["main_category_name"];
+
+                                                                    if(isset($_POST["mainCategoryId"]) && $_POST["mainCategoryId"] == $categoryId )
+                                                                    {
+                                                                        echo("<option selected value=\"$categoryId\">$categoryName</option>");
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        echo("<option value=\"$categoryId\">$categoryName</option>");
+                                                                    }
+                                                                }
+                                                            }
+                                                            ?>
+                                                    </select>
+                                                </div>
                                             </div>
-                                            <select class="form-select" onchange='this.form.submit()' name="mainCategoryId" aria-label="mainCategory" style="color:currentColor;width: 60%;;background-color: white;border: 1px solid;border-radius: 0 5px 5px 0;" required>
-                                                <option value="">Please Select a Category</option>
-                                                    <?php
-                                                    //Main Category
-                                                    $sql = "SELECT * FROM mainCategory";
-                                                    $result = mysqli_query($conn, $sql);
-
-                                                    if (mysqli_num_rows($result) > 0) {
-                                                        while($row = mysqli_fetch_assoc($result)) {
-                                                            $categoryId = $row["main_category_id"];
-                                                            $categoryName = $row["main_category_name"];
-
-                                                            if(isset($_POST["mainCategoryId"]) && $_POST["mainCategoryId"] == $categoryId )
-                                                            {
-                                                                echo("<option selected value=\"$categoryId\">$categoryName</option>");
-                                                            }
-                                                            else
-                                                            {
-                                                                echo("<option value=\"$categoryId\">$categoryName</option>");
-                                                            }
-                                                        }
-                                                    }
-                                                    ?>
-                                            </select>
+                                            
                                         </div>
                                     </div>
                                     <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
