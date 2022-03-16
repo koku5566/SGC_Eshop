c174931a7a56578a11a1822fba76b57449464bdb
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-16 10:47:47 +0800

    Update PurchaseHistory.php

--- PurchaseHistory.php
+++ PurchaseHistory.php
@@ -9,9 +9,11 @@
     <div style="padding-left: 59px;padding-top: 40px;">
         <div>
             <button class="btn btn-primary" type="button" style="width: 89.5px;padding-left: 6px;
-            margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;border-color: var(--bs-body-bg);text-decoration: underline;"><i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);background: rgba(255,255,255,0);"></i>Back</button><br /><br /></div>
+            margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;border-color: var(--bs-body-bg);text-decoration: underline;">
+            <i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);background: rgba(255,255,255,0);">
+        </i>Back</button><br /><br /></div>
     </div>
-    <div style="height: 249px;width: 1117.2px;margin-left: 135px;box-shadow: 4px 4px rgba(108,117,125,0.3);border: 1.5px solid var(--bs-gray-600);">
+    <div style="height: 249px;width: 1117.2px;margin-left: 135px;box-shadow: 4px 4px rgba(108,117,125,0.3);border: 1px solid var(--bs-gray-600);">
     <img style="height: 86px;padding-top: 0px;margin-left: 11px;margin-top: 0px;width: 431.1625px;" src="img/segi kl.png" />
         <div style="height: 159px;">
             <img src="/img/product/iphone-grey.png" style="height: 102px;margin-left: 19px;" />
@@ -22,12 +24,12 @@
             <p style="width: 166px;font-size: 25px;margin-left: 948px;margin-top: -55px;color: #ba1525;">RM3500.00</p>
         </div>
     </div>
-    <div style="width: 1117.2px;height: 89px;margin-left: 135px;border: 1.5px solid var(--bs-gray-600);box-shadow: 4px 4px rgba(108,117,125,0.3);"><button class="btn btn-primary" type="button" style="height: 49px;width: 121.5px;margin-left: 30px;margin-top: 19px;background: #1A2C42;">Order Status</button><button class="btn btn-primary" type="button" style="width: 121.5px;margin-left: 15px;margin-top: 19px;height: 49px;background: #1A2C42;">Order Again</button>
+    <div style="width: 1117.2px;height: 89px;margin-left: 135px;border: 1px solid var(--bs-gray-600);box-shadow: 4px 4px rgba(108,117,125,0.3);"><button class="btn btn-primary" type="button" style="height: 49px;width: 121.5px;margin-left: 30px;margin-top: 19px;background: #1A2C42;">Order Status</button><button class="btn btn-primary" type="button" style="width: 121.5px;margin-left: 15px;margin-top: 19px;height: 49px;background: #1A2C42;">Order Again</button>
         <p style="font-size: 25px;margin-left: 744px;margin-top: -41px;width: 74.2px;">Total:</p>
         <p style="font-size: 25px;margin-left: 948px;margin-top: -57px;">RM3500.00</p>
     </div>
-    <br>
-    <br>
+    <br />
+    <br />
 </body>
 <br>
 <br>
