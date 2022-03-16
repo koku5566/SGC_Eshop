0363e5343796a2f7f511c0272abc56c20b1a785c
Maverick <maverickleeweilin88@gmail.com>
2022-03-16 19:50:01 +0800

    Update addProduct.php

--- seller/addProduct.php
+++ seller/addProduct.php
@@ -10,96 +10,112 @@
             <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                     class="fas fa-download fa-sm text-white-50"></i> Update</a>
         </div>
-                   <!-- Product Filter -->
-                    <div class="row">
-                        <div class="col-xl-12 col-lg-12">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h5 class="m-0 font-weight-bold text-primary">Add Product</h5>
-                                </div>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12">
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h5 class="m-0 font-weight-bold text-primary">Add Product</h5>
+                            </div>
 
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
-                                            <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" >Product Name</span>
-                                                </div>
-                                                <input type="text" pattern="{20,100}" class="form-control" name="keyword" placeholder="Enter ..." aria-label="SearchKeyword" required>
+                            <!-- Card Body -->
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" >Product Name</span>
                                             </div>
+                                            <input type="text" pattern="{20,100}" class="form-control" name="keyword" placeholder="Enter ..." aria-label="SearchKeyword" required>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="row">
-                                        <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
-                                            <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" >Main Category</span>
-                                                </div>
-                                                <select class="form-select" name="mainCategory" aria-label="mainCategory" style="color:currentColor;">
-                                                        <option selected value="name">Others</option>
-                                                        <?php
+                                <div class="row">
+                                    <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" >Main Category</span>
+                                            </div>
+                                            <select class="form-select" onchange='this.form.submit()' name="mainCategoryId" aria-label="mainCategory" style="color:currentColor;width: 60%;;background-color: white;border: 1px solid;border-radius: 0 5px 5px 0;">
+                                                    <?php
+                                                    //Main Category
+                                                    $sql = "SELECT * FROM mainCategory";
+                                                    $result = mysqli_query($conn, $sql);
 
-                                                        //Main Category
-                                                        $sql = "SELECT * FROM mainCategory";
-                                                        $result = mysqli_query($conn, $sql);
+                                                    if (mysqli_num_rows($result) > 0) {
+                                                        while($row = mysqli_fetch_assoc($result)) {
+                                                            $categoryId = $row["main_category_id"];
+                                                            $categoryName = $row["main_category_name"];
 
-                                                        if (mysqli_num_rows($result) > 0) {
-                                                            while($row = mysqli_fetch_assoc($result)) {
-                                                                $categoryId = $row["main_category_id"];
-                                                                $categoryName = $row["main_category_name"];
-
+                                                            if(isset($_POST["mainCategoryId"]) && $_POST["mainCategoryId"] == $categoryId )
+                                                            {
+                                                                echo("<option value=\"$categoryId\">$categoryName</option>");
+                                                            }
+                                                            else
+                                                            {
                                                                 echo("<option value=\"$categoryId\">$categoryName</option>");
                                                             }
                                                         }
-                                                        ?>
-                                                </select>
-                                            </div>
+                                                    }
+                                                    ?>
+                                            </select>
                                         </div>
-                                        <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
-                                            <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text" >Sub Category</span>
-                                                </div>
-                                                    <select class="form-select" name="mainCategory" aria-label="mainCategory" style="color:currentColor;">
-                                                        <option selected value="null">None</option>
-                                                        <?php
-
-                                                            //Main Category
-                                                            $sql = "SELECT * FROM subCategory WHERE main_category_id = '$categoryid'";
+                                    </div>
+                                    <div class="col-xl-6 col-lg-6 col-sm-6" style="padding-bottom: .625rem;">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text" >Sub Category</span>
+                                            </div>
+                                                <select class="form-select" name="subCategoryId" aria-label="mainCategory" style="color:currentColor;width: 60%;;background-color: white;border: 1px solid;border-radius: 0 5px 5px 0;">
+                                                    <?php
+                                                        if(isset($_POST["mainCategoryId"]))
+                                                        {
+                                                            $maincategoryid = $_POST["mainCategoryId"];
+                                                            //Sub Category
+                                                            $sql = "SELECT * FROM subCategory WHERE main_category_id = '$maincategoryid'";
                                                             $result = mysqli_query($conn, $sql);
 
                                                             if (mysqli_num_rows($result) > 0) {
                                                                 while($row = mysqli_fetch_assoc($result)) {
                                                                     $categoryId = $row["sub_category_id"];
                                                                     $categoryName = $row["sub_category_name"];
-
-                                                                    echo("<option value=\"$categoryId\">$categoryName</option>");
+    
+                                                                    if(isset($_POST["mainCategoryId"]) && $_POST["mainCategoryId"] == $categoryId )
+                                                                    {
+                                                                        echo("<option value=\"$categoryId\">$categoryName</option>");
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        echo("<option value=\"$categoryId\">$categoryName</option>");
+                                                                    }
                                                                 }
                                                             }
-                                                            ?>
-                                                    </select>
-                                            </div>
+                                                        }
+                                                        
+                                                        ?>
+                                                </select>
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div class="row">
-                                        <div class="col-xl-10 col-lg-8 col-sm-4" style="padding-bottom: .625rem;">
-                                            
-                                        </div>
-                                        <div class="col-xl-1 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
-                                            <button type="button" class="btn btn-primary">Search</button>
-                                        </div>
-                                        <div class="col-xl-1 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
-                                            <button type="button" class="btn btn-outline-dark">Reset</button>
-                                        </div>
+                                <div class="row">
+                                    <div class="col-xl-10 col-lg-8 col-sm-4" style="padding-bottom: .625rem;">
                                         
                                     </div>
+                                    <div class="col-xl-1 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
+                                        <button type="button" class="btn btn-primary">Search</button>
+                                    </div>
+                                    <div class="col-xl-1 col-lg-2 col-sm-4" style="padding-bottom: .625rem;">
+                                        <button type="button" class="btn btn-outline-dark">Reset</button>
+                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+            </form>
     </div>
     <!-- /.container-fluid -->
 
