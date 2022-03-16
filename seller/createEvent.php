843ac2d886338a0fbefa961f0e97a8d5bd36c37f
koku5566 <koku5566@gmail.com>
2022-03-16 23:18:36 +0800

    First try on saving create event to database

--- seller/createEvent.php
+++ seller/createEvent.php
@@ -2,6 +2,63 @@
     require __DIR__ . '/header.php'
 ?>
 
+<?php
+	// if($_SESSION['login'] == false)
+	// {
+	// 	echo "<script>alert('Login to Continue');
+	// 		window.location.href='login.php';</script>";
+    // }
+?>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_POST["eRegister"])){
+        $checkImage = getimagesize($_FILES["coverImage"]["tmp_name"]);
+        if($checkImage !== false)
+        {
+            $coverIMG = $_FILES['coverImage']['tmp_name'];
+            $coverImgContent = addslashes(file_get_contents($coverIMG));
+        }
+        $eTitle = mysqli_real_escape_string($conn, SanitizeString($_POST["eventTitle"]));
+        $eDateFrom = mysqli_real_escape_string($conn, SanitizeString($_POST["eDate_From"]));
+        $eDateTo = mysqli_real_escape_string($conn, SanitizeString($_POST["eDate_To"]));
+        $eTimeFrom = mysqli_real_escape_string($conn, SanitizeString($_POST["eTime_From"]));
+        $eTimeTo = mysqli_real_escape_string($conn, SanitizeString($_POST["eTime_To"]));
+        $eDes = htmlentities($_POST["eDesc"]);
+        $eCat = mysqli_real_escape_string($conn, SanitizeString($_POST["eCategory"]));
+        $eLoc = mysqli_real_escape_string($conn, SanitizeString($_POST["eLocation"]));
+        $eTnc = htmlentities($_POST["eTnC"]); //decode using html_entity_decode()
+        $eOrganiser = 1;//mysqli_real_escape_string($conn, SanitizeString($_SESSION["eLocation"]));
+
+        // $check = "SELECT * FROM `event`";
+        // if($stmt = mysqli_prepare ($conn, $check)){
+        //   mysqli_stmt_execute($stmt);
+        //   mysqli_stmt_bind_result($stmt, $c1,$c2,$c3,$c4,$c5,$c6,$c7,$c8,$c9,$c10,$c11,$c12);
+
+        //   while(mysqli_stmt_fetch($stmt)){
+        //       if($eTitle == $c3)
+        //       {
+        //         echo "<script>alert('This Event seems to be in our database, check yo');window.location.href='seller/dashboard.php';</script>";
+        //       }
+        //   }
+        //   mysqli_stmt_close($stmt);
+        // }
+        
+        $sql = "INSERT INTO `event`(`cover_image`, `event_name`, `event_date`, `eventEnd_date`, `event_time`, `eventEnd_time`, `description`, `category`, `location`, `event_tnc`, `organiser_id`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+            if ($stmt = mysqli_prepare($conn,$sql)){
+                mysqli_stmt_bind_param($stmt,"bsssssssssi",$coverImgContent,$eTitle,$eDateFrom,$eDateTo,$eTimeFrom,$eTimeTo,$eDes,$eCat,$eLoc,$eTnc,$eOrganiser);
+                mysqli_stmt_execute($stmt);
+                
+                    if(mysqli_stmt_affected_rows($stmt) == 1){
+                        echo "<script>alert('Success!!!!!');</script>";
+                    }
+                    else{
+                        echo "<script>alert('Failed registration!,Please contact our Admin');</script>";
+                    }		
+                    mysqli_stmt_close($stmt);
+            }
+          }
+?>
+
 <title>Create Event</title>
 <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/suneditor@latest/dist/css/suneditor.min.css">
@@ -16,7 +73,7 @@
     <div class="d-lg-block d-xl-block d-xxl-block" style="margin-top: 30px;">
         
         <!-- Form for create new event -->
-        <form>
+        <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST">
             
             <section style="padding-top: 25px;padding-bottom: 40px;padding-right: 30px;padding-left: 30px;margin-top: 20px;box-shadow: 0px 0px 10px;">
                 <h2>Cover Image<input class="form-control" type="file" id="coverImg" style="margin-top: 10px;" name="coverImage"></h2>
@@ -55,7 +112,8 @@
                 <div style="margin-top: 30px;">
                     <h3>Description</h3>
                     <div>
-                        <div id="toolbar_container" class="sun-editor"></div><textarea class="form-control" id="editor" placeholder="Edit your description here to make your event looks more fun" name="eDesc"></textarea>
+                        <div id="toolbar_container" class="sun-editor"></div>
+                        <textarea class="form-control" id="editor" placeholder="Edit your description here to make your event looks more fun" name="eDesc"></textarea>
                     </div>
                 </div>
                 <div style="margin-top: 30px;">
@@ -107,13 +165,15 @@
                 <div>
                     <h2>Terms and Conditions</h2>
                     <div>
-                        <div id="toolbar_container-1" class="sun-editor"></div><textarea class="form-control" id="editor-1" placeholder="Insert placeholder text in sunEditorInit.js" name="eTnC"></textarea>
+                        <div id="toolbar_container-1" class="sun-editor"></div>
+                        <textarea class="form-control" id="editor-1" placeholder="Insert placeholder text in sunEditorInit.js" name="eTnC"></textarea>
                     </div>
                 </div>
             </section>
             
             <div style="margin-top: 61px;text-align: center;margin-bottom: 61px;">
-                <div class="btn-group" role="group"><button class="btn btn-secondary" type="button" style="margin-left: 5px;margin-right: 5px;">Back</button><button class="btn btn-outline-primary" type="submit" style="margin-left: 5px;margin-right: 5px;background: rgb(163, 31, 55);color: rgb(255,255,255);">Submit</button></div>
+                <div class="btn-group" role="group"><button class="btn btn-secondary" type="button" style="margin-left: 5px;margin-right: 5px;">Back</button>
+                <button class="btn btn-outline-primary" type="submit" name="eRegister" style="margin-left: 5px;margin-right: 5px;background: rgb(163, 31, 55);color: rgb(255,255,255);">Submit</button></div>
             </div>
         </form>
     </div>
