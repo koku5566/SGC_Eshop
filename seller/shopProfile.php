2fffb89e45d7d8f539ece0cf922f263206fb7b20
Marcheal <marchealyong@gmail.com>
2022-03-16 15:06:45 +0800

    update

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -9,8 +9,7 @@
 <div class="container-fluid" style="width:80%">
   <div class="container profileContainer">
     <div class="row">
-      <div>
-      <img class="relative bg-image container-fluid" src="https://edufair.fsi.com.my/img/sponsor/20/cover_1530346726.jpeg">
+      <div class="relative bg-image" style: backgrounf="https://edufair.fsi.com.my/img/sponsor/20/cover_1530346726.jpeg";>
       <div class="absolute"><button class="editBtn"><i class="far fa-image"></i> Edit Cover Photo</button></div>
       <div class="sellerPicContainer"><img id="" class="sellerPic" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" class="rounded-circle"></div>
       </div>
