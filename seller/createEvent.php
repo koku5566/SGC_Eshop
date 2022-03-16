35c3b6d7086fd48b38a3ac1320cc47f9afc12443
koku5566 <koku5566@gmail.com>
2022-03-16 23:41:33 +0800

    test 3

--- seller/createEvent.php
+++ seller/createEvent.php
@@ -45,14 +45,15 @@
         
         $sql = "INSERT INTO `event`(`cover_image`, `event_name`, `event_date`, `eventEnd_date`, `event_time`, `eventEnd_time`, `description`, `category`, `location`, `event_tnc`, `organiser_id`) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
             if ($stmt = mysqli_prepare($conn,$sql)){
-                mysqli_stmt_bind_param($stmt,"ssssssssssi",$coverImgContent,$eTitle,$eDateFrom,$eDateTo,$eTimeFrom,$eTimeTo,$eDes,$eCat,$eLoc,$eTnc,$eOrganiser);
+                mysqli_stmt_bind_param($stmt,"bsssssssssi",$coverImgContent,$eTitle,$eDateFrom,$eDateTo,$eTimeFrom,$eTimeTo,$eDes,$eCat,$eLoc,$eTnc,$eOrganiser);
                 mysqli_stmt_execute($stmt);
                 
                     if(mysqli_stmt_affected_rows($stmt) == 1){
                         echo "<script>alert('Success!!!!!');</script>";
                     }
                     else{
-                        echo "<script>alert('Failed registration!,Please contact our Admin');</script>";
+                        $error = mysqli_stmt_errno($stmt);
+                        echo "<script>alert($error);</script>";
                     }		
                     mysqli_stmt_close($stmt);
             }
