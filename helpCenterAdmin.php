157942a0028c3b41c7d301b79c3d2a263212ea56
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-16 13:15:22 +0800

    Update helpCenterAdmin.php

--- helpCenterAdmin.php
+++ helpCenterAdmin.php
@@ -944,7 +944,7 @@ function myBtnGoFunction(){
 	}			
 	
 
-$(".alert alert-success").delay(2000).slideUp(200, function() {
+$(".alert.alert-success").delay(2000).slideUp(200, function() {
     $(this).alert('close');
 });
 
