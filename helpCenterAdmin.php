42e160b9fb72da4ccfdccb6a984b38916e5b9126
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-16 17:20:43 +0800

    Update helpCenterAdmin.php

--- helpCenterAdmin.php
+++ helpCenterAdmin.php
@@ -316,18 +316,26 @@
             $sql ="SELECT hcc.hcc_id, hcc.category, hc.question, hc.answer, hc.pic, hc.pic_type, hc.disable_date
 				   FROM helpCenterCategory hcc LEFT JOIN helpCenter hc
 				   ON hcc.hcc_id = hc.hcc_id
-				   WHERE hcc.hcc_id = '$categorylist' && hcc.disable_date IS NULL && hc.disable_date IS NULL";
-			if($stmt = mysqli_prepare ($conn, $sql)){
-				mysqli_stmt_execute($stmt);
-				mysqli_stmt_bind_result($stmt, $s1,$s2,$s3,$s4,$s5,$s6,s7);
-				
-				while(mysqli_stmt_fetch($stmt)){
-					$pItem = true;
-					echo "<script>alert('SOHAI')</script>";
+				   WHERE hcc.hcc_id = ? && hcc.disable_date IS NULL && hc.disable_date IS NULL";
+		              
+            if($stmt = mysqli_prepare ($conn, $sql)){
+                mysqli_stmt_bind_param($stmt, "s", $categorylist);	//HARLO IF THIS INT = i, STRING = s
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) > 0){
+                    echo "<script>GOT ITEM</script>";
+                }else{
+					echo "<script>No ITEM</script>";
 				}
-				mysqli_stmt_close($stmt);
-			}
+                
+                mysqli_stmt_free_result($stmt);
+                mysqli_stmt_close($stmt);
+            
+            }
 			
+			
+			/*
 			
 			if($pItem == false){
 				echo "<div class='alert alert-success'>CLEAR CAN DELETE</div>";
@@ -335,6 +343,9 @@
 			else{
 				echo "<div class='alert alert-danger'>Can't DELETE CUZ GOT ITEM</div>";
 			}
+			
+			*/
+			
 			
 			
 		}
