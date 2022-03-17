9f7788f526ae4a982025d13b30a0e434b429df2a
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-18 00:02:03 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -1,14 +1,16 @@
 <?php
     require __DIR__ . '/header.php'
 ?>
+
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],$_POST['message'],$_POST['subject'], $_POST['blek']) && !empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"]) && !empty($_POST["subject"])){
-  
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],$_POST['message'],$_POST['subject'],$_POST['CUSubmit']) && !empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"]) && !empty($_POST["subject"])){
+ 
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
   $subject = $_POST['subject'];
   header('Content-Type: application/json');
+  
   if ($name === '') {
     print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
     exit();
@@ -30,123 +32,107 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],
     print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
     exit();
   }
-  $content = "From: $name \nEmail: $email \nMessage: $message";
+  
+  $content="From: $name \n Email: $email \n Message: $message";
   $recipient = "yourmamasofat2000@gmail.com";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $content, $mailheader) or die("Error!");
-  print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-  exit();
-  
+  echo "<script>alert('Email sent!')</script>";
 }
 ?>
-
 
 
 <!-- Begin Page Content --------------------------------------------------------------------------------------------->
 <div class="container-fluid" style="width:80%">		
 		
-		<h1>bruh</h1>
+		
 	<div  class = "faker"style ="width: 80%; margin: auto">
-		<!--Section: Contact v.2-->
-<section class="mb-4">
+      <!--Section: Contact v.2-->
+		<section class="mb-4">
 
-    <!--Section heading-->
-    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-    <!--Section description-->
-    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-        a matter of hours to help you.</p>
+			<!--Section heading-->
+			<h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+			<!--Section description-->
+			<p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+				a matter of hours to help you.</p>
 
-    <div class="row">
+			<div class="row justify-content-md-center">
 
-        <!--Grid column-->
-        <div class="col-md-9 mb-md-0 mb-5">
-            <form id="contact-form" name="contact-form" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+				<!--Grid column-->
+				<div class="col-md-9 mb-md-0 mb-5">
+					<form id="contact-form" name="contact-form" action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
 
-                <!--Grid row-->
-                <div class="row">
+						<!--Grid row-->
+						<div class="row">
 
-                    <!--Grid column-->
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control">
-                            <label for="name" class="">Your name</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
+							<!--Grid column-->
+							<div class="col-md-6">
+								<div class="md-form mb-0">
+									<label for="name" class="">Your name</label>
+									<input type="text" id="name" name="name" class="form-control"  required>
+									
+								</div>
+							</div>
+							<!--Grid column-->
 
-                    <!--Grid column-->
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="email" name="email" class="form-control">
-                            <label for="email" class="">Your email</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
+							<!--Grid column-->
+							<div class="col-md-6">
+								<div class="md-form mb-0">
+									<label for="email" class="">Your email</label>
+									<input type="email" id="email" name="email" class="form-control"  required>
+									
+								</div>
+							</div>
+							<!--Grid column-->
 
-                </div>
-                <!--Grid row-->
+						</div>
+						<!--Grid row-->
 
-                <!--Grid row-->
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="md-form mb-0">
-                            <input type="text" id="subject" name="subject" class="form-control">
-                            <label for="subject" class="">Subject</label>
-                        </div>
-                    </div>
-                </div>
-                <!--Grid row-->
+						<!--Grid row-->
+						<div class="row">
+							<div class="col-md-12">
+								<div class="md-form mb-0">
+									<label for="subject" class="">Subject</label>
+									<input type="text" id="subject" name="subject" class="form-control"  required>
+									
+								</div>
+							</div>
+						</div>
+						<!--Grid row-->
 
-                <!--Grid row-->
-                <div class="row">
+						<!--Grid row-->
+						<div class="row">
 
-                    <!--Grid column-->
-                    <div class="col-md-12">
+							<!--Grid column-->
+							<div class="col-md-12">
 
-                        <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                            <label for="message">Your message</label>
-                        </div>
+								<div class="md-form">
+									<label for="message">Your message</label>
+									<textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"  required></textarea>
+									
+								</div>
 
-                    </div>
-                </div>
-                <!--Grid row-->
-				<input type = "submit" name ="blek" value = "SUBMIT"></input>
-            </form>
+							</div>
+						</div>
+						<!--Grid row-->
+						<input type = "submit" name = "CUSubmit" class="btn btn-primary"  value = "Submit" >
+					</form>
 
-            <div class="text-center text-md-left">
-               <a class="btn btn-primary" onclick="validateForm();">Send</a>
-            </div>
-            <div class="status"></div>
-        </div>
-        <!--Grid column-->
+					<div class="text-center text-md-left">
+						<!--<a class="btn btn-primary" onclick="validateForm();">Send</a>-->
+					</div>
+					<div class="status"></div>
+				</div>
+				
 
-        <!--Grid column-->
-        <div class="col-md-3 text-center">
-            <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                    <p>San Francisco, CA 94126, USA</p>
-                </li>
+				
 
-                <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                    <p>+ 01 234 567 89</p>
-                </li>
+			</div>
 
-                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                    <p>contact@mdbootstrap.com</p>
-                </li>
-            </ul>
-        </div>
-        <!--Grid column-->
-
-    </div>
-
-</section>
+		</section>
+		</div>
 <!--Section: Contact v.2-->
-	</div>
-      
 </div>
-
 <!-- /.container-fluid ----------------------------------------------------------------------------------------------->
 
 <?php
@@ -159,59 +145,58 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],
 }
 </style>
 <script>
+/*
 function validateForm() {
   var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
-  }
   var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
+  var subject =  document.getElementById('subject').value;
+  var message =  document.getElementById('message').value;
+  
+  var n = false 
+  var e = false
+  var s = false
+  var m = false 
+  var c = false
+  
+  
+  if (name.replace(/(^\s+|\s+$)/g, '') == "") {
+	  document.querySelector('.status').innerHTML = "Name cannot be empty";
+      n = false;
+  }else{n = true}
+  
+  if (email.replace(/(^\s+|\s+$)/g, '') == "") {
+	  document.querySelector('.status').innerHTML = "Email cannot be empty";
+      e = false;
   } else {
       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if(!re.test(email)){
           document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
+           e = false;
+      }else{e = true;}
   }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
+  
+  if (subject.replace(/(^\s+|\s+$)/g, '') == "") {
       document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var message =  document.getElementById('message').value;
-  if (message == "") {
+      s = false;
+  }else{s = true;}
+  
+  if (message.replace(/(^\s+|\s+$)/g, '') == "") {
       document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
+      m = false
+  }else{m = true;}
+  
+  
+  if(n == true && e == true && s == true && m == true && c == true){
+	  document.getElementById('CUSubmit').disabled = false;
+  }else{
+	  document.getElementById('CUSubmit').disabled = true;
   }
-  document.querySelector('.status').innerHTML = "Sending...";
+  
+  
+  
 }
+	
+*/
 
 
-document.getElementById('status').innerHTML = "Sending...";
-
-formData = {
-  'name': $('input[name=name]').val(),
-  'email': $('input[name=email]').val(),
-  'subject': $('input[name=subject]').val(),
-  'message': $('textarea[name=message]').val()
-};
-
-
-$.ajax({
-  url: "helpCenterAdminTest.php",
-  type: "POST",
-  data: formData,
-  success: function (data, textStatus, jqXHR) {
-
-    $('#status').text(data.message);
-    if (data.code) //If mail was sent successfully, reset the form.
-      $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-  },
-  error: function (jqXHR, textStatus, errorThrown) {
-    $('#status').text(jqXHR);
-  }
-});
 </script>
