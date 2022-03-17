18209d79299ca5ca272f8d6375270d9736ab6b43
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-17 23:18:37 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -158,3 +158,60 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],
 	border: 1px solid black;
 }
 </style>
+<script>
+function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+      document.querySelector('.status').innerHTML = "Name cannot be empty";
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.querySelector('.status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      return false;
+  }
+  document.querySelector('.status').innerHTML = "Sending...";
+}
+
+
+document.getElementById('status').innerHTML = "Sending...";
+
+formData = {
+  'name': $('input[name=name]').val(),
+  'email': $('input[name=email]').val(),
+  'subject': $('input[name=subject]').val(),
+  'message': $('textarea[name=message]').val()
+};
+
+
+$.ajax({
+  url: "hca.php",
+  type: "POST",
+  data: formData,
+  success: function (data, textStatus, jqXHR) {
+
+    $('#status').text(data.message);
+    if (data.code) //If mail was sent successfully, reset the form.
+      $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    $('#status').text(jqXHR);
+  }
+});
+</script>
