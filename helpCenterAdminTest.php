3158745909939fab0ee82b40603be0ddae0fb9dc
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-17 17:26:40 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -12,16 +12,47 @@
   if(isset( $_POST['subject']))
   $subject = $_POST['subject'];
 
+
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $subject = $_POST['subject'];
+  header('Content-Type: application/json');
+  if ($name === '') {
+    print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+    exit();
+  }
+  if ($email === '') {
+    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+    exit();
+  } else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+      exit();
+    }
+  }
+  if ($subject === '') {
+    print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+    exit();
+  }
+  if ($message === '') {
+    print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+    exit();
+  }
+  
   $content="From: $name \n Email: $email \n Message: $message";
   $recipient = "yourmamasofat2000@gmail.com";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $content, $mailheader) or die("Error!");
   echo "Email sent!";
+
 ?>
 
 
 <!-- Begin Page Content --------------------------------------------------------------------------------------------->
 <div class="container-fluid" style="width:80%">		
+		
 		
 	<div  class = "faker"style ="width: 80%; margin: auto">
       <!--Section: Contact v.2-->
@@ -96,7 +127,7 @@
 					</form>
 
 					<div class="text-center text-md-left">
-						<a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+						<a class="btn btn-primary" onclick="validateForm();">Send</a>
 					</div>
 					<div class="status"></div>
 				</div>
@@ -122,7 +153,59 @@
 }
 </style>
 <script>
-
+function validateForm() {
+  var name =  document.getElementById('name').value;
+  if (name == "") {
+      document.querySelector('.status').innerHTML = "Name cannot be empty";
+      return false;
+  }
+  var email =  document.getElementById('email').value;
+  if (email == "") {
+      document.querySelector('.status').innerHTML = "Email cannot be empty";
+      return false;
+  } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!re.test(email)){
+          document.querySelector('.status').innerHTML = "Email format invalid";
+          return false;
+      }
+  }
+  var subject =  document.getElementById('subject').value;
+  if (subject == "") {
+      document.querySelector('.status').innerHTML = "Subject cannot be empty";
+      return false;
+  }
+  var message =  document.getElementById('message').value;
+  if (message == "") {
+      document.querySelector('.status').innerHTML = "Message cannot be empty";
+      return false;
+  }
+  document.querySelector('.status').innerHTML = "Sending...";
+}
 	
+	
+document.getElementById('status').innerHTML = "Sending...";
+formData = {
+  'name': $('input[name=name]').val(),
+  'email': $('input[name=email]').val(),
+  'subject': $('input[name=subject]').val(),
+  'message': $('textarea[name=message]').val()
+};
+
+
+$.ajax({
+  url: "mail.php",
+  type: "POST",
+  data: formData,
+  success: function (data, textStatus, jqXHR) {
+
+    $('#status').text(data.message);
+    if (data.code) //If mail was sent successfully, reset the form.
+      $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    $('#status').text(jqXHR);
+  }
+});
 
 </script>
