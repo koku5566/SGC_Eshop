535a48709e96bc365a03222fbf391fb05a72993e
Carol <chianxin@gmail.com>
2022-03-18 02:22:24 +0800

    Update viewShippingOrders.php

--- seller/viewShippingOrders.php
+++ seller/viewShippingOrders.php
@@ -5,72 +5,118 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
 <!-- Begin Page Content -->
-<div class="container-fluid" style="width:80%; font-size:14px" >
-        <div class="row">
-            <div class="date-filter col-6 mb-4" style="float:right;">
-            <div class="row">
-              <div class="col"><span style="white-space:nowrap; margin-right:10px;">Order Creation Date: </span></div>
-              <div class="col"><input type="text" class="form-control" name="daterange" value="01/01/2022 - 01/15/2022" /></div>
-            </div>
+<div class="container-fluid" style="width:80%; font-size:14px">
 
-            </div>
+    <div class="card  shadow mb-4">
+        <div class="card-body">
 
-            <div class="filter-search col-6">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="all-tab" data-toggle="tab" href="#all" role="tab" aria-controls="all"
+                        aria-selected="true">All</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="toship-tab" data-toggle="tab" href="#toship" role="tab"
+                        aria-controls="toship" aria-selected="false">To Ship</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="shipping-tab" data-toggle="tab" href="#shipping" role="tab"
+                        aria-controls="shipping" aria-selected="false">Shipping</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="completed-tab" data-toggle="tab" href="#completed" role="tab"
+                        aria-controls="completed" aria-selected="false">Completed</a>
+                </li>
+            </ul>
 
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <select class="form-control">
-                            <option>Order ID</option>
-                            <option>Buyer Name</option>
-                            <option>Product</option>
-                            <option>Tracking Number</option>
-                        </select>
-                    </div>
-                    <input type="text" class="form-control input-text" placeholder="Search order"
-                        aria-label="Recipient's username" aria-describedby="basic-addon2">
-                    <div class="input-group-append"> <button class="btn btn-outline-primary" type="button"><i
-                                class="fa fa-search"></i></button> </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="order-list-panel">
-            <div class="top-card card-header">
-                <div class="row">
-                    <div class="col-5">Product(s)</div>
-                    <div class="col-1">Order Total</div>
-                    <div class="col-2">Status</div>
-                    <div class="col-2">All Channels</div>
-                    <div class="col-2">Actions</div>
-                </div>
-            </div>
-        </div>
-        <div class="order-list-body pt-3" >
-            <!--Each Order Item-->
-            <div class="card">
-                <div class="card-header">
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <!--------------------------------All-------------------------------------->
+                <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
                     <div class="row">
-                        <div class="col md-auto text-start"><span><strong>Username</strong></span></div>
-                        <div class="col md-auto text-end" style="text-align:right;"><span><strong> Order ID: 125353</strong></span></div>
-                    </div>
-                </div>
-                <div class="card-body" >
-                    <div class="row">
-                        <div class="col-1"><img width="100%" src="https://www.w3schools.com/images/w3schools_green.jpg"
-                                alt="W3Schools.com"></div>
-                        <div class="col-3">Wireless Earphone dfdfbdfbd</div>
-                        <div class="col-1">X1</div>
+                        <div class="date-filter col-6 mb-4" style="float:right;">
+                            <div class="row">
+                                <div class="col"><span style="white-space:nowrap; margin-right:10px;">Order Creation
+                                        Date: </span></div>
+                                <div class="col"><input type="text" class="form-control" name="daterange"
+                                        value="01/01/2022 - 01/15/2022" /></div>
+                            </div>
 
-                        <div class="col-1">RM349.00</div>
-                        <div class="col-2">Completed</div>
-                        <div class="col-2">DHL eCommerce 2121113134</div>
-                        <div class="col-2"><a href="#">Check details</a></div>
+                        </div>
+
+                        <div class="filter-search col-6">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <select class="form-control">
+                                        <option>Order ID</option>
+                                        <option>Buyer Name</option>
+                                        <option>Product</option>
+                                        <option>Tracking Number</option>
+                                    </select>
+                                </div>
+                                <input type="text" class="form-control input-text" placeholder="Search order"
+                                    aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                <div class="input-group-append"> <button class="btn btn-outline-primary"
+                                        type="button"><i class="fa fa-search"></i></button> </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="order-list-panel">
+                        <div class="top-card card-header">
+                            <div class="row">
+                                <div class="col-5">Product(s)</div>
+                                <div class="col-1">Order Total</div>
+                                <div class="col-2">Status</div>
+                                <div class="col-2">All Channels</div>
+                                <div class="col-2">Actions</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="order-list-body pt-3">
+                        <!--Each Order Item-->
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col md-auto text-start"><span><strong>Username</strong></span></div>
+                                    <div class="col md-auto text-end" style="text-align:right;"><span><strong> Order ID:
+                                        125353</strong></span></div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-1"><img width="100%"
+                                            src="https://www.w3schools.com/images/w3schools_green.jpg"
+                                            alt="W3Schools.com"></div>
+                                    <div class="col-3">Wireless Earphone dfdfbdfbd</div>
+                                    <div class="col-1">X1</div>
+
+                                    <div class="col-1">RM349.00</div>
+                                    <div class="col-2">Completed</div>
+                                    <div class="col-2">DHL eCommerce 2121113134</div>
+                                    <div class="col-2"><a href="#">Check details</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--End of Order Item-->
                     </div>
                 </div>
+
+                <!--------------------------------To ship--------------------------------------->
+                <div class="tab-pane fade" id="toship" role="tabpanel" aria-labelledby="toship-tab">...</div>
+
+                <!--------------------------------Shipping--------------------------------------->
+                <div class="tab-pane fade" id="shipping" role="tabpanel" aria-labelledby="shipping-tab">...</div>
+
+                <!--------------------------------Completed--------------------------------------->
+                <div class="tab-pane fade" id="completed" role="tabpanel" aria-labelledby="completed-tab">...</div>
             </div>
-            <!--End of Order Item-->
+
 
         </div>
+    </div>
+
 
 </div>
 <!-- /.container-fluid -->
@@ -79,6 +125,8 @@
 <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <script>
+
+
     //Date picker function
     $('input[name="dates"]').daterangepicker();
 
