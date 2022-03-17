c12c30a84954d54e8092935ea50e06ffb2a8dee4
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-17 13:41:25 +0800

    u

--- adminManageUser.php
+++ adminManageUser.php
@@ -2,7 +2,7 @@
 	$title = "RTX-Tech ADMIN PANEL";
 	include "Header.php";
 	
-	if($_SESSION['login'] == false || $_SESSION['role'] == "ADMIN")
+	if($_SESSION['login'] == false || $_SESSION['role'] != "ADMIN")
 	{
 		echo "<script>alert('Login as Admin account to access');
 			window.location.href='login.php';</script>";
