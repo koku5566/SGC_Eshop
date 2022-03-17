b5147fdde525654dec2cb47f36b88ca9b6c40c8a
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-18 00:39:24 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -34,7 +34,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],
   }
   
   $content="From: $name \n Email: $email \n Message: $message";
-  $recipient = "yourmamasofat2000@gmail.com";
+  $recipient = "kitmincheong@gmail.com";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $content, $mailheader) or die("Error!");
   echo "<script>alert('Email sent!')</script>";
