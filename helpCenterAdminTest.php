d56f41b759bb045a4c50651adcddb813edc612b7
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-18 12:48:19 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -4,40 +4,56 @@
 
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'],$_POST['email'],$_POST['message'],$_POST['subject'],$_POST['CUSubmit']) && !empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"]) && !empty($_POST["subject"])){
- 
+ /*
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
   $subject = $_POST['subject'];
+  */
  // header('Content-Type: application/json');
   
+  $to = $_POST['email'];
+  
   if ($name === '') {
-    print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-    exit();
+   // print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+    //exit();
   }
   if ($email === '') {
-    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-    exit();
+    //print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+   // exit();
   } else {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-      exit();
+     // print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+     // exit();
     }
   }
   if ($subject === '') {
-    print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-    exit();
+   // print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+    //exit();
   }
   if ($message === '') {
-    print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-    exit();
+   // print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+    //exit();
   }
-  
+  /*
   $content="From: $name \n Email: $email \n Message: $message";
   $recipient = "kitmincheong@gmail.com";
   $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $content, $mailheader) or die("Error!");
-  echo "<script>alert('Email sent!')</script>";
+ */
+ $from = "Contact_Us_Mail@sgprototype2.com";
+ $subject = "PHP Mail Sending Checking";
+ $message = "PHP mail works fine";
+ $header = "FROM:" . $from;
+ //mail($recipient, $subject, $content, $mailheader)
+ if(mail($to, $subject, $message, $header)){
+	  echo "<script>alert('Email sent!')</script>";
+ }else{
+	 echo "<script>alert('Fail to sent!')</script>";
+ }
+ 
+  
+  /* For those whose mail are moving in spam try this mail($to,$Subject,$Message,$headers,'-fsomeone@example.com');
+someone@example.com don't miss that  -f TO ADD BEFORE*/
 }
 ?>
 
