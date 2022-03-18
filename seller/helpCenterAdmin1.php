0f37a78ee9e6e03835c7c6d44637180d8df28394
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-18 18:48:27 +0800

    Update helpCenterAdmin1.php

--- seller/helpCenterAdmin1.php
+++ seller/helpCenterAdmin1.php
@@ -625,7 +625,7 @@
 									
 		
 		
-
+		<!--FAQ SECTION-->
 		<div class = "pp">	
 			<!--Display All Category-->
 			<form action ="<?php echo $_SERVER['PHP_SELF'];?>" method = "POST">		
@@ -738,8 +738,29 @@
 	
 				</div>
 		</div>
+		
+		<!--CONTACT US SECTION-->
+		<ul class="nav nav-tabs" id="myTab" role="tablist">
+		  <li class="nav-item">
+			<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+		  </li>
+		</ul>
+		<div class="tab-content" id="myTabContent">
+		  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+		  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+		  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+		</div>
+    
 
-      
+
+
+	
  </div>       
     <!-- /.container-fluid --------------------------------------------------------------------------------------------------------------------------------------->
 
