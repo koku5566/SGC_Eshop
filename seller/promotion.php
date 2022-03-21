cdf5b21cc3f34dfa539b7dec11ad303b7eac89c0
Qxlim12 <qxionglim@yahoo.com>
2022-03-22 00:17:17 +0800

    test

--- seller/promotion.php
+++ seller/promotion.php
@@ -152,9 +152,36 @@
     .imgInp{
         display:none;
     }
+    .hide{
+        display:none;
+    }
 </style>
+<script>
+function initImages()
+    {
+        const deleteImg = document.querySelectorAll('.image-tools-delete-icon');
 
+        deleteImg.forEach(img => {
+            img.addEventListener('click', function handleClick(event) {
+                img.parentElement.previousElementSibling.previousElementSibling.src="";
+                img.parentElement.nextElementSibling.classList.remove("hide");
+                img.parentElement.classList.add("hide");
+            });
+        });
 
+        const imgInp = document.querySelectorAll('.imgInp');
+        imgInp.forEach(img => {
+            img.addEventListener('change', function handleChange(event) {
+                const [file] = img.files
+                if (file) {
+                    img.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.src = URL.createObjectURL(file)
+                    img.parentElement.parentElement.previousElementSibling.previousElementSibling.classList.remove("hide");
+                    img.parentElement.parentElement.classList.add("hide");
+                }
+            });
+        });
+    }
+</script>
 <?php
     require __DIR__ . '/footer.php'
 ?>
