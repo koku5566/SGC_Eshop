b746a063b83fbccdebb4052e65dd68e31f7bde37
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-21 16:38:14 +0800

    j

--- seller/review.php
+++ seller/review.php
@@ -1,26 +1,27 @@
 <?php
     require __DIR__ . '/header.php'
 ?>
-<script src = "jquery.js"></script>
-<script src = "js/bootstrap.js"></script>
-<link href="css/bootstrap.css" rel= "stylesheet"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
 
 
     <!-- Begin Page Content -------------------------------------------------------------------------------------------------------------------->
     <div class="container-fluid" style="width:100%;">
-    <h2>I tell u ar this </h2>
-	<div class = "container">
-		<br />
-		<h2 align="center">Ajax Live Data Search</h2>
-		<div class = "form-group">
-			<div class = "input-group">
-				<span class = "input-group-addon">Search</span>
-				<input type = "text" name = "search_text" id = "search_text" placeholder = "Search By customer name" class = "form-control" />
-			</div>
+    <body>
+	  <div class="container">
+	   <br />
+	   <h2 align="center">Ajax Live Data Search using Jquery PHP MySql</h2><br />
+	   <div class="form-group">
+		<div class="input-group">
+		 <span class="input-group-addon">Search</span>
+		 <input type="text" name="search_text" id="search_text" placeholder="Search by Customer Details" class="form-control" />
 		</div>
-		<br />
-		<div id="result"></div>
-	</div>
+	   </div>
+	   <br />
+	   <div id="result"></div>
+	  </div>
+	 </body>
     </div>
     <!-- /.container-fluid --------------------------------------------------------------------------------------------------------------------->
 
@@ -28,28 +29,32 @@
 
 <script>
 $(document).ready(function(){
-	$('#search_text').keyup(function(){
-		var txt = $(this).val();
-		consol.log($txt);
-		if(txt != '')
-		{
-			
-		}
-		else
-		{
-			$('#result').html('');
-			$.ajax({
-				url: "reviewSearch.php",
-				method: "post",
-				data: {search:txt},
-				dataType: "text",
-				success: function(data)
-				{
-					$('#result').html(data);
-				}
-			});
-		}
-	});
+
+ load_data();
+
+ function load_data(query)
+ {
+  $.ajax({
+   url:"search.php",
+   method:"POST",
+   data:{query:query},
+   success:function(data)
+   {
+    $('#result').html(data);
+   }
+  });
+ }
+ $('#search_text').keyup(function(){
+  var search = $(this).val();
+  if(search != '')
+  {
+   load_data(search);
+  }
+  else
+  {
+   load_data();
+  }
+ });
 });
 </script>
 <?php
--- seller/reviewSearch.php
+++ seller/reviewSearch.php
@@ -1,45 +1,62 @@
 <?php
 $output = '';
-$sql = "SELECT cu_id, name, email, campus, subject, message, status 
-		FROM `contactUs` 
-		WHERE cu_id LIKE '%".$_POST["search"]."%'";
-$result = mysqli_query($conn, $sql);
-if(mysqli_num_rows($result) > 0)
+if(isset($_POST["query"]))
 {
-	$output .= '<h4 align ="center">Search Result</h4>';
-	$output .= '<div class = "table-responsive">
-							  <table class = "table table bordered">
-							  <tr>
-								<th>cu_id</th>
-								<th>Name</th>
-								<th>Email</th>
-								<th>Campus</th>
-								<th>Subject</th>
-								<th>Message</th>
-								<th>Status</th>
-							   </tr>';
-    while($row = mysqli_fetch_array($result))
-	{
-	  $output .= '
-				<tr>
-					<td>'.$row["cu_id"].'</td>
-					<td>'.$row["name"].'</td>
-					<td>'.$row["email"].'</td>
-					<td>'.$row["campus"].'</td>
-					<td>'.$row["subject"].'</td>
-					<td>'.$row["message"].'</td>
-					<td>'.$row["status"].'</td>
-				</tr>
-				
-			';
-	}
-	echo $output;
-	echo "<script>alert('no way')</script>";
+ $search = mysqli_real_escape_string($conn, $_POST["query"]);
+ $query = "
+  SELECT cu_id, name, email, campus, subject, message, status
+  FROM contactUs 
+  WHERE cu_id LIKE '%".$search."%'
+  OR name LIKE '%".$search."%' 
+  OR email LIKE '%".$search."%' 
+  OR campus LIKE '%".$search."%' 
+  OR subject LIKE '%".$search."%'
+  OR message LIKE '%".$search."%'
+  OR status LIKE '%".$search."%'
+  
+ ";
 }
 else
 {
-	echo 'Data Not Found';
+ $query = "
+  SELECT cu_id, name, email, campus, subject, message, status FROM tbl_customer ORDER BY cu_id
+ ";
 }
-
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0)
+{
+ $output .= '
+  <div class="table-responsive">
+   <table class="table table bordered">
+    <tr>
+     <th> cu_id</th>
+     <th>name</th>
+     <th>email</th>
+     <th>campus</th>
+     <th>subject</th>
+	 <th>message</th>
+     <th>status</th>
+    </tr>
+ ';
+ while($row = mysqli_fetch_array($result))
+ {
+  $output .= '
+   <tr>
+    <td>'.$row["cu_id"].'</td>
+    <td>'.$row["name"].'</td>
+    <td>'.$row["email"].'</td>
+    <td>'.$row["campus"].'</td>
+    <td>'.$row["subject"].'</td>
+	<td>'.$row["message"].'</td>
+    <td>'.$row["status"].'</td>
+   </tr>
+  ';
+ }
+ echo $output;
+}
+else
+{
+ echo 'Data Not Found';
+}
 
 ?>
