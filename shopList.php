1faf03f37d1709476e38fc0278750e6d2d4c3470
Maverick <maverickleeweilin88@gmail.com>
2022-03-21 22:10:42 +0800

    Update shopList.php

--- shopList.php
+++ shopList.php
@@ -7,7 +7,9 @@
 <div class="container">
   <div class="row">
     <div class="list">
-      <div class="image"><img src=""></div>
+      <div class="image-container">
+          <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
+      </div>
       <div class="description">
         <p class="descriptionContainer1">
           <i class="bi bi-people-fill"></i>
