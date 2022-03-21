7232e1e11e94d9ba4edf0e7f6da2b1b32df10036
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-21 11:30:34 +0800

    Update login.php

--- login.php
+++ login.php
@@ -28,7 +28,7 @@
 					$_SESSION['id'] = $row["username"];
 					$_SESSION['name'] = $row["name"];
 					$_SESSION['role'] = $row["role"];
-					?><script>window.location = '<?php echo($domain/index.php);?>';</script><?php
+					?><script>window.location = '<?php echo("$domain/index.php");?>';</script><?php
 				}
 			} else {
 				$Login = false;
