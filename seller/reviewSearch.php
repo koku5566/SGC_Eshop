3ba18130776d4119aec359f5ab9886abe4713f34
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-21 18:05:35 +0800

    Update reviewSearch.php

--- seller/reviewSearch.php
+++ seller/reviewSearch.php
@@ -1,73 +1,66 @@
-
 <?php
- $conn = mysqli_connect("localhost","sgcprot1_SGC_ESHOP","bXrAcmvi,B#U","sgcprot1_SGC_ESHOP");
- 
-$output = 'OUTPUT';
+$conn = mysqli_connect("localhost","sgcprot1_SGC_ESHOP","bXrAcmvi,B#U","sgcprot1_SGC_ESHOP");
+$output = '';
 if(isset($_POST["query"]))
 {
  $search = mysqli_real_escape_string($conn, $_POST["query"]);
- $sql = "
+ $query = "
   SELECT cu_id, name, email, campus, subject, message, status
   FROM contactUs 
-  WHERE cu_id LIKE '%".$search."%'";
-
+  WHERE cu_id LIKE '%".$search."%'
+  OR name LIKE '%".$search."%' 
+  OR email LIKE '%".$search."%' 
+  OR campus LIKE '%".$search."%' 
+  OR subject LIKE '%".$search."%'
+  OR message LIKE '%".$search."%'
+  OR status LIKE '%".$search."%'
   
-echo $search;
+ ";
 }
 else
 {
- $sql = "SELECT cu_id, name, email, campus, subject, message, status 
+ $query = "SELECT cu_id, name, email, campus, subject, message, status 
 		   FROM contactUs 
-		   WHERE disable_date IS NULL
-		   ORDER BY cu_id;";
+		   ORDER BY cu_id;
   
-
+ ";
  echo "sohai gone d";
 }
-$result = mysqli_query($conn, $sql);
-
- 
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0)
+{
+ $output .= '
+  <div class="table-responsive">
+   <table class="table table bordered">
+    <tr>
+     <th> cu_id</th>
+     <th>name</th>
+     <th>email</th>
+     <th>campus</th>
+     <th>subject</th>
+	 <th>message</th>
+     <th>status</th>
+    </tr>
+ ';
  while($row = mysqli_fetch_array($result))
  {
-	 
-  $output .= '$row["cu_id"]';
-  
-  
-  $output .= "niama";
-  
+  $output .= '
+   <tr>
+    <td>'.$row["cu_id"].'</td>
+    <td>'.$row["name"].'</td>
+    <td>'.$row["email"].'</td>
+    <td>'.$row["campus"].'</td>
+    <td>'.$row["subject"].'</td>
+	<td>'.$row["message"].'</td>
+    <td>'.$row["status"].'</td>
+   </tr>
+  ';
  }
  echo $output;
-   //echo 'GOTGOTGOTOGOT';
+}
+else
+{
+ echo 'Data Not Found';
+}
 
-///////////////////////////////////////////////////////////
-/*
-$sql = "SELECT hcc_id, category, pic, pic_type 
-	        FROM `helpCenterCategory` 
-			WHERE disable_date IS NULL";	//KM - CHANGE TO helpCenterCategory
-		if($stmt = mysqli_prepare ($conn, $sql)){
-			mysqli_stmt_execute($stmt);
-			mysqli_stmt_bind_result($stmt, $c1,$c2,$c3,$c4);
-			
-			
-			while(mysqli_stmt_fetch($stmt)){			
-
-					
-					
-					if(strlen($c2) < 15){
-						//$cTrim = substr($c2, 0, 16);
-						//$cTag = "$cTrim...";
-						$cTag = "$c2 \n";
-					}else{$cTag = $c2;}
-					
-				echo "<div class='item'>" .
-					"<form action = '". $_SERVER['PHP_SELF']."' method = 'POST' class = 'imgCssT'>" .
-						"<input type='hidden' name='pid' value='$c1'>" .
-						 "<input type='image' name = 'uProduct' src='data: $c4;base64, " . base64_encode($c3)."' alt='Submit' class='imgCss'></form>" .
-						"<div class='catTag'>$cTag</div></div>"; 
-			}
-
-	}
-
-*/
 ?>
-
