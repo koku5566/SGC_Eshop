9f468af4be6df128358aed4a41544d5f6d2146fc
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-21 21:47:37 +0800

    Update PurchaseHistory.php

--- PurchaseHistory.php
+++ PurchaseHistory.php
@@ -6,51 +6,86 @@
                
 
 <!-- Begin Page Content -->
-<div class="container-fluid" style="width:80%">
+<div class="container-fluid" style="width:100%">
                 <h1 style="width: 315px;margin-left: 460px;color: #c71526;">Purchase History</h1>
-    <div style="padding-left: 20px;padding-top: 40px;">
-        <div>
-            <button class="btn btn-primary" type="button" style="width: 89.5px;padding-left: 0px;
-            margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;
-            border-color: var(--bs-body-bg);text-decoration: underline;">
-            <i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);
-            background: rgba(255,255,255,0);">
-            </i>Back</button><br /><br /><br /></div>
+                <div style="padding-left: 20px;padding-top: 40px;">
+                     <div>
+                         <button class="btn btn-primary" type="button" style="width: 89.5px;padding-left: 0px;
+                                     margin-left: 0px;background: rgba(13,110,253,0);color: var(--bs-blue);border-style: none;
+                                      border-color: var(--bs-body-bg);text-decoration: underline;">
+                                  <i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);
+                                        background: rgba(255,255,255,0);">
+                                        </i>Back</button>
+                                        <br /><br />
+                                        <br />
+                     </div>
+                </div>
+  
+  <div style="height: 550px;border-style: solid;">
+    <div class="row">
+      <div class="col" style="width: 500px;">
+      <img src="segi kl.png" style="width:400px; margin-left:10px">
+      </div>
+      <div class="col-xl-2">
+        <p style="text-aligh:centre;">Date & Time:<br/> <span id="datetime"></span></p><br/>
+      </div>
     </div>
-    <div style="height: 500px;width: 1117.2px;margin-left: 50px;
-        border: 1.5px solid black;margin-top: 13px;">
-        <img style="height: 86px;padding-top: 0px;margin-left: 11px;margin-top: 6px;width: 431.1625px;" src="img/segi kl.png" />
-         <br />
-         <br />
-        <div style="height: 159px;">
-          <img src="img/product/iphone-grey.png" style="height: 102px;margin-left: 19px; margin-top: 10px" />
-            <p style="width: 316px;margin-left: 140px;margin-top: -78px;font-size: 25px;">3-in-1 Power Bank with Phone Stand Model: WI-SP510</p>
-            <p style="width: 66px;font-size: 25px;margin-left: 474px;margin-top: -90px;">Navy Blue</p>
-            <p style="font-size: 25px;width: 56px;margin-left: 631px;margin-top: -80px;">x1</p>
-            <p style="font-size: 25px;width: 126px;margin-left: 748px;margin-top: -51px;">RM349.00</p>
-            <p style="width: 166px;font-size: 25px;margin-left: 948px;margin-top: -55px;color: #ba1525;">RM349.00</p>
+    <br/>
+    <div class="row" style="font-size:20px; ">
+      <div class="col">
+      <img src="iphone-grey.png" style="width:150px; height:150px;">
+      </div>
+      <div class="col" style="margin-top:10px">3-in-1 Power Bank with Phone Stand Model: WI-SP510
+      </div>
+      <div class="col" style="margin-top:10px;">Navy Blue
+      </div>
+      <div class="col" style="margin-top:10px">x1
+      </div>
+      <div class="col" style="margin-top:10px">RM349.00
+      </div>
+      <div class="col" style="margin-top:10px">RM349.00
+      </div>
+    </div>
+    <div class="row" style="font-size:20px; ">
+      <div class="col">
+      <img src="iphone-grey.png" style="width:150px; height:150px;">
+      </div>
+      <div class="col" style="margin-top:10px">3-in-1 Power Bank with Phone Stand Model: WI-SP510
+      </div>
+      <div class="col" style="margin-top:10px;">Navy Blue
+      </div>
+      <div class="col" style="margin-top:10px">x1
+      </div>
+      <div class="col" style="margin-top:10px">RM349.00
+      </div>
+      <div class="col" style="margin-top:10px" id="price">RM349.00
+      </div>
+    </div>
+    <br/>
+    <br/>
+    <div style="width: 1509px;height: 89px;margin-left: 0px;
+            border: 2px solid black;">
+      <div class="row">
+      <div class="col" style="margin-top:20px">
+        <button class="btn btn-primary" type="button" style="background: #1A2C42; margin-left:20px">Order Status</button>
+        <button class="btn btn-primary" type="button" style="background: #1A2C42; margin-left:10px">Order Again</button>
         </div>
-        <div style="height: 159px;">
-          <img src="img/product/iphone-grey.png" style="height: 102px;margin-left: 19px; margin-top: 10px" />
-            <p style="width: 316px;margin-left: 140px;margin-top: -78px;font-size: 25px;">3-in-1 Power Bank with Phone Stand Model: WI-SP510</p>
-            <p style="width: 66px;font-size: 25px;margin-left: 474px;margin-top: -90px;">Navy Blue</p>
-            <p style="font-size: 25px;width: 56px;margin-left: 631px;margin-top: -80px;">x1</p>
-            <p style="font-size: 25px;width: 126px;margin-left: 748px;margin-top: -51px;">RM349.00</p>
-            <p style="width: 166px;font-size: 25px;margin-left: 948px;margin-top: -55px;color: #ba1525;">RM349.00</p>
-        </div>
-    </div>
-    <div style="width: 1117.2px;height: 89px;margin-left: 50px;
-    border: 1.5px solid black;">
-    <button class="btn btn-primary" type="button" style="height: 49px;width: 121.5px;
-    margin-left: 30px;margin-top: 19px;background: #1A2C42;" href="purchaseShippingDetails.php">Order Status</button>
-    <button class="btn btn-primary" type="button" style="width: 121.5px;margin-left: 15px;
-    margin-top: 19px;height: 49px;background: #1A2C42;">Order Again</button>
-        <p style="font-size: 25px;margin-left: 744px;margin-top: -41px;width: 74.2px;">Total:</p>
-        <p style="font-size: 25px;margin-left: 948px;margin-top: -57px;">RM465.60</p>
-    </div>
-    <br />
-
-</div>  
+        <div class="col-2" style="margin-top:20px; font-size:20px">Total:
+      </div>
+        <div class="col-2" style="margin-top:20px; font-size:20px;color:red" id="calc">RM465.60<span id="total_Price"></span>
+      </div>
+      </div> 
+             
+</div>
+  
+  <script>
+   var dt = new Date();
+  document.getElementById("datetime").innerHTML = dt.toLocaleString();
+  </script> 
+  
+  
+</div>
+  
    <!-- /.container-fluid -->
 
 <?php
