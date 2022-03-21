d3d3e4782c52715397f65fa0b31379212bed754b
koku5566 <koku5566@gmail.com>
2022-03-21 21:42:01 +0800

    Update createEvent.php

--- seller/createEvent.php
+++ seller/createEvent.php
@@ -61,7 +61,6 @@
                     if(mysqli_stmt_affected_rows($stmt) == 1){
                         echo "<script>alert('Success!!!!!');</script>";
                         $_SESSION['eventID'] = 1; //need change
-                        header("Location:https://eshop.sgcprototype2.com/seller/addTicketType.php");
                     }
                     else{
                         $error = mysqli_stmt_error($stmt);
