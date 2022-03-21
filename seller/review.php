4ff6176e3d919ef86f78da8772fc08617b2dfff1
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-21 17:03:47 +0800

    Update review.php

--- seller/review.php
+++ seller/review.php
@@ -3,8 +3,8 @@
 ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
- <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />-->
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />-->
 
 
     <!-- Begin Page Content -------------------------------------------------------------------------------------------------------------------->
