cad6d4e1590633971ee0f6bed28f44bd0885abb4
Maverick <maverickleeweilin88@gmail.com>
2022-03-22 13:03:51 +0800

    Update product.php

--- seller/product.php
+++ seller/product.php
@@ -839,16 +839,13 @@
 
                 if (img.files && img.files[0] && img.files.length > 1) {
                     for (var j = 0,i = 0; i < this.files.length; i++) {
-                        while(imgInp[j].parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.srcset != "" && j < 9)
+                        while(imgInp[j].parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.getAttribute('src') != "" && j < 9)
                         {
                             j++;
                         }
 
                         if(j < 9)
                         {
-                            console.log(j);
-                            console.log(imgInp[j]);
-
                             imgInp[j].parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.src = URL.createObjectURL(img.files[i])
                             imgInp[j].parentElement.parentElement.previousElementSibling.previousElementSibling.classList.remove("hide");
                             imgInp[j].parentElement.parentElement.classList.add("hide");
