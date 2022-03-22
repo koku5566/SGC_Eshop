01fbd94d6d10c71c833b7cf7e018120cd6bdf6fb
easonying0723 <ey73210723@gmail.com>
2022-03-23 03:10:37 +0800

    dasdasd

--- voucher.php
+++ voucher.php
@@ -1,3 +1,15 @@
+<?php
+   require __DIR__ . '/header.php'
+?>
+
+<?php
+
+   $res = mysqli_query($conn, "SELECT v.*, t.* FROM voucher v, voucherType vt WHERE v.voucher_type_id=t.voucher_type_id");
+
+   while($row=mysql_fetch_array($res))
+   {
+?>
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
@@ -11,8 +23,8 @@
    </div>
    <div class="card-body">
       <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
-      <h5 class="card-subtitle text-muted">RM1 off</h5>
-      <small>Used : 30 FEB 2022</small><br>
+      <h5 class="card-subtitle text-muted"><?php echo $row['voucher_type'];?><?php echo $row['discount_amount'];?> off</h5>
+      <small>Used : <?php echo $row['voucher_startdate'];?></small><br>
       <u>
          <a type="" class="" data-toggle="modal" data-target="#termsModal">
          T&C applied.
@@ -23,23 +35,6 @@
       <button type="button" class="btn btn-warning btn-sm" style="float: right" data-toggle="modal" data-target="#alert">CLAIM</button>
    </div>
 </div>
-
-<!-- <div class="card" id="vouchercard2">
-   <div class="card-body">
-      <div class="row">
-         <div class="col-mb-3 m-2">
-            <img class="m-2" src="../img/segilogo.png" id="voucherlogo">
-         </div>
-         <div class="col-mb-7 m-2">
-            <h6 class="card-title"><strong>SEGi Group of Colleges</strong></h6>
-            <h5 class="card-subtitle text-muted">RM1 off</h5>
-            <small>Used : 30 FEB 2022</small><br>
-            <u>
-               <a type="" class="" data-toggle="modal" data-target="#termsModal">
-               T&C applied.
-               </a>
-            </u>
-         </div>
-      </div>
-   </div>
-</div> -->
+<?php 
+}
+?>
