a4997ec141106008d4c4198f73ecf8688efdcaa6
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-03-23 15:47:33 +0800

    Update facilityManagement.php

--- facilityManagement.php
+++ facilityManagement.php
@@ -76,47 +76,7 @@
 
 <script type ="module" src="../bootstrap/js/bootstrap.min.js"></script>
 
-<script>
-   var vouchertable = $('#vouchertable').DataTable( {
 
-   retrieve: true,
-   responsive: true,
-   scrollCollapse: true,
-   ordering: true,
-   searching: true,
-   paging: true,
-
-   columnDefs: [ {
-   targets:   0,
-   className: 'select-checkbox',
-   },
-   {
-      targets: -1,
-      data: null,
-      defaultContent:'<button class="btn btn-light btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-trash"></i></button>'
-   }],
-
-   lengthMenu:[
-   [6,-1],
-   [6,"All"]
-   ],
-
-   select: {
-   style:    'multi', //'multi' - select multiple checkbox
-   selector: 'td:first-child'
-   },
-
-   order: [[ 1, 'asc' ]]
-
-   } );
-
-   //-----------------------Delete Row-----------------------------//
-   $('#vouchertable tbody').on( 'click', 'button', function () {
-
-   var row = vouchertable.row($(this).parents('tr'));
-   row.remove().draw(false);
-   })
-</script>
 
 <?php
    require __DIR__ . '/footer.php'
