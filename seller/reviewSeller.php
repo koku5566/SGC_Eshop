6781ccaefca7ff354c876f65209d70eed81b3c71
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-25 16:29:49 +0800

    Update reviewSeller.php

--- seller/reviewSeller.php
+++ seller/reviewSeller.php
@@ -5,7 +5,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />-->
- 
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
  
 <?php
 /*
