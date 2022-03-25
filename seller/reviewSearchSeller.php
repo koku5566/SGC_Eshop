15de6a562540c5ab2612bc3f93276a14e1c0d5b0
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-25 17:05:14 +0800

    Update reviewSearchSeller.php

--- seller/reviewSearchSeller.php
+++ seller/reviewSearchSeller.php
@@ -94,9 +94,9 @@ if(mysqli_num_rows($result) > 0)
  {
 	 for($i=0; $i<5; $i++){
 		 if($i < $row["rating"]){
-			 $starR .='<i class="bi bi-star-fill"></i>';
+			 $starR .='<i class="bi bi-star-fill"></i> ';
 		 }else{
-			 $starR .='<i class="bi bi-star"></i>';
+			 $starR .='<i class="bi bi-star"></i> ';
 		 }
 	 }
   $output .= '
@@ -108,7 +108,7 @@ if(mysqli_num_rows($result) > 0)
 	<td>'.$row["rr_id"].'</td>											
     <td>'.$row["product_id"].'</td>
     <td>
-	<div class="container">.'$starR'.</div>
+	<div class="container">'.$starR.'</div>
 	'.$row["message"].'
 	</td>
     <td>'.$row["rating"].'</td>
