ff4e37c06cfe41c8a596267972d62f376087e5b8
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-25 20:26:54 +0800

    Update header.php

--- header.php
+++ header.php
@@ -318,6 +318,44 @@
                             <a class="nav-link" href="login.php">Login <i class="fas fa-user"></i></a>
                             <div class="topbar-divider d-none d-sm-block"></div>
                             <a class="nav-link" href="seller/sellerLogin.php">Seller Login <i class="fas fa-user"></i></a>
+
+
+                            <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sign Up <i class="fas fa-user"></i></a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="register.php">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    User
+                                </a>
+
+                                <a class="dropdown-item" href="sellerRegister.php">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Seller
+                                </a>
+                            </div>
+                            </li>
+
+                            <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login <i class="fas fa-user"></i></a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="login.php">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    User
+                                </a>
+
+                                <a class="dropdown-item" href="sellerLogin.php">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Seller
+                                </a>
+
+                                <a class="dropdown-item" href="sellerLogin.php">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Admin
+                                </a>
+                            </div>
+                            </li>
                         <?php endif?>
                         </li>
                 </nav>
