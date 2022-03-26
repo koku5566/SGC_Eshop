e0e362cabfe0af173db7483b37aa6f383d75ecd1
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-26 22:51:42 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -10,7 +10,11 @@
 <!-- Begin Page Content --------------------------------------------------------------------------------------------->
 <div class="container-fluid" style="width:80%">		
 	
-	
+	<div class="d-flex flex-row">
+	  <div class="p-2">Flex item 1</div>
+	  <div class="p-2">Flex item 2</div>
+	  <div class="p-2">Flex item 3</div>
+	</div>
 	
 	
 
