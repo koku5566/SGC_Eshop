10a4c2356dfac1ed2e3d56129aa0461473f031a8
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-27 03:21:47 +0800

    Update account.php

--- account.php
+++ account.php
@@ -72,7 +72,7 @@ if (file_exists($target_file)) {
 }
 
 // Check file size
-if ($_FILES["proPic"]["size"] > 500000) {
+if ($_FILES["proPic"]["size"] > 50000000) {
   echo "Sorry, your file is too large.";
   $uploadOk = 0;
 }
@@ -89,8 +89,8 @@ if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  if (move_uploaded_file($_FILES["proPic"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["proPic"]["name"])). " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
