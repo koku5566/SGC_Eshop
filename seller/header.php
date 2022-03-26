f25ea44b143efa2934bac6015e438ba250324776
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-26 13:18:19 +0800

    Update header.php

--- seller/header.php
+++ seller/header.php
@@ -316,7 +316,9 @@
                                 </form>
                             </div>
                         </li>
-
+                        
+                        <!--Login-->
+                        <?php if ($_SESSION['login'] == true) :?>
                         <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
