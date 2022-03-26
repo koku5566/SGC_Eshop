577d6567e48093058742a86680668969a3504686
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-26 23:04:59 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -10,10 +10,12 @@
 <!-- Begin Page Content --------------------------------------------------------------------------------------------->
 <div class="container-fluid" style="width:80%">		
 	
-	<div class="d-flex flex-row">
+	<div class="d-flex flex-row" style = "background-color: green; border: 1px solid black;">
 	  <div class="p-2">Flex item 1</div>
 	  <div class="p-2">Flex item 2</div>
 	  <div class="p-2">Flex item 3</div>
+	  <div class="p-2">Flex item 4</div>
+	  <div class="p-2">Flex item 5</div>
 	</div>
 	
 	
