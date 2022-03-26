0abbde92659df236603e379a56a7e023ac529724
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-27 03:47:07 +0800

    Update header.php

--- header.php
+++ header.php
@@ -280,7 +280,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo($_SESSION['name']);?></span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="data:image;base64,.base64_encode(<?php echo($row['profile_picture']);?>)">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
