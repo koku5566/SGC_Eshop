696e6c1c641cbacce094af8dc43d01d7fc39e2c5
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-26 23:44:02 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -22,14 +22,7 @@
                                         <div class="col-xl-3 col-lg-4 col-sm-6" style="padding-bottom: .625rem; background-color: pink;">
                                             <a data-sqe="link" href="#">
                                                 <div class="card" style="background-color: red;">
-                                                    <div class="image-container">
-                                                        <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
-                                                    </div>
-                                                    <div class="card-body-text">
-                                                        <div class="Name">
-                                                            <p class="card-text campus-name">SUBANG JAYA</p>
-                                                        </div>            
-                                                    </div>
+                                                    
                                                 </div>   
                                             </a>
                                         </div>
