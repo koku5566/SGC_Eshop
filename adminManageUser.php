f383b17e8312177bc7a79b3cb5a21ecdec2e2425
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-27 14:11:42 +0800

    add

--- adminManageUser.php
+++ adminManageUser.php
@@ -31,7 +31,7 @@
 
 <div id="DataDiv">
 <h1>User</h1>
-<a href="ADMIN-AddUser.php"><button>Add User</button></a>
+<a href="adminAddUser.php"><button>Add User</button></a>
 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" enctype="multipart/form-data">
 	<table>
 	<tr>
