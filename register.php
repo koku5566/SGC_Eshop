f7a662bbea187565242e39b79cc158ac7c5e3722
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-03-29 00:48:52 +0800

    Update register.php

--- register.php
+++ register.php
@@ -26,8 +26,8 @@ if(isset($_POST['signup']))
 					VALUES ('$username','$email','$password','$username','$date','USER')";
 				
 
-				$sql  = "INSERT INTO user (username, email, password, name, registration_date, role)
-				VALUES ((SELECT CONCAT('CC',(SELECT LPAD((SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'user'), 6, 0))) AS newuserID),(SELECT AUTO_INCREMENT-1 FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'user'),'0','0')";
+				//$sql  = "INSERT INTO user (username, email, password, name, registration_date, role)
+				//VALUES ((SELECT CONCAT('CC',(SELECT LPAD((SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'user'), 6, 0))) AS newuserID),(SELECT AUTO_INCREMENT-1 FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'user'),'0','0')";
 
 
 					if (mysqli_query($conn, $sql)) {
