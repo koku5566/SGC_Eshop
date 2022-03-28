938ee48a2d32c5497464c6619bfc860c705efd48
Marcheal <marchealyong@gmail.com>
2022-03-29 02:59:34 +0800

    update

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -31,7 +31,7 @@
       <textarea class="form-control" id="customFile" rows="3"></textarea>
     </div>
     <div class="row">
-      <div id="uploadContainer" class="clearfix">
+      <div id="uploadContainer" class="imageContainer clearfix">
         <!-- Image display frame (place where the image will display)
           <img id="frame" src="" class="img-fluid" />
         -->
