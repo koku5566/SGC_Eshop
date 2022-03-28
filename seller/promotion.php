e1d53ced7879d942c68c6d768c52b8513d21c291
Qxlim12 <qxionglim@yahoo.com>
2022-03-28 16:52:58 +0800

    update

--- seller/promotion.php
+++ seller/promotion.php
@@ -14,10 +14,10 @@
                         <table class="table">
                             <thead>
                                 <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
-                                <th scope="col">Handle</th>
+                                <th scope="col"></th>
+                                <th scope="col">Promotion Title</th>
+                                <th scope="col">Date/Time</th>
+                                <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
