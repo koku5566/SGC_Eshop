181403b13d26002d4bd58c11161f14817823de64
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-29 23:31:46 +0800

    Update reviewRatingSearch.php

--- reviewRatingSearch.php
+++ reviewRatingSearch.php
@@ -63,10 +63,10 @@ if(mysqli_num_rows($result) > 0)
 	 $picR = '';
 	 for($i=1; $i<=5; $i++){
 		 if($row["pic$i"] === null){
-			 /*$picR .='';*/
-			 
+			 $picR .='';
+			 /*
 			 $picR .='<td><img src="https://cdn4.iconfinder.com/data/icons/lucid-files-and-folders/24/file_disabled_not_allowed_no_permission_no_access-512.png" class="imgReply"></td>';
-			 
+			 */
 		 }else{
 			 $picR .='<td><img src="https://i.kym-cdn.com/photos/images/original/001/431/201/40f.png" class="imgReply"></td>';
 		 }
