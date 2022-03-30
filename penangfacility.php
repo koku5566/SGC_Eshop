0d93edeb17824f511182ccc43c892aacdcda1641
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-03-30 16:45:23 +0800

    Update penangfacility.php

--- penangfacility.php
+++ penangfacility.php
@@ -129,7 +129,7 @@
                                             </a>
                                         </div>
                                         <div class="col-xl-4 col-lg-4 col-sm-6" style="padding-bottom: .625rem;">
-                                        <a data-sqe="link" href="penangfacility.php?id=a">
+                                        <a data-sqe="link" href="penangAuditorium.php?id=a">
                                                 <div class="card">
                                                     <div class="image-container">
                                                         <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-white-select-2020?wid=834&hei=1000&fmt=jpeg&qlt=95&.v=1586574259457" alt="Card image cap">
