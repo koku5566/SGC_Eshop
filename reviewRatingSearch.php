fcd054f564cefe837759314fe69008d44ba3230b
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-30 17:08:11 +0800

    Update reviewRatingSearch.php

--- reviewRatingSearch.php
+++ reviewRatingSearch.php
@@ -62,6 +62,7 @@ if(mysqli_num_rows($result) > 0)
 	 if($row["profile_picture"] === null){
 		 $profilePicR .= '<img src = "https://us.123rf.com/450wm/panyamail/panyamail1809/panyamail180900248/109879025-user-avatar-icon-sign-profile-symbol.jpg?ver=6" class = "reviewprofilepic">';
 	 }else{
+		 //DISPLAY PROFILE PIC NO LE JIU DEFAULT ^^
 		 $profilePicR .= '<img src = "https://cdn.vox-cdn.com/thumbor/9j-s_MPUfWM4bWdZfPqxBxGkvlw=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg" class = "reviewprofilepic">';
 	 }
 	 
@@ -74,6 +75,7 @@ if(mysqli_num_rows($result) > 0)
 			 $picR .='<td><img src="https://cdn4.iconfinder.com/data/icons/lucid-files-and-folders/24/file_disabled_not_allowed_no_permission_no_access-512.png" class="imgReply"></td>';
 			 */
 		 }else{
+			 //DISPLAY REAL PICTURE/VIDEO THEY POST
 			 $picR .='<td><img src="https://i.kym-cdn.com/photos/images/original/001/431/201/40f.png" class="imgReply"></td>';
 		 }
 			 
