a16c02c4a9cdd44b00f239f74ea1b5c811ab080e
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-30 22:49:57 +0800

    Update PurchaseHistory.php

--- PurchaseHistory.php
+++ PurchaseHistory.php
@@ -58,7 +58,7 @@ $result_2 = $stmt_2->get_result();
                 <div class="card-body">
                     <div class="row">
                         <div class="col-1 image-container">
-                            <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="/img/product/" alt="">
+                            <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="/img/product//img/product/<?php echo $row['product_cover_picture']?>" alt="">
                         </div>
                         <div class="col-md-3 col-lg-2 offset-lg-1"><?php echo $row['product_name']?></div>
                         <div class="col-md-3 col-lg-1 offset-lg-1">x<?php echo $row['quantity']?></div>
@@ -70,6 +70,7 @@ $result_2 = $stmt_2->get_result();
                     <button class="btn btn-primary" type="button" style="background: #1A2C42;">Order Status</button>
                     <button class="btn btn-primary" type="button" style="background: #1A2C42;">Order Again</button>
                      <a style="margin-left:50%;">Total</a>
+                     <a style="text-align=right;">RM<?php echo $row['price']?></a>
                 </div>
             </div>
                                    
