e3ee99898c87d2d4716cbbfc0be986c22321307e
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-03-30 23:07:35 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -114,8 +114,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['pid']) && !empty($_PO
 									<?php
 										if(isset($c1) && !empty ($c1)){											
 											for($i=1; $i<=5; $i++){
-												$k = $i-1;												
-												 if($c."$i+9" === null){
+												$k = $i-1;	
+												$j = $i+9;
+												 
+												 if(${"c" . $i+9} === null){
 													 echo '';													 
 												 }else{	
 													if($i == 1){
@@ -142,7 +144,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['pid']) && !empty($_PO
 									<?php
 										if(isset($c1) && !empty ($c1)){											
 											for($i=1; $i<=5; $i++){												
-												 if($c."$i+9" === null){
+												 if(${"c" . $i+9} === null){
 													 echo '';													 
 												 }else{	
 													//DISPLAY REAL PIC/VID
