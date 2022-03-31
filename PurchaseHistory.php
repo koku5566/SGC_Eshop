3927250c13bcfc2353d1bf29bcc9ce24639dd119
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-03-31 09:33:35 +0800

    Update PurchaseHistory.php

--- PurchaseHistory.php
+++ PurchaseHistory.php
@@ -39,45 +39,27 @@ $result_2 = $stmt_2->get_result();
     <i class="fa fa-long-arrow-left" style="padding-right: 9px;color: var(--bs-blue);background: rgba(255,255,255,0);">
     <a href="index.php">Back</a></i>
     </button>
-    <div class="tab-pane show active fade">
-        <?php 
-        while ($row = $result_2->fetch_assoc()) {
-         ?>
-        <!--Each Order Item-->
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col md-auto text-start"><span><strong><?php echo $row['shop_name']?></strong></span>
-                    </div>
-                    <div class="col md-auto text-end" style="text-align:right;"><span><strong>
-                        Order
-                         ID:
-                         <?php echo $row['order_id']?></strong></span></div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-1 image-container">
-                            <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="/img/product//img/product/<?php echo $row['product_cover_picture']?>" alt="">
-                        </div>
-                        <div class="col-md-3 col-lg-2 offset-lg-1"><?php echo $row['product_name']?></div>
-                        <div class="col-md-3 col-lg-1 offset-lg-1">x<?php echo $row['quantity']?></div>
-                        <div class="col-md-3 col-lg-2 offset-lg-1"><?php echo $row['quantity']?></div>
-                        <div class="col-md-3 col-lg-2 offset-lg-1" style="text-align:right;">RM<?php echo $row['price']?></div>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-primary" type="button" style="background: #1A2C42;">Order Status</button>
-                    <button class="btn btn-primary" type="button" style="background: #1A2C42;">Order Again</button>
-                     <a style="margin-left:50%;">Total</a>
-                     <a style="text-align=right;">RM<?php echo $row['price']?></a>
-                </div>
+    <div class="card">
+    <div class="card-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">Shop__name</div>
+                <div class="col-md-6 col-lg-4 offset-lg-2">Purchased__Date</div>
             </div>
-            <?php
-             }?>                       
         </div>
-        
     </div>
+    <div class="card-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-lg-1">Product__Image</div>
+                <div class="col-md-3 col-lg-2">Product__Name</div>
+                <div class="col-md-3 col-lg-1 offset-lg-1">Product_Qty</div>
+                <div class="col-md-3 offset-lg-1">Product_Variant</div>
+                <div class="col-lg-2 offset-lg-1">Product_Price</div>
+            </div>
+        </div>
+    </div>
+</div>
    <!-- /.container-fluid -->
 
 
