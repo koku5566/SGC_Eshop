5bc44d5e2e987d2f8aac4ac61a7eaa07e79da31f
Maverick <maverickleeweilin88@gmail.com>
2022-03-31 19:13:07 +0800

    Update header.php

--- seller/header.php
+++ seller/header.php
@@ -91,6 +91,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" charset="utf-8"></script>
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
