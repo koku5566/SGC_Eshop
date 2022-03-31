0eb4b3ddf75c468009ca4208c396dedd30611b17
Maverick <maverickleeweilin88@gmail.com>
2022-03-31 15:05:59 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -1776,3 +1776,10 @@
     require __DIR__ . '/footer.php'
 ?>
 
+<!-- Page level plugins -->
+<script src="vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="js/demo/datatables-demo.js"></script>
+
