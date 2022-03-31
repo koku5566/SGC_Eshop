5bdc2d1489460ac93822c68a5823d370aa0943a2
Maverick <maverickleeweilin88@gmail.com>
2022-03-31 21:31:40 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -654,50 +654,50 @@
     }
     
     $(function(){
-        var numberOfItems = $(".card-content .card").length;
+        var numberOfItems = $(".card-content .product-item").length;
         var limitPerPage = 3; //How many card items visible per a page
         var totalPages = Math.ceil(numberOfItems / limitPerPage);
         var paginationSize = 7; //How many page elements visible in the pagination
         var currentPage;
     
         function showPage(whichPage){
-        if(whichPage < 1 || whichPage > totalPages) return false;
-    
-        currentPage = whichPage;
-    
-        $(".card-content .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
-    
-        $(".pagination li").slice(1, -1).remove();
-    
-        getPageList(totalPages, currentPage, paginationSize).forEach(item => {
-            $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
-            .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
-            .attr({href: "javascript:void(0)"}).text(item || "...")).insertBefore(".next-page");
-        });
-    
-        $(".previous-page").toggleClass("disabled", currentPage === 1);
-        $(".next-page").toggleClass("disabled", currentPage === totalPages);
-        return true;
-        }
-    
-        $(".pagination").append(
-        $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Prev")),
-        $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Next"))
-        );
-    
-        $(".card-content").show();
-        showPage(1);
-    
-        $(document).on("click", ".pagination li.current-page:not(.active)", function(){
-        return showPage(+$(this).text());
-        });
-    
-        $(".next-page").on("click", function(){
-        return showPage(currentPage + 1);
-        });
-    
-        $(".previous-page").on("click", function(){
-        return showPage(currentPage - 1);
+            if(whichPage < 1 || whichPage > totalPages) return false;
+        
+            currentPage = whichPage;
+        
+            $(".card-content .product-item").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+        
+            $(".pagination li").slice(1, -1).remove();
+        
+            getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+                $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
+                .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
+                .attr({href: "javascript:void(0)"}).text(item || "...")).insertBefore(".next-page");
+            });
+        
+            $(".previous-page").toggleClass("disabled", currentPage === 1);
+            $(".next-page").toggleClass("disabled", currentPage === totalPages);
+            return true;
+            }
+        
+            $(".pagination").append(
+            $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Prev")),
+            $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Next"))
+            );
+        
+            $(".card-content").show();
+            showPage(1);
+        
+            $(document).on("click", ".pagination li.current-page:not(.active)", function(){
+            return showPage(+$(this).text());
+            });
+        
+            $(".next-page").on("click", function(){
+            return showPage(currentPage + 1);
+            });
+        
+            $(".previous-page").on("click", function(){
+            return showPage(currentPage - 1);
         });
     });
 </script>
