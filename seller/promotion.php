ad28c80f3957fecf9a64291adc9122898d3a0fc3
Qxlim12 <qxionglim@yahoo.com>
2022-04-01 16:13:43 +0800

    u

--- seller/promotion.php
+++ seller/promotion.php
@@ -14,36 +14,18 @@
                         <table class="table table-hover">
                             <thead>
                                 <tr>
-                                <th scope="col"></th>
                                 <th scope="col">Promotion Title</th>
                                 <th scope="col">Date/Time</th>
                                 <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <!--<tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                                </tr>
-                                <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                                </tr> -->
                                 <?php
                                     $sql = "SELECT promotion_id, promotion_title, promotion_period from promotion";
                                     $result = $conn->query($sql); 
                                     if($result-> num_rows > 0){
                                          while($row = $result->fetch_assoc()){
-                                             echo"<tr><td>".$row["promotion_id"]."</td><td>"
+                                             echo"<tr><td>"
                                              .$row["promotion_title"]."</td><td>".$row["promotion_period"]."</td></tr>";
                                          }
                                          echo"</table>";
