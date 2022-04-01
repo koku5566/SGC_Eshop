976fdb5c927d5ca3d2c19faf175e6cff63531c39
Maverick <maverickleeweilin88@gmail.com>
2022-04-01 15:36:50 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -342,7 +342,7 @@
                                                                 while($row_1 = mysqli_fetch_assoc($result_1)) {
                                                                     
                                                                     echo("
-                                                                        <div class=\"col-xl-2 col-lg-2 col-sm-3 product-item\" style=\"padding-bottom: .625rem;\">
+                                                                        <div class=\"col-xl-2 col-lg-4 col-sm-6 product-item\" style=\"padding-bottom: .625rem;\">
                                                                             <a data-sqe=\"link\" href=\"".$row_1['product_id']."\">
                                                                                 <div class=\"card\">
                                                                                     <div class=\"image-container\">
@@ -454,7 +454,7 @@
                                                                 while($row_1 = mysqli_fetch_assoc($result_1)) {
                                                                     
                                                                     echo("
-                                                                        <div class=\"col-xl-2 col-lg-2 col-sm-3 product-item\" style=\"padding-bottom: .625rem;\">
+                                                                        <div class=\"col-xl-2 col-lg-4 col-sm-6 product-item\" style=\"padding-bottom: .625rem;\">
                                                                             <a data-sqe=\"link\" href=\"".$row_1['product_id']."\">
                                                                                 <div class=\"card\">
                                                                                     <div class=\"image-container\">
