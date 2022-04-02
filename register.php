e9af560c5f3c05d2c8c2715149a4a599fe389500
Maverick <maverickleeweilin88@gmail.com>
2022-04-02 15:36:34 +0800

    Update register.php

--- register.php
+++ register.php
@@ -28,7 +28,7 @@ if(isset($_POST['signup']))
 
 					//Save new user
 					$sql  = "INSERT INTO user (userID, username, email, password, name, registration_date, role)
-					VALUES ((SELECT CONCAT('U',(SELECT LPAD((SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'user'), 6, 0))) AS newUserId),'$email','$password','$username','$date','USER')";
+					VALUES ((SELECT CONCAT('U',(SELECT LPAD((SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'user'), 6, 0))) AS newUserId),'$username','$email','$password','$username','$date','USER')";
 					if(mysqli_query($conn, $sql))
 					{
 						//Save new shop
