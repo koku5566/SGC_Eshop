e1a9118588e81a1f3b9fbb6610f791b329233894
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-04-04 16:27:55 +0800

    Update facilityManagement.php

--- facilityManagement.php
+++ facilityManagement.php
@@ -61,14 +61,10 @@
                </div>
             </div>
 
-            <div class="row">
-               <div class="col-xl-2 col-lg-2 col-sm-12">
-                  <p class="p-title">Hourly Rate</p>
-               </div>
-               <div class="col-xl-10 col-lg-10 col-sm-12">
-                  <div class="input-group mb-3">
-                     <input type="number"min="0" value="0" class="form-control" name="productStock" required>
-                  </div>
+         <div class="form-row">
+               <div class="form-group col-md-12">
+                  <label for="">Hourly Rate</label>
+                  <input type="text" aria-label="Hourly rate" class="form-control">
                </div>
             </div>
          
