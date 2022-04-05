5bf02a6d8bcf8b5579ffd9fa787a784bcb87ab33
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-04-05 18:42:00 +0800

    Update helpCenterAdmin.php

--- helpCenterAdmin.php
+++ helpCenterAdmin.php
@@ -7,10 +7,10 @@
 <?php
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['rrsub']) && $_POST['rrsub'] === 'Submit'){
 		
-		$ss1 = $_FILES['img']['name'][0];
-		$ss2 = $_FILES['img']['name'][1];
+		$ss1 = $_FILES['img']['name'];
 		
-		echo "$ss1 || $ss2";
+		
+		echo "$ss1 || sohai";
 		
 	}
    
