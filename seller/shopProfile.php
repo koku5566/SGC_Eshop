ae9071a8fbac03eb01a417aa83f8d78cf3def573
Marcheal <marchealyong@gmail.com>
2022-04-07 20:53:39 +0800

    Update shopProfile.php

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -149,7 +149,7 @@ div.absolute {
   height: 70px;
   box-shadow: 0 0 10px 0 rgba(255, 255, 255, 0.35);
   border-radius: 100px;
-  right: 150px;
+  right: 350px;
 }
 .profile-pic .-label {
   position: absolute;
