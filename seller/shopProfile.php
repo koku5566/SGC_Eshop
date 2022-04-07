15140f51d4da91529877c433726d99f6a4f8d695
Marcheal <marchealyong@gmail.com>
2022-04-07 17:18:32 +0800

    update

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -3,51 +3,52 @@
 ?>
 
 <?php
-    //if(isset($_POST['submit'])){
-    //  if(!empty($_POST['coverPhoto']) && !empty($_POST['profileImage']) && !empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['imageVideo'])){
-//
-    //    $coverPhoto = $_POST['coverPhoto'];
-    //    $profileImage = $_POST['profileImage'];
-    //    $name = $_POST['name'];
-    //    $description = $_POST['description'];
-    //    $imageVideo = $_POST['imageVideo'];
-//
-    //    $query = "INSERT INTO shopProfile(shop_profile_cover,shop_profile_image,shop_name,shop_description, shop_media) VALUES ('$coverPhoto','$profileImage','$name','$description','$imageVideo')";
-//
-    //    $run = mysqli_query($conn,$query);
-//
-    //    if($run){
-    //      echo "Form Submitted Successfully" ;
-    //    }
-    //    else{
-    //      echo "Form not submitted";
-    //    }
-//
-    //  }
-    //  else{
-    //    echo "all fields required";
-    //  }
-    //}
     if(isset($_POST['submit'])){
-      //$coverPhoto = $_POST['coverPhoto'];
-      //$profileImage = $_POST['profileImage'];
-      $name = $_POST['name'];
-      $description = $_POST['description'];
-      //$imageVideo = $_POST['imageVideo'];
+      if(!empty($_POST['coverPhoto']) && !empty($_POST['profileImage']) && !empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['imageVideo'])){
 
+        $coverPhoto = $_POST['coverPhoto'];
+        $profileImage = $_POST['profileImage'];
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $imageVideo = $_POST['imageVideo'];
 
-    echo"hello";
-    $sql = "INSERT INTO shopProfile (shop_name, shop_description) /*(shop_profile_cover, shop_profile_image, shop_name, shop_description, shop_media)*/
-    VALUES ('$name', '$description')"; /* ($coverPhoto, '$profileImage', '$name', '$description' , '$imageVideo') */
-    
-    if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully";
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
+        $query = "INSERT INTO shopProfile(shop_profile_cover,shop_profile_image,shop_name,shop_description, shop_media) VALUES ('$coverPhoto','$profileImage','$name','$description','$imageVideo')";
+
+        $run = mysqli_query($conn,$query);
+
+        if($run){
+          echo "Form Submitted Successfully" ;
+        }
+        else{
+          echo "Form not submitted";
+        }
+
+      }
+      else{
+        echo "all fields required";
+      }
     }
-    
-    $conn->close();
-  }
+
+    //if(isset($_POST['submit'])){
+    //  //$coverPhoto = $_POST['coverPhoto'];
+    //  //$profileImage = $_POST['profileImage'];
+    //  $name = $_POST['name'];
+    //  $description = $_POST['description'];
+    //  //$imageVideo = $_POST['imageVideo'];
+//
+//
+    //echo"hello";
+    //$sql = "INSERT INTO shopProfile (shop_name, shop_description) /*(shop_profile_cover, shop_profile_image, shop_name, shop_description, shop_media)*/
+    //VALUES ('$name', '$description')"; /* ($coverPhoto, '$profileImage', '$name', '$description' , '$imageVideo') */
+    //
+    //if ($conn->query($sql) === TRUE) {
+    //  echo "New record created successfully";
+    //} else {
+    //  echo "Error: " . $sql . "<br>" . $conn->error;
+    //}
+    //
+    //$conn->close();
+    //}
 ?>
 
 <!-- Icon -->
