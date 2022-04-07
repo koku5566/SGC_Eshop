119f0a317c6064c05933c4c551fdccdae4a9803b
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-04-07 22:23:16 +0800

    Update helpCenterAdminTest.php

--- helpCenterAdminTest.php
+++ helpCenterAdminTest.php
@@ -46,7 +46,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['pid']) && !empty($_PO
 	<i class="fa fa-star ratingStar"></i>
     
   <i class="fa fa-star-half-o tqy" sytle = "font-size: 1.2rem;"></i>
- 
+  
+  <i class="fa fa-star-half-o" aria-hidden="true"></i>
+  <i class="fa fa-star-half-o" aria-hidden="true"></i>
+  <i class="fa fa-star-half-o tqy" aria-hidden="true"></i>
    
 
 
