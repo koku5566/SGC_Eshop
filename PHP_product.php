acd4b250f365a54a4924bdca72ecc7608e842512
Maverick <maverickleeweilin88@gmail.com>
2022-04-08 20:02:01 +0800

    a

--- PHP_product.php
+++ PHP_product.php
@@ -9,12 +9,14 @@
         $sql_product = $_POST['query'];
         $result_product = mysqli_query($conn, $sql_product);
 
-        if (mysqli_num_rows($result_product) > 0) {
+        if (mysqli_num_rows($result_product) == 1) {
             while($row_product = mysqli_fetch_assoc($result_product)) {
                 $return_arr[] = array("price" => $row_product['product_price'],"stock" => $row_product['product_stock']);
             }
             echo json_encode($return_arr);
         }
-        echo json_encode("");
+        else{
+            echo json_encode("");
+        }
     }
 ?>
