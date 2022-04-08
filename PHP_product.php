5cc30d0812ff8ad96b7ad2d6290ddc148845f8a4
Maverick <maverickleeweilin88@gmail.com>
2022-04-08 19:02:08 +0800

    Update PHP_product.php

--- PHP_product.php
+++ PHP_product.php
@@ -1,15 +1,18 @@
 <?php
     require_once __DIR__ . '/mysqli_connect.php';
 
-    //Fetch each product information
-    $sql_product = $_POST['query'];
-    $result_product = mysqli_query($conn, $sql_product);
+    if(isset($_POST['query']))
+    {
+        //Fetch each product information
+        $sql_product = $_POST['query'];
+        $result_product = mysqli_query($conn, $sql_product);
 
-    if (mysqli_num_rows($result_product) > 0) {
-        while($row_product = mysqli_fetch_assoc($result_product)) {
-            $return_arr[] = array("price" => $row_product['product_price'],"stock" => $row_product['product_stock']);
+        if (mysqli_num_rows($result_product) > 0) {
+            while($row_product = mysqli_fetch_assoc($result_product)) {
+                $return_arr[] = array("price" => $row_product['product_price'],"stock" => $row_product['product_stock']);
+            }
+            echo json_encode($return_arr);
         }
-        echo json_encode($return_arr);
+        echo json_encode("");
     }
-    echo json_encode("");
 ?>
