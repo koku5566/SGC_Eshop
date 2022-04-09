9af6ef4712f34c64753130c6dfc7c2c1effa3169
Maverick <maverickleeweilin88@gmail.com>
2022-04-09 18:08:12 +0800

    Update product.php

--- product.php
+++ product.php
@@ -643,15 +643,12 @@
                 </div>
                 <!-- /.container-fluid -->
 
+
 <div class="modal fade" id="MsgModel" tabindex="-1" role="dialog" aria-labelledby="MsgModel" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered" role="document">
-		<div class="modal-content">
-			<div class="modal-body">
-				<div class="row">
-					<img src="/img/resource/check.png" style="width: 80px;"/>
-				</div>
-			</div>
-			<div class="modal-footer">
+		<div id="SuccessMsg" hidden>
+			<div class="SuccessMsg-content">
+				<img src="/img/resource/check.png" style="width: 80px;"/>
 				<p style="color: white;">Item has been added to your shopping cart</p>
 			</div>
 		</div>
