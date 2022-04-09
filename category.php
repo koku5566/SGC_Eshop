b1a7d88f43cac48c3e8b58b2f710693a4a3438c8
Maverick <maverickleeweilin88@gmail.com>
2022-04-09 12:22:35 +0800

    Update category.php

--- category.php
+++ category.php
@@ -133,15 +133,12 @@
                                                                         //Make it Active
                                                                         if(isset($_SESSION['subCategory']) && $_SESSION['subCategory'] == $subCategoryId)
                                                                         {
-                                                                            echo("yay");
-                                                                            //echo("<i class=\"fa fa-caret-right\" style=\"padding: .3rem 0 0px;font-size: larger;color:#a31f37;\"></i>");
-                                                                            //echo("<a href=\"category.php?subCategory=$subCategoryId\" class=\"dropdown-item\" style=\"padding-left: 0.9rem;\">$subCategoryName</a>");
+                                                                            echo("<i class=\"fa fa-caret-right\" style=\"padding: .3rem 0 0px;font-size: larger;color:#a31f37;\"></i>");
+                                                                            echo("<a href=\"category.php?subCategory=$subCategoryId\" class=\"dropdown-item\" style=\"padding-left: 0.9rem;\">$subCategoryName</a>");
                                                                         }
-                                                                        else(
-                                                                            echo("sad");
-                                                                            //echo("<a href=\"category.php?subCategory=$subCategoryId\" class=\"dropdown-item\">$subCategoryName</a>");
-                                                                        )
-                                                                        
+                                                                        else{
+                                                                            echo("<a href=\"category.php?subCategory=$subCategoryId\" class=\"dropdown-item\">$subCategoryName</a>");
+                                                                        }
                                                                         echo("</li>");
                                                                         
                                                                     }
