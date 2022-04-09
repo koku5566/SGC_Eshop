f39c24c3e9f24dd74554a12de3c7fca845ecb519
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-09 14:50:40 +0800

    Update viewShippingOrders.php

--- seller/viewShippingOrders.php
+++ seller/viewShippingOrders.php
@@ -172,6 +172,37 @@ $result = $stmt->get_result();
                             <!--------------------------------To ship--------------------------------------->
                             <div class="tab-pane fade" id="toship" role="tabpanel" aria-labelledby="toship-tab">
                             AIYA WHY X BLH?
+                            <div class="tab-pane fade" id="toship" role="tabpanel" aria-labelledby="    toship-tab">
+                                <div class="tab-content" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="ship" role="tabpanel" aria-labelledby="ship-tab">
+                                    <?php                       
+                                    while ($row = $result_3->fetch_assoc()) {
+                                    ?>
+                                    <div class="body">
+                                        <div class="row">
+                                            <div class="col md-auto text-start"><img src=" https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1644969385433" style="width:100%; height:100%;object-fit:contain"/>
+                                            </div>
+                                            <div class="col md-auto text-start " name="product_name"><?php echo $row['product_name']?>
+                                                
+                                            </div>
+                                            <div class="col md-auto text-center offset-md-3" name="price">RM <?php echo $row['price']?>.00
+                                            </div>
+                                            <div class="col-2" name="order_id"><?php echo $row['order_id']?>
+                                                
+                                            </div>
+                                            <div class="col-2" style="color:red;" name="order_status"><?php echo $row['order_status']?>
+                                                
+                                            </div>
+                                            <div class="col-2">
+                                                <button onclick="generate()"><a href="#" data-bs-target="#modal-1" data-bs-toggle="modal">Arrange Shipment</a>    </button>
+                                            </div>
+                                        </div>
+                                    <?php 
+                                    }?>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
                                 
                             </div>
                             <!--------------------------------Pick Up--------------------------------------->
