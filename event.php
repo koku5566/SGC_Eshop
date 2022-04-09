1517f4ef8ff5726b37aff1a2983d431523a5978c
koku5566 <koku5566@gmail.com>
2022-04-10 07:53:02 +0800

    Update event.php

--- event.php
+++ event.php
@@ -17,18 +17,19 @@
             if (mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_assoc($result)) {
 
-                    echo("
-                    <div class=\"col-sm-3\" style=\"margin-top: 20px;margin-bottom: 20px;\">
-                        <div class=\"card\">
-                            <div class=\"card-body\"><img src=\"data:".stripslashes($row['cover_image_type']).";base64,".base64_encode($row['cover_image'])."\"/>
-                                <h3 class=\"card-title\" style=\"margin-top: 10px;\">Start your E-Commerce with Shopee!</h3>
-                                <h1 style=\"color: rgb(163, 31, 55);font-size: 20px;\">Online</h1>
-                                <h5 style=\"font-size: 20px;margin-bottom: 6px;margin-top: 19px;\">Date: 6th October 2021</h5>
-                                <h4 style=\"font-size: 20px;\">Organizer: SEGi Group of Colleges</h4><button class=\"btn btn-primary float-end\" type=\"button\" style=\"margin-top: 5px;background: rgb(163, 31, 55);padding-right: 25px;padding-left: 25px;\">Free</button>
-                            </div>
-                        </div>
-                    </div>
-                    ");
+                    // echo("
+                    // <div class=\"col-sm-3\" style=\"margin-top: 20px;margin-bottom: 20px;\">
+                    //     <div class=\"card\">
+                    //         <div class=\"card-body\"><img src=\"data:".stripslashes($row['cover_image_type']).";base64,".base64_encode($row['cover_image'])."\"/>
+                    //             <h3 class=\"card-title\" style=\"margin-top: 10px;\">Start your E-Commerce with Shopee!</h3>
+                    //             <h1 style=\"color: rgb(163, 31, 55);font-size: 20px;\">Online</h1>
+                    //             <h5 style=\"font-size: 20px;margin-bottom: 6px;margin-top: 19px;\">Date: 6th October 2021</h5>
+                    //             <h4 style=\"font-size: 20px;\">Organizer: SEGi Group of Colleges</h4><button class=\"btn btn-primary float-end\" type=\"button\" style=\"margin-top: 5px;background: rgb(163, 31, 55);padding-right: 25px;padding-left: 25px;\">Free</button>
+                    //         </div>
+                    //     </div>
+                    // </div>
+                    // ");
+                    echo '<img src="data:image/jpeg;base64,'.base64_encode($row['cover_image']).'" height = "100" width="100"/>';
                 }
             }
         ?>
