2ca45059306bff68a6700d6f88e3607613e6a581
Qxlim12 <qxionglim@yahoo.com>
2022-04-10 17:21:42 +0800

    center2

--- seller/promotion.php
+++ seller/promotion.php
@@ -29,11 +29,11 @@
                                              .$row["promotion_title"]."</td><td>"."Start:  "
                                              .$row["promotion_Date"]."<br>"."End:   "
                                              .$row["promotionEnd_Date"]."</td>
-                                             <td>
-                                             <div class=\"col-xl-6 text-center\" style=\"padding:0;\">
+                                             <td class=\"text-center\">
+                                             <div class=\"col-xl-6\" style=\"padding:0;\">
                                                 <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?edit=".$row_1['promotion_id']."\" ><i class=\"fa fa-edit \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Edit</a>
                                                 </div>
-                                             <div class=\"col-xl-6 text-center\"  style=\"padding:0;\">
+                                             <div class=\"col-xl-6\" style=\"padding:0;\">
                                                 <a class=\"btn btn-outline-danger\" style=\"border:none;width:100%;\" href=\"?delete=".$row_1['promotion_id']."\" ><i class=\"fa fa-trash \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Delete</a>
                                                 </div>
                                              </td></tr>";
