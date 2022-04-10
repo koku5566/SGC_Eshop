36c664ad976821f6a4322ff9e3660e3949d0b3aa
koku5566 <koku5566@gmail.com>
2022-04-11 03:07:48 +0800

    Update eventSellerDashboard.php

--- seller/eventSellerDashboard.php
+++ seller/eventSellerDashboard.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="assets/css/styles.css">
     
     <div style="margin-left: 80px;margin-right: 80px;height: 181.8px;">
-        <div class="text-end" style="margin-top: 20px;"><button class="btn btn-primary" id="createEventBtn" type="button" style="background: rgb(163, 31, 55);width: 121.75px;height: 47px;">Create Event</button></div>
+        <div class="text-end" style="margin-top: 20px; text-align:right;"><a href="https://eshop.sgcprototype2.com/seller/createEvent.php"><button class="btn btn-primary" id="createEventBtn" type="button" style="background: rgb(163, 31, 55);width: 121.75px;height: 47px;">Create Event</button></a></div>
         <h4>Search</h4><input type="search" style="width: 100%;">
         <div>
             <div class="btn-group float-end" role="group" style="margin-top: 7px;"><button class="btn btn-primary" type="button" style="margin-right: 0px;margin-left: 10px;">Reset</button><button class="btn btn-primary" type="button" style="margin-right: 0px;margin-left: 10px;background: rgb(163, 31, 55);">Search</button></div>
@@ -27,29 +27,161 @@
         <div class="tab-content">
             <div class="tab-pane active" role="tabpanel" id="tab-1">
                 <div style="margin-left: 80px;margin-right: 80px;">
-                    <div class="card" style="margin-top: 20px;">
-                        <div class="card-body shadow">
-                            <h4 class="card-title">Move IT! Malaysia Calories Burn Challenge</h4>
-                            <h6 class="text-muted card-subtitle mb-2">Approved</h6>
-                            <h5>Date: 14 Sep 2021 - 10 Oct 2021</h5>
-                            <h5>Price: RM 20 - RM 180</h5><button class="btn btn-primary float-end" type="button" style="background: rgb(163, 31, 55);width: 164.5px;">Dashboard</button>
-                        </div>
-                    </div>
-                    <div class="card" style="margin-top: 20px;">
-                        <div class="card-body shadow">
-                            <h4 class="card-title">Future of Work</h4>
-                            <h6 class="text-muted card-subtitle mb-2">Approved</h6>
-                            <h5>Date: 14 Sep 2021 - 10 Oct 2021</h5>
-                            <h5>Price: RM 20 - RM 180</h5><button class="btn btn-primary float-end" type="button" style="background: rgb(163, 31, 55);width: 164.5px;">Dashboard</button>
-                        </div>
-                    </div>
+                    <?php
+                        $sql = "SELECT * FROM `event` INNER JOIN `user` ON `organiser_id` = `user_id` WHERE `organiser_id` = 1";
+                        $result = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($result) > 0) {
+                            while($row = mysqli_fetch_assoc($result)) {
+                                $eventID = $row['event_id'];
+                                $sql1 = "SELECT * FROM `event` INNER JOIN `user` ON `organiser_id` = `user_id` INNER JOIN `ticketType` ON `event`.`event_id` = `ticketType`.`event_id` WHERE `event`.`event_id` = $eventID";
+                                $result1 = mysqli_query($conn, $sql1);
+                                $minPrice = 999999;
+                                $maxPrice = 0;
+                                $buttonPrice = "";
+
+                                if (mysqli_num_rows($result1) > 0) {
+                                    while($row1 = mysqli_fetch_assoc($result1)) {
+                                        if($row1['price'] >= $maxPrice)
+                                        {
+                                            $maxPrice = $row1['price'];
+                                        }
+                                        if($row1['price'] <= $minPrice)
+                                        {
+                                            $minPrice = $row1['price'];
+                                        }
+                                    
+                                    }
+                                }
+                                if($maxPrice == 0)
+                                {
+                                    $buttonPrice = "Free";
+                                }
+                                else
+                                {
+                                    $buttonPrice = "RM ".$minPrice . " - RM " . $maxPrice;
+                                }
+                                echo("
+                                    <div class=\"card\" style=\"margin-top: 20px;\">
+                                        <div class=\"card-body shadow\">
+                                            <h4 class=\"card-title\">".$row['event_name']."</h4>
+                                            <h6 class=\"text-muted card-subtitle mb-2\">".$row['status']."</h6>
+                                            <h5>Date: ".$row['event_date']." - ".$row['eventEnd_date']."</h5>
+                                            <h5>Price: $buttonPrice</h5><button class=\"btn btn-primary float-end\" type=\"button\" style=\"background: rgb(163, 31, 55);width: 164.5px;\">Dashboard</button>
+                                        </div>
+                                    </div>
+                                ");
+                            }
+                        }
+
+                    ?>
+                    
                 </div>
             </div>
             <div class="tab-pane" role="tabpanel" id="tab-2">
-                <p>Content for tab 2.</p>
+                <div style="margin-left: 80px;margin-right: 80px;">
+                    <?php
+                        $sql = "SELECT * FROM `event` INNER JOIN `user` ON `organiser_id` = `user_id` WHERE `organiser_id` = 1";
+                        $result = mysqli_query($conn, $sql);
+                        if($row['event_date'] >= date("Y-m-d"))
+                        {
+                            if (mysqli_num_rows($result) > 0) {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    $eventID = $row['event_id'];
+                                $sql1 = "SELECT * FROM `event` INNER JOIN `user` ON `organiser_id` = `user_id` INNER JOIN `ticketType` ON `event`.`event_id` = `ticketType`.`event_id` WHERE `event`.`event_id` = $eventID";
+                                $result1 = mysqli_query($conn, $sql1);
+                                $minPrice = 999999;
+                                $maxPrice = 0;
+                                $buttonPrice = "";
+
+                                if (mysqli_num_rows($result1) > 0) {
+                                    while($row1 = mysqli_fetch_assoc($result1)) {
+                                        if($row1['price'] >= $maxPrice)
+                                        {
+                                            $maxPrice = $row1['price'];
+                                        }
+                                        if($row1['price'] <= $minPrice)
+                                        {
+                                            $minPrice = $row1['price'];
+                                        }
+                                    
+                                    }
+                                }
+                                if($maxPrice == 0)
+                                {
+                                    $buttonPrice = "Free";
+                                }
+                                else
+                                {
+                                    $buttonPrice = "RM ".$minPrice . " - RM " . $maxPrice;
+                                }
+                                echo("
+                                    <div class=\"card\" style=\"margin-top: 20px;\">
+                                        <div class=\"card-body shadow\">
+                                            <h4 class=\"card-title\">".$row['event_name']."</h4>
+                                            <h6 class=\"text-muted card-subtitle mb-2\">".$row['status']."</h6>
+                                            <h5>Date: ".$row['event_date']." - ".$row['eventEnd_date']."</h5>
+                                            <h5>Price: $buttonPrice</h5><button class=\"btn btn-primary float-end\" type=\"button\" style=\"background: rgb(163, 31, 55);width: 164.5px;\">Dashboard</button>
+                                        </div>
+                                    </div>
+                                ");
+                                }
+                            }
+                        }
+                    ?>
+                </div>
             </div>
             <div class="tab-pane" role="tabpanel" id="tab-3">
-                <p>Content for tab 3.</p>
+            <div style="margin-left: 80px;margin-right: 80px;">
+                    <?php
+                        $sql = "SELECT * FROM `event` INNER JOIN `user` ON `organiser_id` = `user_id` WHERE `organiser_id` = 1";
+                        $result = mysqli_query($conn, $sql);
+                        if($row['event_date'] < date("Y-m-d"))
+                        {
+                            if (mysqli_num_rows($result) > 0) {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    $eventID = $row['event_id'];
+                                $sql1 = "SELECT * FROM `event` INNER JOIN `user` ON `organiser_id` = `user_id` INNER JOIN `ticketType` ON `event`.`event_id` = `ticketType`.`event_id` WHERE `event`.`event_id` = $eventID";
+                                $result1 = mysqli_query($conn, $sql1);
+                                $minPrice = 999999;
+                                $maxPrice = 0;
+                                $buttonPrice = "";
+
+                                if (mysqli_num_rows($result1) > 0) {
+                                    while($row1 = mysqli_fetch_assoc($result1)) {
+                                        if($row1['price'] >= $maxPrice)
+                                        {
+                                            $maxPrice = $row1['price'];
+                                        }
+                                        if($row1['price'] <= $minPrice)
+                                        {
+                                            $minPrice = $row1['price'];
+                                        }
+                                    
+                                    }
+                                }
+                                if($maxPrice == 0)
+                                {
+                                    $buttonPrice = "Free";
+                                }
+                                else
+                                {
+                                    $buttonPrice = "RM ".$minPrice . " - RM " . $maxPrice;
+                                }
+                                echo("
+                                    <div class=\"card\" style=\"margin-top: 20px;\">
+                                        <div class=\"card-body shadow\">
+                                            <h4 class=\"card-title\">".$row['event_name']."</h4>
+                                            <h6 class=\"text-muted card-subtitle mb-2\">".$row['status']."</h6>
+                                            <h5>Date: ".$row['event_date']." - ".$row['eventEnd_date']."</h5>
+                                            <h5>Price: $buttonPrice</h5><button class=\"btn btn-primary float-end\" type=\"button\" style=\"background: rgb(163, 31, 55);width: 164.5px;\">Dashboard</button>
+                                        </div>
+                                    </div>
+                                ");
+                                }
+                            }
+                        }
+                    ?>
+                </div>
             </div>
         </div>
     </div>
