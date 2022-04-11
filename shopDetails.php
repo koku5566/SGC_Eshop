7fb306d67dc976fc23526b194424d8556b0ec336
Marcheal <marchealyong@gmail.com>
2022-04-11 18:46:57 +0800

    Update shopDetails.php

--- shopDetails.php
+++ shopDetails.php
@@ -56,12 +56,6 @@
         <!--Section: Content-->
         <section class="text-center">
           <h4 class="mb-5"><strong>best Sellers</strong></h4>
-          <div>
-            <?php
-              if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-            ?>
           <div class="row">
             <!--<div class="col-lg-4 col-md-12 mb-4">
               <div class="card">
@@ -85,6 +79,11 @@
             </div>-->
 
             <div class="col-lg-4 col-md-6 mb-4">
+            <?php
+              if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+            ?>
               <div class="card">
                 <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
                   <img
