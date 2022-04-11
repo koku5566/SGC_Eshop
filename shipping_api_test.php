8b5d35ad1cfc93e22f3d145206b25e723e428e91
Carol <chianxin@gmail.com>
2022-04-11 19:49:29 +0800

    Update shipping_api_test.php

--- shipping_api_test.php
+++ shipping_api_test.php
@@ -1,94 +1,9 @@
 <?php 
 
 
-//get seller id -> retrieve seller shipping option from db
-$sellerUID = 11; //*TO GET*
-$customerUID = 3; //TO GET *
+//get seller id -> retrieve seller shipping option from 
+$sellerUID = 11;
 
-$checkoutProduct = array ( //productid, quantity
-    array(000034,2),
-    array(000035,2),
-  );
-
-  $productlength =[];
-  $productwidth = [];
-  $productheight = 0;
-
-
-  foreach($checkoutProduct as $product => $quantity){
-    $sqlinfo = " SELECT product_length, product_width, product_height, product_weight FROM product WHERE id = '$product'";
-    $stmt = $conn->prepare($sqlinfo);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    while ($row = $result->fetch_assoc()) {
-
-    //to calculate parcel size of including all products
-    array_push($productlength, $row['product_length']);
-    array_push($productwidth, $row['product_width']);
-
-    $productheight += $row['product_height'] * $quantity; // Sum (Height (cm) x Quantity)
-    }
-  }
-  $maximumlength = max($productlength);
-  $maximumwidth = max($productwidth);
-  
-  echo $productheight, $maximumlength, $maximumwidth;
-
-
-$sql2 ="SELECT
-id.user,
-contact_name.userAddress,
-phone_number.userAddress,
-address.userAddress,
-postal_code.userAddress,
-area.userAddress,
-state.userAddress,
-country.userAddress
-FROM
-user
-JOIN userAddress ON user.id = userAddress.user_id
-WHERE
-user.id = $customerUID";
-
-$stmt = $conn->prepare($sql2);
-$stmt->execute();
-$result = $stmt->get_result();
-while ($row = $result->fetch_assoc()) {
-$cPhone = $row['phone_number'];
-$cContactName  = $row['contact_name'];
-$cFullAddress = $row['address'];
-$cPostalCode = $row['postal_code'];
-$cState = $row['state'];
-}
-
-
-$sql ="SELECT
-id.user,
-contact_name.userAddress,
-phone_number.userAddress,
-address.userAddress,
-postal_code.userAddress,
-area.userAddress,
-state.userAddress,
-country.userAddress
-FROM
-user
-JOIN userAddress ON user.id = userAddress.user_id
-WHERE
-user.id = $sellerUID";
-
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$result = $stmt->get_result();
-
-while ($row = $result->fetch_assoc()) {
-
-$sPhone = $row['phone_number'];
-$sContactName  = $row['contact_name'];
-$sFullAddress = $row['address'];
-$sPostalCode = $row['postal_code'];
-$sState = $row['state'];
-}
 
 //if get is not null then
 
@@ -99,20 +14,18 @@ $postparam = array(
 'authentication'	=> 'LoFwGSDIZ4',
 'api'	=> 'EP-1ksAmVhmY',
 'bulk'	=> array(
-
-    //l0oop arraay product
 array(
-'pick_code'	=> $sPostalCode,//10050
-'pick_state'	=> $sState,//'png',
+'pick_code'	=> '10050',
+'pick_state'	=> 'png',
 'pick_country'	=> 'MY',
-'send_code'	=> $cPostalCode,//'11950',
-'send_state'	=> $cState,//'png',
+'send_code'	=> '11950',
+'send_state'	=> 'png',
 'send_country'	=> 'MY',
-'weight'	=> '5', //passed from checkout (get product id)
-'width'	=> $maximumwidth,
-'length'	=> $maximumlength,
-'height'	=> $productheight,
-'date_coll'	=> date("Y-m-d"),
+'weight'	=> '5',
+'width'	=> '0',
+'length'	=> '0',
+'height'	=> '0',
+'date_coll'	=> '2022-4-10',
 ),
 
 ),
