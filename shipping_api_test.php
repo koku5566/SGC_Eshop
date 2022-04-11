3cca173037fe7069358f7d57513653b6ce399409
Carol <chianxin@gmail.com>
2022-04-12 01:04:24 +0800

    Update shipping_api_test.php

--- shipping_api_test.php
+++ shipping_api_test.php
@@ -1,5 +1,22 @@
 <?php 
-    require '/mysqli_connect.php';
+  define("HOST","localhost");
+  define("USERNAME","sgcprot1_SGC_ESHOP");
+  define("PASSWORD","3g48B8Qn8k6v6VF");
+  define("DATABASE","sgcprot1_SGC_ESHOP");
+
+  //create database connection
+  $conn = mysqli_connect(HOST,USERNAME,PASSWORD,DATABASE);
+
+  if(!$conn)
+  {
+      die("Connection Failed".mysqli_connect_error());
+  }
+  else
+  {
+      if(!isset($_SESSION)){
+          session_start();
+      }
+  }
 
 //get seller id -> retrieve seller shipping option from db
 $sellerUID = 11; //*TO GET*
