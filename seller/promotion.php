446fa87186bd623061bf47157702aa45453a5dd5
Qxlim12 <qxionglim@yahoo.com>
2022-04-12 21:27:52 +0800

    pp

--- seller/promotion.php
+++ seller/promotion.php
@@ -121,7 +121,7 @@
                                                     <div class="col-xl-10 col-lg-10 col-sm-12">
                                                         <div class="col-sm-5"><input class="form-control" type="date" name="pDate_From" id="promotion_Date" required></div>
                                                             <div class="col-sm-2">
-                                                                <h5 style="text-align: center;margin-top: 6px;">To</h5>
+                                                                <p>To</p>
                                                             </div>
                                                         <div class="col-sm-5"><input class="form-control" type="date" name="pDate_To" id="promotionEnd_Date" required></div>
                                                     </div>
