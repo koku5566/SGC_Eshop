38941abcd6ee13302e3bdb57a21c1aa27c85f9c4
Qxlim12 <qxionglim@yahoo.com>
2022-04-12 21:39:17 +0800

    news

--- seller/promotion.php
+++ seller/promotion.php
@@ -58,132 +58,133 @@
             </div>
         </div>
 
-                    <!-- Create Promotion -->
-                    <div class="row">
-                        <div class="col-xl-12 col-lg-12">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h5 class="m-0 font-weight-bold text-primary">Promotion Details</h5>
+    <!-- Create Promotion -->
+    <div class="row">
+        <div class="col-xl-12 col-lg-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h5 class="m-0 font-weight-bold text-primary">Promotion Details</h5>
+                </div>
+                <div class="card-body">
+                    <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-xl-2 col-lg-2 col-sm-12">
+                                <p class="p-title">Promotion Title</p>
+                            </div>
+                            <div class="col-xl-10 col-lg-10 col-sm-12">
+                                <div class="input-group mb-3">
+                                    <input class="form-control" type="text" required placeholder="Enter ..." name="promotion_Title" required>
                                 </div>
-                                <div class="card-body">
-                                    <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST" enctype="multipart/form-data">
-                                        <div class="row">
-                                            <div class="col-xl-2 col-lg-2 col-sm-12">
-                                                <p class="p-title">Promotion Title</p>
-                                            </div>
-                                            <div class="col-xl-10 col-lg-10 col-sm-12">
-                                                <div class="input-group mb-3">
-                                                    <input class="form-control" type="text" required placeholder="Enter ..." name="promotion_Title" required>
-                                                </div>
-                                            </div>
-                                        </div>
+                            </div>
+                        </div>
 
+                        <div class="row">
+                            <div class="col-xl-2 col-lg-2 col-sm-12">
+                                <p class="p-title">Cover Image</p>
+                            </div>
+                                <div class="row">
+                                    <div class="col-xl-10 col-lg-10 col-sm-12">
                                         <div class="row">
-                                            <div class="col-xl-2 col-lg-2 col-sm-12">
-                                                <p class="p-title">Cover Image</p>
-                                            </div>
-                                                <div class="row">
-                                                    <div class="col-xl-10 col-lg-10 col-sm-12">
-                                                        <div class="row">
-                                                            <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
-                                                                <div class="row" style="margin-right: 0.5rem;margin-left: 0.5rem;">
-                                                                    <div style="padding-bottom: .625rem;display:flex">
-                                                                        <div class="imageDiv">
-                                                                            <div class="image-container">
-                                                                                <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="">
-                                                                                <div class="image-layer">
-                                                                                </div>
-                                                                                <div class="image-tools-delete hide">
-                                                                                    <i class="fa fa-trash image-tools-delete-icon" aria-hidden="true"></i>
-                                                                                </div>
-                                                                                <div class="image-tools-add">
-                                                                                    <label class="custom-file-upload">
-                                                                                        <input type="file" accept=".png,.jpeg,.jpg" name="img[]" id="upload_file" class="imgInp" required>
-                                                                                        <i class="fa fa-plus image-tools-add-icon" aria-hidden="true"></i>
-                                                                                    </label>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
+                                            <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
+                                                <div class="row" style="margin-right: 0.5rem;margin-left: 0.5rem;">
+                                                    <div style="padding-bottom: .625rem;display:flex">
+                                                        <div class="imageDiv">
+                                                            <div class="image-container">
+                                                                <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="">
+                                                                <div class="image-layer">
+                                                                </div>
+                                                                <div class="image-tools-delete hide">
+                                                                    <i class="fa fa-trash image-tools-delete-icon" aria-hidden="true"></i>
+                                                                </div>
+                                                                <div class="image-tools-add">
+                                                                    <label class="custom-file-upload">
+                                                                        <input type="file" accept=".png,.jpeg,.jpg" name="img[]" id="upload_file" class="imgInp" required>
+                                                                        <i class="fa fa-plus image-tools-add-icon" aria-hidden="true"></i>
+                                                                    </label>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
-                                                    </div>
-                                                </div>
-                                        </div> 
-
-                                            <div class="row" style="margin-top: 6px">
-                                                <div class="col-xl-2 col-lg-2 col-sm-12">
-                                                    <p class="p-title">Date</p>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-xl-10 col-lg-10 col-sm-12">
-                                                        <div class="row">
-                                                            <div class="col-sm-5"><input class="form-control" type="date" name="pDate_From" id="promotion_Date" required></div>
-                                                                <div class="col-sm-2">
-                                                                    <p style="margin-top: 6px;">To</p>
-                                                                </div>
-                                                            <div class="col-sm-5"><input class="form-control" type="date" name="pDate_To" id="promotionEnd_Date" required></div>
-                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
+                                        </div>
+                                        <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
                                     </div>
                                 </div>
+                        </div> 
 
-                                <!-- Page Ending -->         
-                                <div class="d-sm-flex align-items-center mb-4" style="justify-content: end;">
-                                    <button class="btn btn-outline-primary" type="submit" name="create_btn" >Submit</button>
+                            <div class="row" style="margin-top: 10px">
+                                <div class="col-xl-2 col-lg-2 col-sm-12">
+                                    <p class="p-title">Date</p>
                                 </div>
-
-                                        <?php
-                                            if($_SERVER['REQUEST_METHOD'] == 'POST' ||isset($_POST['create_btn']))
-                                            {
-                                                $title = mysqli_real_escape_string($conn, SanitizeString($_POST['promotion_Title']));
-                                                $dateStart = mysqli_real_escape_string($conn, SanitizeString($_POST['pDate_From']));
-                                                $dateEnd = mysqli_real_escape_string($conn, SanitizeString($_POST['pDate_To']));
-                                                
-                                                //File upload configuration 
-                                                $fileNames = array_filter($_FILES['img']['name']); 
-                                                $targetDir = dirname(__DIR__, 1)."/img/promotion/"; 
-                                                $allowTypes = array('jpg','png','jpeg');
-
-                                                $fileName = basename($_FILES['img']['name'][0]); 
-                                                $ext = pathinfo($fileName, PATHINFO_EXTENSION);
-                                                $fileName = round(microtime(true) * 1000).".".$ext;
-                                                $targetFilePath = $targetDir.$fileName; 
-                                                // Check whether file type is valid 
-                                                $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
-                                                if(in_array($fileType, $allowTypes)){ 
-                                                    if(move_uploaded_file($_FILES["img"]["tmp_name"][0], $targetFilePath)){ 
-                                                        $sql = "INSERT INTO `promotion` (`promotionID`,`promotion_title`,`promotion_image`, `promotion_Date`, `promotionEnd_Date`) 
-                                                                VALUES((SELECT CONCAT('PR',(SELECT LPAD((SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'promotion'), 6, 0))) AS newCombinationId), '$title','$fileName','$dateStart','$dateEnd')";
-                                                                
-                                                                $result = mysqli_query($conn,$sql);
-
-                                                                if($result)
-                                                                {
-                                                                    echo '<script>alert("Add promotion successfully!")</script>';
-                                                                    ?>
-                                                                        <script type="text/javascript">
-                                                                            window.location.href = window.location.origin + "/seller/promotion.php";
-                                                                        </script>
-                                                                    <?php
-                                                                }
-                                                                else
-                                                                {
-                                                                    echo '<script>alert("Failed")</script>';
-                                                                }
-                                                    }
-                                                }
-                                            }
-                                        ?>
-                                </form>
+                                <div class="row">
+                                    <div class="col-xl-10 col-lg-10 col-sm-12">
+                                        <div class="row">
+                                            <div class="col-sm-5"><input class="form-control" type="date" name="pDate_From" id="promotion_Date" required></div>
+                                                <div class="col-sm-2">
+                                                    <p style="margin-top: 6px;">To</p>
+                                                </div>
+                                            <div class="col-sm-5"><input class="form-control" type="date" name="pDate_To" id="promotionEnd_Date" required></div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
                     </div>
-    <!-- /.container-fluid -->
+                </div>
+
+                <!-- Page Ending -->         
+                <div class="d-sm-flex align-items-center mb-4" style="justify-content: end;">
+                    <button class="btn btn-outline-primary" type="submit" name="create_btn" >Submit</button>
+                </div>
+
+                        <?php
+                            if($_SERVER['REQUEST_METHOD'] == 'POST' ||isset($_POST['create_btn']))
+                            {
+                                $title = mysqli_real_escape_string($conn, SanitizeString($_POST['promotion_Title']));
+                                $dateStart = mysqli_real_escape_string($conn, SanitizeString($_POST['pDate_From']));
+                                $dateEnd = mysqli_real_escape_string($conn, SanitizeString($_POST['pDate_To']));
+                                
+                                //File upload configuration 
+                                $fileNames = array_filter($_FILES['img']['name']); 
+                                $targetDir = dirname(__DIR__, 1)."/img/promotion/"; 
+                                $allowTypes = array('jpg','png','jpeg');
+
+                                $fileName = basename($_FILES['img']['name'][0]); 
+                                $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+                                $fileName = round(microtime(true) * 1000).".".$ext;
+                                $targetFilePath = $targetDir.$fileName; 
+                                // Check whether file type is valid 
+                                $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+                                if(in_array($fileType, $allowTypes)){ 
+                                    if(move_uploaded_file($_FILES["img"]["tmp_name"][0], $targetFilePath)){ 
+                                        $sql = "INSERT INTO `promotion` (`promotionID`,`promotion_title`,`promotion_image`, `promotion_Date`, `promotionEnd_Date`) 
+                                                VALUES((SELECT CONCAT('PR',(SELECT LPAD((SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'sgcprot1_SGC_ESHOP' AND TABLE_NAME = 'promotion'), 6, 0))) AS newCombinationId), '$title','$fileName','$dateStart','$dateEnd')";
+                                                
+                                                $result = mysqli_query($conn,$sql);
+
+                                                if($result)
+                                                {
+                                                    echo '<script>alert("Add promotion successfully!")</script>';
+                                                    ?>
+                                                        <script type="text/javascript">
+                                                            window.location.href = window.location.origin + "/seller/promotion.php";
+                                                        </script>
+                                                    <?php
+                                                }
+                                                else
+                                                {
+                                                    echo '<script>alert("Failed")</script>';
+                                                }
+                                    }
+                                }
+                            }
+                        ?>
+                </form>
+            </div>
+        </div>
+    </div>
+<!-- /.container-fluid -->
+
 <style>
     .image-container{
         width: 368px; 
@@ -255,6 +256,7 @@
         display:none;
     }
 </style>
+
 <script>
 
     initImages();
