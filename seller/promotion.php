a0244cfb8d76225d2840518116bd57a503e703fe
Qxlim12 <qxionglim@yahoo.com>
2022-04-12 21:09:58 +0800

    esr

--- seller/promotion.php
+++ seller/promotion.php
@@ -108,9 +108,10 @@
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
                                                     </div>
                                                 </div>
+                                                <div class="col-xl-10 col-lg-10 col-sm-12"><small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
+</div>
                                         </div> 
 
                                     <div>
