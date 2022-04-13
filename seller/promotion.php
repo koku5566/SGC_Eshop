c07b716aff9666900b1681eb273c11e15e372962
Qxlim12 <qxionglim@yahoo.com>
2022-04-13 13:52:08 +0800

    eeee

--- seller/promotion.php
+++ seller/promotion.php
@@ -57,95 +57,95 @@
             </div>
         </div>
 
-    <!-- Create Promotion -->
-    <div class="row">
-        <div class="col-xl-12 col-lg-12">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h5 class="m-0 font-weight-bold text-primary">Promotion Details</h5>
-                </div>
-                <div class="card-body">
-                    <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST" enctype="multipart/form-data">
-                        <div class="row">
-                            <div class="col-xl-2 col-lg-2 col-sm-12">
-                                <p class="p-title">Promotion Title</p>
-                            </div>
-                            <div class="col-xl-10 col-lg-10 col-sm-12">
-                                <div class="input-group mb-3">
-                                    <input class="form-control" type="text" required placeholder="Enter ..." name="promotion_Title" required>
+        <!-- Create Promotion -->
+        <div class="row">
+            <div class="col-xl-12 col-lg-12">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h5 class="m-0 font-weight-bold text-primary">Promotion Details</h5>
+                    </div>
+                    <div class="card-body">
+                        <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST" enctype="multipart/form-data">
+                            <div class="row">
+                                <div class="col-xl-2 col-lg-2 col-sm-12">
+                                    <p class="p-title">Promotion Title</p>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="row" style="margin-top: 10px">
-                            <div class="col-xl-2 col-lg-2 col-sm-12">
-                                <p class="p-title">Date</p>
-                            </div>
-                            <div class="col-xl-10 col-lg-10 col-sm-12">
-                                <div class="row">
-                                    <div class="col-xl-6 col-lg-6">
-                                        <div class="input-group mb-2">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">Start</span>
-                                            </div>
-                                            <input class="form-control" type="date" name="pDate_From" id="promotion_Date" required>
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-6 col-lg-6">
-                                        <div class="input-group mb-2">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text" id="basic-addon1">End</span>
-                                            </div>
-                                            <input class="form-control" type="date" name="pDate_To" id="promotionEnd_Date" required>
-                                        </div>
+                                <div class="col-xl-10 col-lg-10 col-sm-12">
+                                    <div class="input-group mb-3">
+                                        <input class="form-control" type="text" required placeholder="Enter ..." name="promotion_Title" required>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-xl-2 col-lg-2 col-sm-12">
-                                <p class="p-title">Cover Image</p>
+                            <div class="row" style="margin-top: 10px">
+                                <div class="col-xl-2 col-lg-2 col-sm-12">
+                                    <p class="p-title">Date</p>
+                                </div>
+                                <div class="col-xl-10 col-lg-10 col-sm-12">
+                                    <div class="row">
+                                        <div class="col-xl-6 col-lg-6">
+                                            <div class="input-group mb-2">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="basic-addon1">Start</span>
+                                                </div>
+                                                <input class="form-control" type="date" name="pDate_From" id="promotion_Date" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-6 col-lg-6">
+                                            <div class="input-group mb-2">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="basic-addon1">End</span>
+                                                </div>
+                                                <input class="form-control" type="date" name="pDate_To" id="promotionEnd_Date" required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                                <div class="row">
-                                    <div class="col-xl-10 col-lg-10 col-sm-12">
-                                        <div class="row">
-                                            <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
-                                                <div class="row" style="margin-right: 0.5rem;margin-left: 0.5rem;">
-                                                    <div style="padding-bottom: .625rem;display:flex">
-                                                        <div class="imageDiv">
-                                                            <div class="image-container">
-                                                                <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="">
-                                                                <div class="image-layer">
-                                                                </div>
-                                                                <div class="image-tools-delete hide">
-                                                                    <i class="fa fa-trash image-tools-delete-icon" aria-hidden="true"></i>
-                                                                </div>
-                                                                <div class="image-tools-add">
-                                                                    <label class="custom-file-upload">
-                                                                        <input type="file" accept=".png,.jpeg,.jpg" name="img[]" id="upload_file" class="imgInp" required>
-                                                                        <i class="fa fa-plus image-tools-add-icon" aria-hidden="true"></i>
-                                                                    </label>
+
+                            <div class="row">
+                                <div class="col-xl-2 col-lg-2 col-sm-12">
+                                    <p class="p-title">Cover Image</p>
+                                </div>
+                                    <div class="row">
+                                        <div class="col-xl-10 col-lg-10 col-sm-12">
+                                            <div class="row">
+                                                <div class="col-xl-12 col-lg-12 col-sm-12" style="padding-bottom: .625rem;">
+                                                    <div class="row" style="margin-right: 0.5rem;margin-left: 0.5rem;">
+                                                        <div style="padding-bottom: .625rem;display:flex">
+                                                            <div class="imageDiv">
+                                                                <div class="image-container">
+                                                                    <img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="">
+                                                                    <div class="image-layer">
+                                                                    </div>
+                                                                    <div class="image-tools-delete hide">
+                                                                        <i class="fa fa-trash image-tools-delete-icon" aria-hidden="true"></i>
+                                                                    </div>
+                                                                    <div class="image-tools-add">
+                                                                        <label class="custom-file-upload">
+                                                                            <input type="file" accept=".png,.jpeg,.jpg" name="img[]" id="upload_file" class="imgInp" required>
+                                                                            <i class="fa fa-plus image-tools-add-icon" aria-hidden="true"></i>
+                                                                        </label>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
                                         </div>
-                                        <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
                                     </div>
-                                </div>
-                        </div> 
+                            </div> 
 
-                        
+                            
+                        </div>
                     </div>
-                </div>
 
-                <!-- Page Ending -->         
-                <div class="d-sm-flex align-items-center mb-4" style="justify-content: end;">
-                    <button class="btn btn-outline-primary" type="submit" name="create_btn" >Submit</button>
-                </div>
+                    <!-- Page Ending -->         
+                    <div class="d-sm-flex align-items-center mb-4" style="justify-content: end;">
+                        <button class="btn btn-outline-primary" type="submit" name="create_btn" >Submit</button>
+                    </div>
 
                         <?php
                             if($_SERVER['REQUEST_METHOD'] == 'POST' ||isset($_POST['create_btn']))
