faa32292154c0c189b0a5fd6b2f03f75cec9f0f3
Qxlim12 <qxionglim@yahoo.com>
2022-04-13 13:26:34 +0800

    qwwer

--- seller/promotion.php
+++ seller/promotion.php
@@ -84,16 +84,16 @@
                             </div>
                             <div class="col-xl-10 col-lg-10 col-sm-12">
                                 <div class="row">
-                                    <div class="col-xl-4 col-lg-4">
-                                        <div class="input-group mb-3">
+                                    <div class="col-xl-6 col-lg-6">
+                                        <div class="input-group mb-2">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">Start</span>
                                             </div>
                                             <input class="form-control" type="date" name="pDate_From" id="promotion_Date" required>
                                         </div>
                                     </div>
-                                    <div class="col-xl-4 col-lg-4">
-                                        <div class="input-group mb-3">
+                                    <div class="col-xl-6 col-lg-6">
+                                        <div class="input-group mb-2">
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1">End</span>
                                             </div>
