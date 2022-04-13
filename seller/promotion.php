fa35394942c4b7705e4fa356fa89e23150a98e6e
Qxlim12 <qxionglim@yahoo.com>
2022-04-13 17:13:17 +0800

    jhw

--- seller/promotion.php
+++ seller/promotion.php
@@ -135,12 +135,13 @@
                                     </div>
                                 </div>
                             </div> 
-                              <div class="auto">                      
+
+                              <div class="row">                      
                             <div class="col-xl-2 col-lg-2 col-sm-12">
                                     <p class="p-title">Date</p>
                                 </div>
                                 <div class="row">
-                                    <div class="col-xl-10 col-lg-10 col-sm-12" style="flex=auto">
+                                    <div class="col-xl-10 col-lg-10 col-sm-12">
                                         <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
                                     </div>
                                 </div>
