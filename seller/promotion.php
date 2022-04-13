02836ed92d62768399eb29eaded5b4cdb457d6ea
Qxlim12 <qxionglim@yahoo.com>
2022-04-13 14:34:38 +0800

    wqwqw

--- seller/promotion.php
+++ seller/promotion.php
@@ -134,11 +134,15 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-xl-2 col-lg-2 col-sm-12">
-                                    
+                                <div class="row">
+                                    <div class="col-xl-2 col-lg-2 col-sm-12">
+                                        <p class="p-title"> </p>
+                                    </div>
                                 </div>
-                                <div class="col-xl-10 col-lg-10 col-sm-12">
-                                    <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
+                                <div class="row">
+                                    <div class="col-xl-10 col-lg-10 col-sm-12">
+                                        <small class="text-muted m-2">This image should be landscape. Recommended image size in ratio 16:9. (Example: 1920 x 1080)</small>
+                                    </div>
                                 </div>
                             </div> 
 
