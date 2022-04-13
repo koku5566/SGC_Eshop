b35f2d14051a57bb7e79bee14917959747e2d6ef
Qxlim12 <qxionglim@yahoo.com>
2022-04-14 00:22:44 +0800

    upup

--- seller/promotion.php
+++ seller/promotion.php
@@ -376,7 +376,7 @@
 
                                             echo("<input type=\"text\" class=\"form-control\" name=\"EditPromotionID\" value=\"$promotionId\" hidden>");
                                             echo("<input type=\"text\" class=\"form-control\" name=\"EditPromotionTitle\" value=\"$promotionTitle\">");
-                                            echo("<label>Date:\</label>");
+                                            echo("<label>Date:</label>");
                                             echo("<input type=\"date\" class=\"form-control\" min=\"2022-04-14\" name=\"EditPromotionDate\" value=\"$promotionDate\">");
                                             echo("<input type=\"date\" class=\"form-control\" min=\"2022-04-14\" name=\"EditPromotionEndDate\" value=\"$promotionEnd_Date\">");
                                         }
