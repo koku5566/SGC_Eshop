1cb47f3417cd25c0cd9ca9c7b52a2b899ac7181a
Qxlim12 <qxionglim@yahoo.com>
2022-04-13 13:30:20 +0800

    table

--- seller/promotion.php
+++ seller/promotion.php
@@ -15,41 +15,39 @@
                         <div class="row">
                             <div class="col-xl-12 col-lg-12 col-sm-12">
                                 <div class="row">
-                                    <table class="table table-hover">
-                                        <thead>
-                                            <tr>
-                                            <th scope="col">Promotion Title</th>
-                                            <th scope="col">Date</th>
-                                            <th scope="col">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                                $sql = "SELECT promotion_title, promotion_Date, promotionEnd_Date from promotion";
-                                                $result = $conn->query($sql); 
-                                                if($result-> num_rows > 0){
-                                                    while($row = $result->fetch_assoc()){
-                                                        echo"<tr><td>"
-                                                        .$row["promotion_title"]."</td><td>"."Start:  "
-                                                        .$row["promotion_Date"]."<br>"."End:   "
-                                                        .$row["promotionEnd_Date"]."</td>
-                                                        <td>
-                                                        <div class=\"col-xl-6\" style=\"padding:0;\">
-                                                            <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?edit=".$row_1['promotion_id']."\" ><i class=\"fa fa-edit \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Edit</a>
-                                                            </div>
-                                                        <div class=\"col-xl-6\" style=\"padding:0;\">
-                                                            <a class=\"btn btn-outline-danger\" style=\"border:none;width:100%;\" href=\"?delete=".$row_1['promotion_id']."\" ><i class=\"fa fa-trash \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Delete</a>
-                                                            </div>
-                                                        </td></tr>";
-                                                    }
-                                                    echo"</table>";
-                                                }
-                                                else{
-                                                    echo"No Promotion.";
-                                                }
-                                            ?>
-                                        </tbody>
-                                    </table>
+                                    <?php
+                                        $sql = "SELECT promotion_title, promotion_Date, promotionEnd_Date from promotion";
+                                        $result = $conn->query($sql); 
+                                        if($result-> num_rows > 0){
+                                            while($row = $result->fetch_assoc()){
+                                                echo"<table class=\"table table-hover\">
+                                                <thead>
+                                                    <tr>
+                                                    <th scope=\"col\">Promotion Title</th>
+                                                    <th scope=\"col\">Date</th>
+                                                    <th scope=\"col\">Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody> "."<tr><td>"
+                                                .$row["promotion_title"]."</td><td>"."Start:  "
+                                                .$row["promotion_Date"]."<br>"."End:   "
+                                                .$row["promotionEnd_Date"]."</td>
+                                                <td>
+                                                <div class=\"col-xl-6\" style=\"padding:0;\">
+                                                    <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?edit=".$row_1['promotion_id']."\" ><i class=\"fa fa-edit \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Edit</a>
+                                                    </div>
+                                                <div class=\"col-xl-6\" style=\"padding:0;\">
+                                                    <a class=\"btn btn-outline-danger\" style=\"border:none;width:100%;\" href=\"?delete=".$row_1['promotion_id']."\" ><i class=\"fa fa-trash \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Delete</a>
+                                                    </div>
+                                                </td></tr>";
+                                            }
+                                            echo"</table>";
+                                            echo"</tbodu>";
+                                        }
+                                        else{
+                                            echo"No Promotion.";
+                                        }
+                                    ?>
                                 </div>
                             </div>
                         </div>
