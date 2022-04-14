9c5d923f43db79324479d9cbedb327998ae21045
Maverick <maverickleeweilin88@gmail.com>
2022-04-14 11:52:43 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -379,14 +379,12 @@
                                                                                 <div class=\"card\">
                                                                                     <div class=\"image-container\">
                                                                     ");
-                                                                    if($row_1['product_status'] == "A")
-                                                                    {
-                                                                        echo("
-                                                                            <div style=\"position: absolute;width: 100%;height: 40%;background-color: rgba(9, 9, 9, 0.6);padding: 10px;\">
-                                                                                <p style=\"color: white;\">Product get banned, please contact administrator for future help</p>
-                                                                            </div>
-                                                                        ");
-                                                                    }
+
+                                                                    echo("
+                                                                        <div style=\"position: absolute;width: 100%;height: 40%;background-color: rgba(9, 9, 9, 0.6);padding: 10px;\">
+                                                                            <p style=\"color: white;\">Product get banned, please contact administrator for future help</p>
+                                                                        </div>
+                                                                    ");
                                                                     
                                                                     echo("
                                                                                         
