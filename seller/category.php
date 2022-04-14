61a6deb6ef09beae73976ca0a2390b1315c6984c
Maverick <maverickleeweilin88@gmail.com>
2022-04-14 22:09:38 +0800

    Update category.php

--- seller/category.php
+++ seller/category.php
@@ -134,31 +134,29 @@
         $targetDir = dirname(__DIR__, 1)."/img/category/"; 
         $allowTypes = array('jpg','png','jpeg'); 
 
-        if(!empty($fileNames)){ 
-            foreach($_FILES['img']['name'] as $key=>$val){ 
-                // File upload path 
-                $fileName = basename($_FILES['img']['name'][$key]); 
-                $ext = pathinfo($fileName, PATHINFO_EXTENSION);
-                $fileName = round(microtime(true) * 1000).".".$ext;
-                $targetFilePath = $targetDir.$fileName; 
-                // Check whether file type is valid 
-                $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
-                if(in_array($fileType, $allowTypes)){ 
-                    if(move_uploaded_file($_FILES["img"]["tmp_name"][$key], $targetFilePath)){ 
-                        $sql_update .= ", category_pic = '$fileName' ";
-                    }
-                }
-                else if($defaultFile[$key] != "") //Get the default picture name
-                {
-                    $fileName = $defaultFile[$key];
+        foreach($_FILES['img']['name'] as $key=>$val){ 
+            // File upload path 
+            $fileName = basename($_FILES['img']['name'][$key]); 
+            $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+            $fileName = round(microtime(true) * 1000).".".$ext;
+            $targetFilePath = $targetDir.$fileName; 
+            // Check whether file type is valid 
+            $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+            if(in_array($fileType, $allowTypes)){ 
+                if(move_uploaded_file($_FILES["img"]["tmp_name"][$key], $targetFilePath)){ 
                     $sql_update .= ", category_pic = '$fileName' ";
                 }
-                else
-                {
-                    $sql_update .= ", category_pic = '' ";
-                }
-            } 
-        }
+            }
+            else if($defaultFile[$key] != "") //Get the default picture name
+            {
+                $fileName = $defaultFile[$key];
+                $sql_update .= ", category_pic = '$fileName' ";
+            }
+            else
+            {
+                $sql_update .= ", category_pic = '' ";
+            }
+        } 
 
         $sql_update .= " WHERE category_id = $categoryId ";
 
