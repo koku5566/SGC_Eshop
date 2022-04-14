ccb52d5009e25a1192c8f75534e2b26d3adac479
Maverick <maverickleeweilin88@gmail.com>
2022-04-14 11:51:17 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -379,7 +379,7 @@
                                                                                 <div class=\"card\">
                                                                                     <div class=\"image-container\">
                                                                     ");
-                                                                    if($row_1['product_status'] == "B")
+                                                                    if($row_1['product_status'] == "A")
                                                                     {
                                                                         echo("
                                                                             <div style=\"position: absolute;width: 100%;height: 40%;background-color: rgba(9, 9, 9, 0.6);padding: 10px;\">
