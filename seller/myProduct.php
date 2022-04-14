d6e5f35dd447f72007d7385a2c6a32da96a98ba8
Maverick <maverickleeweilin88@gmail.com>
2022-04-14 11:49:46 +0800

    Update myProduct.php

--- seller/myProduct.php
+++ seller/myProduct.php
@@ -383,7 +383,7 @@
                                                                     {
                                                                         echo("
                                                                             <div style=\"position: absolute;width: 100%;height: 40%;background-color: rgba(9, 9, 9, 0.6);padding: 10px;\">
-                                                                                <p style=\"color: white;/*! opacity: 1.0; */\">Product get banned, please contact administrator for future help</p>
+                                                                                <p style=\"color: white;\">Product get banned, please contact administrator for future help</p>
                                                                             </div>
                                                                         ");
                                                                     }
