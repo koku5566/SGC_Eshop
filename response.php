52a1e21257b472804a58cfd3668dc47165636596
chrischom02 <chrischom02@gmail.com>
2022-04-16 22:12:28 +0800

    Update response.php

--- response.php
+++ response.php
@@ -18,7 +18,7 @@ try {
     // Take the payment
     $payment->execute($execution, $apiContext);
 
-    try {
+ /*    try {
         $db = new mysqli($dbConfig['HOST'], $dbConfig['USERNAME'], $dbConfig['PASSWORD'], $dbConfig['NAME']);
 
         $payment = Payment::get($paymentId, $apiContext);
@@ -42,11 +42,37 @@ try {
             // Payment failed
 			header("location:https://eshop.sgcprototype2.com/payment-cancelled.html");
              exit(1);
+        } */
+        try {
+            $db = new mysqli($dbConfig['HOST'], $dbConfig['USERNAME'], $dbConfig['PASSWORD'], $dbConfig['NAME']);
+    
+            $payment = Payment::get($paymentId, $apiContext);
+    
+           
+                $product_id =[ 'product_id' => $payment->transactions[0]->item_list->items[0]->sku];
+                $transaction_id =['transaction_id' => $payment->getId()];
+                $payment_amount =['payment_amount' => $payment->transactions[0]->amount->total];
+                $currency_code =['currency_code' => $payment->transactions[0]->amount->currency];
+                $payment_status =['payment_status' => $payment->getState()];
+                $invoice_id =['invoice_id' => $payment->transactions[0]->invoice_number];
+                $product_name =['product_name' => $payment->transactions[0]->item_list->items[0]->name];
+                $description =['description' => $payment->transactions[0]->description];
+            
+            if (addPayment($data) !== false && $data['payment_status'] === 'approved') {
+                // Payment successfully added, redirect to the payment complete page.
+                $inserids =$db->insert_id;
+                header("location:https://eshop.sgcprototype2.com/PaypalSuccess.php?payid=$inserids");
+                exit(1);
+            } else {
+                // Payment failed
+                header("location:https://eshop.sgcprototype2.com/payment-cancelled.html");
+                 exit(1);
+            
+    
         }
-
     } catch (Exception $e) {
         // Failed to retrieve payment from PayPal
-        throw new Exception('Failed to retrieve payment from PayPal');
+
     }
 
 } catch (Exception $e) {
@@ -64,22 +90,22 @@ function addPayment($data)
 {
     global $db;
 
-    if (is_array($data)) {
+    if ($data!== false) {
 		//'isdsssss' --- i - integer, d - double, s - string, b - BLOB
-        $send = $db->prepare('INSERT INTO `payments` (product_id,transaction_id, payment_amount,currency_code, payment_status, invoice_id, product_name, createdtime) VALUES(?, ?, ?, ?, ?, ?, ?, ?)');
-        $send->bind_param(
-            'ssfsssss',
-            $data['product_id'],
-            $data['transaction_id'],
-            $data['payment_amount'],
-            $data['currency_code'],
-            $data['payment_status'],
-            $data['invoice_id'],
-            $data['product_name'],
+        $stmt = $db->prepare('INSERT INTO `payments` (product_id,transaction_id, payment_amount,currency_code, payment_status, invoice_id, product_name, createdtime) VALUES(?, ?, ?, ?, ?, ?, ?, ?)');
+        $stmt->bind_param(
+            'ssdsssss',
+            $product_id,
+            $transaction_id,
+            $payment_amount,
+            $currency_code,
+            $payment_status,
+            $invoice_id,
+            $product_name,
             date('Y-m-d H:i:s')
         );
-        $send->execute();
-        $send->close();
+        $stmt->execute();
+        $stmt->close();
 
         return $db->insert_id;
     }
