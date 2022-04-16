507ed11d44fc35292ad024ca197d7e5299203f9d
chrischom02 <chrischom02@gmail.com>
2022-04-16 14:50:26 +0800

    Update config.php

--- config.php
+++ config.php
@@ -24,8 +24,8 @@ $paypalConfig = [
     'PASSWORD' => '3g48B8Qn8k6v6VF',
     'NAME' => 'sgcprot1_SGC_ESHOP'
 ]; 
-
-$results = mysqli_query($dbConfig,"SELECT * FROM paymentPaypal where id='4' ");
+$db = new mysqli($dbConfig['HOST'], $dbConfig['USERNAME'], $dbConfig['PASSWORD'], $dbConfig['NAME']);
+$results = mysqli_query($db,"SELECT * FROM paymentPaypal where id='4' ");
 		$row = mysqli_fetch_array($results);
 echo $row['invoice_id']; 
 echo $row['transaction_id'];
