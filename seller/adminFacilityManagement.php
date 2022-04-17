6b9a7513679d59feca8bc1c54febc0f77807b0d8
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-04-17 19:43:34 +0800

    Update adminFacilityManagement.php

--- seller/adminFacilityManagement.php
+++ seller/adminFacilityManagement.php
@@ -5,7 +5,7 @@
 <?php
 if(isset($_POST['DeleteFacility']))
 {
-    $id= $_POST['DeleteFacility'];
+    $id= $_POST['id'];
     $sql_delete = "DELETE FROM facilityPic WHERE id = '$id'";
     if(mysqli_query($conn, $sql_delete))
     {
@@ -20,58 +20,7 @@ if(isset($_POST['DeleteFacility']))
         echo '<script>alert("Failed")</script>';
     }
 }
-   if(isset($_POST['EditPromotion']))
-   {
-       $promotionId = $_POST['EditPromotionID'];
-       $promotion_title = $_POST['EditPromotionTitle'];
-       $promotion_Date = date('Y-m-d', strtotime($_POST['EditPromotionDate']));
-       $promotionEnd_Date = date('Y-m-d', strtotime($_POST['EditPromotionEndDate']));
-       $promotion_image = "";
-       
-       $fileNames = array_filter($_FILES['imgEdit']['name']); 
-       $defaultFile = $_POST['imgDefaultEdit'];
-
-       // File upload configuration 
-       $targetDir = dirname(__DIR__, 1)."/img/promotion/"; 
-       $allowTypes = array('jpg','png','jpeg'); 
-
-       foreach($_FILES['imgEdit']['name'] as $key=>$val){ 
-           // File upload path 
-           $fileName = basename($_FILES['imgEdit']['name'][$key]); 
-           $ext = pathinfo($fileName, PATHINFO_EXTENSION);
-           $fileName = round(microtime(true) * 1000).".".$ext;
-           $targetFilePath = $targetDir.$fileName; 
-           // Check whether file type is valid 
-           $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
-           if(in_array($fileType, $allowTypes)){ 
-               if(move_uploaded_file($_FILES["imgEdit"]["tmp_name"][$key], $targetFilePath)){ 
-                   $promotion_image = $fileName;
-               }
-           }
-           else if($defaultFile[$key] != "") //Get the default picture name
-           {
-               $promotion_image = $defaultFile[$key];
-           }
-           else
-           {
-               $promotion_image = "";
-           }
-       } 
-       $sql_edit = "UPDATE promotion SET promotion_image='$promotion_image', promotion_title='$promotion_title', promotion_Date='$promotion_Date', promotionEnd_Date='$promotionEnd_Date' WHERE promotionID = '$promotionId'";
-
-       if(mysqli_query($conn, $sql_edit))
-       {
-           ?>
-               <script type="text/javascript">
-                   alert("Promotion Edited Successful");
-                   window.location.href = window.location.origin + "/seller/promotion.php";
-               </script>
-           <?php
-       }
-       else{
-           echo '<script>alert("Failed")</script>';
-       }
-   }
+  
 ?>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
@@ -126,7 +75,10 @@ if(isset($_POST['DeleteFacility']))
           </td>
           <td>
             <button type="button" class="btn btn-link btn-rounded btn-sm fw-bold">Edit</button>
-            <button type="button" class="btn btn-link btn-rounded btn-sm fw-bold" name="DeleteFacility">Delete</button>
+            <form action ="adminFacilityManagement.php" method="post">
+              <input type = "hidden" name="id" value="<?php $row['id']?>">
+              <input type="submit" class="btn btn-danger btn-rounded btn-sm fw-bold" name="DeleteFacility" value="Delete">
+            </form>
           </td>
         </tr>
         <?php endforeach ?> 
