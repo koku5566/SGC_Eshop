045aace36abaf197b53e424da2bdc40725067d95
koku5566 <koku5566@gmail.com>
2022-04-17 21:38:28 +0800

    Update updateEvent.php

--- seller/updateEvent.php
+++ seller/updateEvent.php
@@ -105,7 +105,7 @@
                             }
                             else{
                                 $error = mysqli_stmt_error($stmt);
-                                echo "<script>alert($error);</script>";
+                                echo "<script>alert($error + 'error');</script>";
                             }		
                             mysqli_stmt_close($stmt);
                     }
