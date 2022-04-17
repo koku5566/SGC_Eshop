3a6cda83281a35b0f541056dea4d34857267bd6c
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-04-17 13:39:41 +0800

    Update review.php

--- seller/review.php
+++ seller/review.php
@@ -6,6 +6,7 @@
   <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+  
  
 <?php
 /*
@@ -136,6 +137,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['uimage'], $_POST['t1fa
 	width: 75px;
 	height: 75px;
 }
+.tqy{
+		color: #A31F37
+	}
 </style>
 
 
