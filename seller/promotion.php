28e333c63ea8736702f7daf03e1543a284c77602
Qxlim12 <qxionglim@yahoo.com>
2022-04-19 16:06:42 +0800

    拜托拜托

--- seller/promotion.php
+++ seller/promotion.php
@@ -96,8 +96,7 @@
                                 <div class="row">
                                     <?php
                                         $userId = $_SESSION['uid'];
-                                        $sql = "SELECT * FROM promotion AS A LEFT JOIN user AS B ON A.user_id = B.user_id WHERE user_id = '$userId' ";
-                                        echo '<script>alert("1")</script>';
+                                        $sql = "SELECT * FROM promotion AS A LEFT JOIN user AS B ON A.user_id = B.user_id WHERE userID = '$userId' ";
                                         $result = $conn->query($sql); 
                                         if($result-> num_rows > 0){
                                             echo"<table class=\"table table-hover\">
