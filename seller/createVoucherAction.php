cac9774a4f6dc6d3e56b73f0791eee77c5e275d9
easonying0723 <ey73210723@gmail.com>
2022-04-20 05:40:54 +0800

    1

--- seller/createVoucherAction.php
+++ seller/createVoucherAction.php
@@ -1,7 +1,7 @@
 <?php
 
-   require __DIR__ . '/header.php';
-   require __DIR__ . '/createVoucher.php';
+   require_once __DIR__ . '/mysqli_connect.php';   
+   //require '../localDbConn.php';
 
    if(isset($_POST['submit'])){
 
@@ -19,12 +19,7 @@
       $sqlv = "INSERT INTO voucher (voucher_code, voucher_startdate, voucher_expired, discount_amount, voucher_limit, voucher_details, voucher_display, voucher_type, created_at, voucher_status, voucher_list)
                VALUES ('$voucherCode', '$voucherStartdate', '$voucherExpired', '$discountAmount', '$voucherLimit', '$voucherDetails', '$voucherDisplay', '$voucherType', '$date', '2', '0');";
       
-      if(mysqli_query($conn, $sqlv)){
-         echo 'success add voucher';
-      }
-      else{
-         echo $voucherCode;
-      }
+      mysqli_query($conn, $sqlv);
 
       $product = $_POST['productlist'];
       $v = mysqli_insert_id($conn);//specific table
@@ -34,12 +29,7 @@
          $sqlpv = "INSERT INTO productVoucher (product_id, voucher_id)
                   VALUES ('".$product[$i]."', '$v');"; //get prod first array
 
-         if(mysqli_query($conn, $sqlpv)){
-            echo 'success add product';
-         }
-         else{
-            //echo mysqli_error($conn, $sqlv);
-         }
+         mysqli_query($conn, $sqlpv);
          
       }
 
