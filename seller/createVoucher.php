ed11517d1b363c8760bb135a295724a18dab44a6
easonying0723 <ey73210723@gmail.com>
2022-04-20 03:24:21 +0800

    Update createVoucher.php

--- seller/createVoucher.php
+++ seller/createVoucher.php
@@ -222,7 +222,7 @@
                      </div>
                      <div class="form-group col-md-12">
                         <label for="">Applicable products</label>
-                        <button type="button" class="btn btn-light btn-lg btn-block rounded p-1" data-toggle="modal" data-bs-target="#selectproduct" style="border: dashed;" >+ Add Products</button>
+                        <button type="button" class="btn btn-light btn-lg btn-block rounded p-1" data-toggle="modal" data-bs-dismiss="modal" data-target="#selectproduct" style="border: dashed;" >+ Add Products</button>
                      </div>
                   </div>
                   <div class="form-row" id="productraw">
@@ -316,7 +316,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-warning" data-dismiss="modal" id="select">Select</button>
+        <button type="button" class="btn btn-warning" data-dismiss="modal" id="select" data-bs-target="#createVoucherModal">Select</button>
       </div>
     </div>
   </div>
