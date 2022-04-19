f4536b6fb070f922d7f06feaf05fe51484d94fdc
cheongkitmin <100913248+cheongkitmin@users.noreply.github.com>
2022-04-19 13:55:20 +0800

    Update helpCenterAdmin1.php

--- seller/helpCenterAdmin1.php
+++ seller/helpCenterAdmin1.php
@@ -3,6 +3,8 @@
 ?>
 
 <?php
+
+$_SESSION["userId"] = "A000001";
 	
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['uimage']) && !empty($_POST['uimage'])  ){	
             
@@ -395,6 +397,7 @@
 	}
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset ($_POST['CUmessagereply']) && !empty($_POST['CUmessagereply']) && $_POST['CUreplyadmin'] === 'Reply' ){
 		
+		$adminId = $_SESSION["userId"];
 		$CUmessagereply = $_POST['CUmessagereply'];
 		//echo "<div class='alert alert-success'>$CUmessagereply</div>";
 		$selectedPID = $_POST['CUid2'];
@@ -437,10 +440,10 @@
 		 if(mail($to, $subject, $content, $header)){
 			  echo "<script>alert('Email sent!')</script>";
 			  $sql = "UPDATE 
-					 `contactUs` SET status =?, r_message=? 
+					 `contactUs` SET status =?, r_message=?, admin_id=?
 			          WHERE cu_id =?";
 				if($stmt = mysqli_prepare($conn, $sql)){
-					mysqli_stmt_bind_param($stmt, 'iss', $status, $CUmessagereply, $selectedPID); 	//s=string , d=decimal value i=ID
+					mysqli_stmt_bind_param($stmt, 'isss', $status, $CUmessagereply, $adminId, $selectedPID); 	//s=string , d=decimal value i=ID
 			
 					mysqli_stmt_execute($stmt);
 				
