2b80adf46f252d7631637c914c066b1cf37d46f0
Qxlim12 <qxionglim@yahoo.com>
2022-04-19 16:22:28 +0800

    进化中

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     //Fetch each promotion image information
     $promotion_title = array();
     $promotion_image = array();
-    $_SESSION['role'] = $row["role"];
+    $userId = $_SESSION['uid'];
+    $role = $_SESSION['role'];
 
-    $sql_promotion = "SELECT * FROM promotion AS A LEFT JOIN user AS B ON A.user_id = B.role WHERE B.role = 'ADMIN' ";
+    $sql_promotion = "SELECT * FROM promotion AS A LEFT JOIN user AS B ON A.user_id = B.userID WHERE B.role = 'ADMIN' ";
 
     $result_promotion = mysqli_query($conn, $sql_promotion);
     
