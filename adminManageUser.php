32bcf71d53488475bf8beeef639494c18aa3fcec
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-04-19 21:31:40 +0800

    Update adminManageUser.php

--- adminManageUser.php
+++ adminManageUser.php
@@ -30,45 +30,6 @@
 	}
 ?>
 
-<div id="DataDiv">
-<h1>User</h1>
-<a href="../adminAddUser.php" class="btn btn-primary btn-block">Add User</a>
-<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" enctype="multipart/form-data">
-	<table class="table">
-	<tr>
-		<th class="text-center">User ID</th>
-		<th class="text-center">Username</th>
-		<th class="text-center">E-Mail</th>
-		<th class="text-center">Password</th>
-		<th class="text-center">Name</th>
-		<th class="text-center">Contact</th>
-		<th class="text-center">Registration Date</th>
-		<th class="text-center">Role</th>
-		<th class="text-center">Action</th>
-	</tr>
-<?php
-	$sql = "SELECT * FROM user";
-	$res_data = mysqli_query($conn,$sql);
-	while($row = mysqli_fetch_array($res_data)){
-		echo("
-			<tr>
-				<td class='text-center text-lg text-medium'>".$row["user_id"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["username"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["email"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["password"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["name"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["contact"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["registration_date"]."</td>
-				<td class='text-center text-lg text-medium'>".$row["role"]."</td>
-				<td class='text-center text-lg text-medium' style='display:flex;'><button name=\"edit\" value=".$row["username"]." class=\"btn btn-primary\" style=\"margin-right: 0.5rem;\">Edit</button>
-				<button name=\"remove\" value=".$row["username"]." class=\"btn btn-primary\">Remove</button></td>
-			</tr>
-			");
-	}
-?>
-	</table>
-</form>
-
 <div class="container-fluid">
 <h1 class="h3 mb-2 text-gray-800">User Management</h1>
 <!-- DataTales Example -->
