f10d56c68d53be465a888cef5a1d934b9561e2f0
easonying0723 <ey73210723@gmail.com>
2022-04-20 01:55:04 +0800

    Update createVoucher.php

--- seller/createVoucher.php
+++ seller/createVoucher.php
@@ -45,7 +45,7 @@
 <!-- Page Content -->
 <div class="container p-2" style="background-color: #FFFFFF; width:80%;">
    <h2 class="m-4">Create Voucher</h2>
-   <form name="form" action="/createVoucherAction.php" method="POST">
+   <form name="form" action="/seller/createVoucherAction.php" method="POST">
       <div class="container m-2">
          <h5 class="mt-2 mb-4">Basic Information</h5>
             <div class="form-row">
