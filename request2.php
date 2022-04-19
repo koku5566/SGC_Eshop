617e795ee1aa39a4320191d834f728898bdeba0e
chrischom02 <chrischom02@gmail.com>
2022-04-20 04:18:15 +0800

    a

--- request2.php
+++ request2.php
@@ -6,7 +6,6 @@ use PayPal\Api\RedirectUrls;
 use PayPal\Api\Transaction;
 use PayPal\Api\ItemList; 
 
-require __DIR__. '/header.php';
 require __DIR__. '/config.php';
 
 $ticket = $_SESSION['ticketSelected'];
