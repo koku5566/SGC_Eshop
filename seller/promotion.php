450fb01e07228cf35035d60bebfff77fa35494f0
Qxlim12 <qxionglim@yahoo.com>
2022-04-20 15:16:34 +0800

    6677

--- seller/promotion.php
+++ seller/promotion.php
@@ -214,11 +214,14 @@
                                                     {
                                                         echo ("
                                                                 <div class=\"row\">
-                                                                    <div class=\"col-xl-10 col-lg-10 col-sm-12\">
+                                                                    <div class=\"col-xl-8 col-lg-8 col-sm-12\">
                                                                         <input type=\"text\" class=\"form-control\" name=\"requestSectionTitle\" value=\"$promotionTitle\" readonly>
                                                                     </div>
                                                                     <div class=\"col-xl-2 col-lg-2 col-sm-12\">
-                                                                        <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?approveSection=".$row['promotionID']."\" ><i class=\"fa fa-exclamation-circle \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Rejected by Admin</a>
+                                                                        <p style=\"border:none;width:100%;\" href=\"?approveSection=".$row['promotionID']."\" ><i class=\"fa fa-exclamation-circle \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Rejected by Admin</p>
+                                                                    </div>
+                                                                    <div class=\"col-xl-2 col-lg-2 col-sm-12\">
+                                                                        <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?approveSection=".$row['promotionID']."\" ><i class=\"fa fa-bin \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Delete</a>
                                                                     </div>
                                                                 </div>
                                                             ");
@@ -227,11 +230,14 @@
                                                     {
                                                         echo ("
                                                                 <div class=\"row\">
-                                                                    <div class=\"col-xl-10 col-lg-10 col-sm-12\">
+                                                                    <div class=\"col-xl-8 col-lg-8 col-sm-12\">
                                                                         <input type=\"text\" class=\"form-control\" name=\"requestSectionTitle\" value=\"$promotionTitle\" readonly>
                                                                     </div>
                                                                     <div class=\"col-xl-2 col-lg-2 col-sm-12\">
                                                                         <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?approveSection=".$row['promotionID']."\" ><i class=\"fa fa-check \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Approved by Admin</a>
+                                                                    </div>
+                                                                    <div class=\"col-xl-2 col-lg-2 col-sm-12\">
+                                                                        <a class=\"btn btn-outline-primary\" style=\"border:none;width:100%;\" href=\"?approveSection=".$row['promotionID']."\" ><i class=\"fa fa-bin \" style=\"padding:0 10px;\" aria-hidden=\"true\"></i>Delete</a>
                                                                     </div>
                                                                 </div>
                                                             ");
