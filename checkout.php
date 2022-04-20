9fe9bb4338dfec18b4230e968abd0f15135b09c6
chrischom02 <chrischom02@gmail.com>
2022-04-20 23:26:47 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -171,7 +171,7 @@ if(isset($_GET['addressid']))
                             </thead>
                             <tbody>
                             <?php
-                            $cartsql ="SELECT product.product_name AS P_name, product.product_price AS P_price, cart.variation_id AS variation_id, 
+                            $sql ="SELECT product.product_name AS P_name, product.product_price AS P_price, cart.variation_id AS variation_id, 
                             cart.quantity AS P_quantity, product.product_variation AS P_variation, product.product_stock AS product_stock,
                             product.product_cover_picture AS P_pic, cart.product_ID AS PID, product.product_status AS P_status, cart.cart_ID AS cart_id
                             FROM `cart`
@@ -185,7 +185,7 @@ if(isset($_GET['addressid']))
                             ORDER BY cart.update_at DESC
                             ";
                             
-                            $queryKL = mysqli_query($conn, $cartsql);
+                            $queryKL = mysqli_query($conn, $sql);
                             
                             
                              while ($rowKL = mysqli_fetch_array($queryKL)) {
