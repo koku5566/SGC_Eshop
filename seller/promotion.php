e2bd469bf4708e2ac8e1e20f0efab63e83d89d59
Qxlim12 <qxionglim@yahoo.com>
2022-04-20 22:59:22 +0800

    sesee

--- seller/promotion.php
+++ seller/promotion.php
@@ -727,21 +727,30 @@
                                     <label>Promotion Title</label>
                                     <?php
                                     $promotionId = $_GET['edit'];
+                                    $dateStart = date('Y-m-d', strtotime($_GET['startDate']));
+                                    $dateEnd = date('Y-m-d', strtotime($_GET['endDate']));
+
                                     $sql = "SELECT promotionID, promotion_title, promotion_Date, promotionEnd_Date FROM promotion WHERE promotionID = '$promotionId'";
                                     $result = mysqli_query($conn, $sql);
+                                     //check if date valid
+                                    if( $dateEnd < $dateStart)
+                                    {
+                                        echo"<script>alert('The start date and end date is invalid.')</script>";
+                                    }
+                                    else{
+                                        if (mysqli_num_rows($result) > 0) {
+                                            while($row = mysqli_fetch_assoc($result)) {
+                                                $promotionId = $row["promotionID"];
+                                                $promotionTitle = $row["promotion_title"];
+                                                $promotionDate = $row["promotion_Date"];
+                                                $promotionEnd_Date = $row["promotionEnd_Date"];
 
-                                    if (mysqli_num_rows($result) > 0) {
-                                        while($row = mysqli_fetch_assoc($result)) {
-                                            $promotionId = $row["promotionID"];
-                                            $promotionTitle = $row["promotion_title"];
-                                            $promotionDate = $row["promotion_Date"];
-                                            $promotionEnd_Date = $row["promotionEnd_Date"];
-
-                                            echo("<br><input type=\"text\" class=\"form-control\" name=\"EditPromotionID\" value=\"$promotionId\" hidden>");
-                                            echo("<input type=\"text\" class=\"form-control\" name=\"EditPromotionTitle\" value=\"$promotionTitle\">");
-                                            echo("<br><label>Date</label>");
-                                            echo("<div class=\"input-group mb-2\"><div class=\"input-group-prepend\"><span class=\"input-group-text\" id=\"basic-addon1\">Start</span></div><input type=\"date\" class=\"form-control\" min=\"". date("Y-m-d",  strtotime("-1 month"))."\"name=\"EditPromotionDate\" value=\"$promotionDate\"></div>");
-                                            echo("<div class=\"input-group mb-2\"><div class=\"input-group-prepend\"><span class=\"input-group-text\" id=\"basic-addon1\">End</span></div><input type=\"date\" class=\"form-control\" min=\"". date("Y-m-d",  strtotime("-1 month"))."\" name=\"EditPromotionEndDate\" value=\"$promotionEnd_Date\"></div>");
+                                                echo("<br><input type=\"text\" class=\"form-control\" name=\"EditPromotionID\" value=\"$promotionId\" hidden>");
+                                                echo("<input type=\"text\" class=\"form-control\" name=\"EditPromotionTitle\" value=\"$promotionTitle\">");
+                                                echo("<br><label>Date</label>");
+                                                echo("<div class=\"input-group mb-2\"><div class=\"input-group-prepend\"><span class=\"input-group-text\" id=\"basic-addon1\">Start</span></div><input type=\"date\" class=\"form-control\" min=\"". date("Y-m-d",  strtotime("-1 month"))."\"name=\"EditPromotionDate\" value=\"$promotionDate\"></div>");
+                                                echo("<div class=\"input-group mb-2\"><div class=\"input-group-prepend\"><span class=\"input-group-text\" id=\"basic-addon1\">End</span></div><input type=\"date\" class=\"form-control\" min=\"". date("Y-m-d",  strtotime("-1 month"))."\" name=\"EditPromotionEndDate\" value=\"$promotionEnd_Date\"></div>");
+                                            }
                                         }
                                     }
                                     ?>
