8224dc59170173348c9cfa010a42a19c39ba2921
Qxlim12 <qxionglim@yahoo.com>
2022-04-20 19:58:30 +0800

    45

--- seller/promotion.php
+++ seller/promotion.php
@@ -739,7 +739,7 @@
 
                                     else {
                                         $result = mysqli_query($conn, $sql);
-                                        if(mysqli_num_rows($result) > 0) {
+                                        if($result > 0) {
                                             while($row = mysqli_fetch_assoc($result)) {
                                                 $promotionId = $row["promotionID"];
                                                 $promotionTitle = $row["promotion_title"];
