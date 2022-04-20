93da3343859763581711017baff53f4d2eac4f93
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-04-21 01:56:55 +0800

    Update adminManageUser.php

--- adminManageUser.php
+++ adminManageUser.php
@@ -171,7 +171,8 @@ const removeButton = document.querySelectorAll('.remove');
 removeButton.forEach(btn => {
 	btn.addEventListener('click', function handleClick(event) {
 		removeButton.forEach(btn => {
-			document.getElementById('deleteStaff').value=btn.value
+			document.getElementById('deleteStaff').value;
+			console.log('deleteStaff');
 		});
 	});
 });
