85b035bbc70fb9f9dd8128b0790f4324151a31eb
Maverick <maverickleeweilin88@gmail.com>
2022-04-20 08:44:34 +0800

    a

--- seller/editProduct.php
+++ seller/editProduct.php
@@ -1040,7 +1040,7 @@
     var priceTableArray = [];
 
     function submitForm(){
-        if(document.querySelectorAll('.imgInp')[0].value != "")
+        if(document.querySelectorAll('.imgInp')[0].value != "" || document.querySelectorAll('.imgDefault')[0].value != "")
         {
             if(document.querySelectorAll('.warning').length == 0)
             {
