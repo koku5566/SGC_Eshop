e370cda9e4a3b9532396a888ef08adbe6f25177b
Marcheal <marchealyong@gmail.com>
2022-04-20 13:23:46 +0800

    Update shopDetails.php

--- shopDetails.php
+++ shopDetails.php
@@ -6,7 +6,7 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "SELECT voucher_startdate, voucher_expired FROM voucher"; 
+    $sql = "SELECT product_name, product_description, product_brand, product_cover_picture FROM product";
     $result = $conn->query($sql);
 ?>
 
@@ -14,7 +14,7 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    $sql = "SELECT product_name, product_description, product_brand, product_cover_picture FROM product";
+    $sql = "SELECT voucher_startdate, voucher_expired FROM voucher"; 
     $result = $conn->query($sql);
 ?>
 <!-- Slide Show by Lim Qiu Xiong-->
