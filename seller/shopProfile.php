d10510ab5e96c2ad9498edf5fcdcc0cb89f44cc9
Marcheal <marchealyong@gmail.com>
2022-04-21 11:08:47 +0800

    Update shopProfile.php

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -70,42 +70,43 @@
 
 <?php 
 // If file upload form is submitted 
-$status = $statusMsg = ''; 
-if(isset($_POST["submit"])){ 
-    $status = 'error'; 
-    if(!empty($_FILES["image"]["name"])) { 
-        // Get file info 
-        $fileName = basename($_FILES["image"]["name"]); 
-        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
-         
-        // Allow certain file formats 
-        $allowTypes = array('jpg','png','jpeg','gif'); 
-        if(in_array($fileType, $allowTypes)){ 
-            $image = $_FILES['image']['tmp_name']; 
-            $shopImage = addslashes(file_get_contents($image)); 
-         
-            // Insert image content into database 
-            $insert = $db->query("INSERT into shopProfile (image, created) VALUES ('$shopImage', NOW())"); 
-             
-            if($insert){ 
-                $status = 'success'; 
-                $statusMsg = "File uploaded successfully."; 
-            }else{ 
-                $statusMsg = "File upload failed, please try again."; 
-            }  
-        }else{ 
-            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
-        } 
-    }else{ 
-        $statusMsg = 'Please select an image file to upload.'; 
-    } 
-} 
- 
-// Display status message 
-echo $statusMsg; 
+//$status = $statusMsg = ''; 
+//if(isset($_POST["submit"])){ 
+//    $status = 'error'; 
+//    if(!empty($_FILES["image"]["name"])) { 
+//        // Get file info 
+//        $fileName = basename($_FILES["image"]["name"]); 
+//        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+//         
+//        // Allow certain file formats 
+//        $allowTypes = array('jpg','png','jpeg','gif'); 
+//        if(in_array($fileType, $allowTypes)){ 
+//            $image = $_FILES['image']['tmp_name']; 
+//            $shopImage = addslashes(file_get_contents($image)); 
+//         
+//            // Insert image content into database 
+//            $insert = $db->query("INSERT into shopProfile (image, created) VALUES ('$shopImage', NOW())"); 
+//             
+//            if($insert){ 
+//                $status = 'success'; 
+//                $statusMsg = "File uploaded successfully."; 
+//            }else{ 
+//                $statusMsg = "File upload failed, please try again."; 
+//            }  
+//        }else{ 
+//            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
+//        } 
+//    }else{ 
+//        $statusMsg = 'Please select an image file to upload.'; 
+//    } 
+//} 
+// 
+//// Display status message 
+//echo $statusMsg; 
 ?>
 
 <?php
+  $sql = "SELECT * FROM shopProfile WHERE shop_id = 8";
   $imageResult = $db->sql("SELECT shop_profile_cover, shop_profile_image, shop_profile_media FROM shopProfile WHERE shop_id = 8");
 ?>
 
