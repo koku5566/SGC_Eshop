953768593db7aef75fdebe7217d37021fc0a076f
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-04-22 01:10:58 +0800

    u

--- adminManageUser.php
+++ adminManageUser.php
@@ -112,24 +112,6 @@
 					<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
-					<!--
-					<div class="imageDiv">
-						<div class="image-container">
-							<img class="card-img-top img-thumbnail" style="object-fit:contain;width:100%;height:100%" src="">
-							<div class="image-layer">
-							</div>
-							<div class="image-tools-delete hide">
-								<i class="fa fa-trash image-tools-delete-icon" aria-hidden="true"></i>
-							</div>
-							<div class="image-tools-add">
-								<label class="custom-file-upload">
-									<input accept=".png,.jpeg,.jpg" name="img[]" type="file" class="imgInp">
-									<i class="fa fa-plus image-tools-add-icon" aria-hidden="true"></i>
-								</label>
-							</div>
-						</div>
-					</div>
-					--->
 			</div>
 		</div>
 	</div>
