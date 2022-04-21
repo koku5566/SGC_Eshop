db1737bff6f7ae7d97aeb877683b010067f17dee
easonying0723 <ey73210723@gmail.com>
2022-04-22 01:52:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -196,8 +196,6 @@
                             while ($row = $result->fetch_assoc()) {
                                 $td = date('y-m-d');
                                 $expr = $row['voucher_expired'];
-
-                                $r = $res->fetch_assoc();
                                 
                                 $today = strtotime($td);
                                 $expired = strtotime($expr);
@@ -253,7 +251,17 @@
                                     <div class="tnccontainer m-2">
                                         <div class="container">
                                             <strong>Product</strong>
+                                            <?php 
+                                                    while ($r = $res->fetch_assoc()) {
+                                                        $voucherid = $r['voucher_id'];
+                                                        $voucherid2 = $row['voucher_id'];
+
+                                                        for($i = 0; $i < count($voucherid2); $i++){
+                                                            for($x = 0; $x < count($voucherid); $x++){
+                                                                if($voucherid2[$i] === $voucherid[$x]){
+                                            ?>
                                             <p><?php echo $r['product_name'];?>, </p>
+                                            <?php }}}}?>
                                         </div>
                                         <div class="container">
                                             <strong>More Details</strong>
