96b15f27cf4d255d1187657cd0b9eb778dd0ec40
easonying0723 <ey73210723@gmail.com>
2022-04-21 11:13:29 +0800

    Update myVoucher.php

--- seller/myVoucher.php
+++ seller/myVoucher.php
@@ -69,6 +69,7 @@
                         JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id	
                         JOIN product ON productVoucher.product_id = product.product_id		
                         JOIN shopProfile ON product.shop_id	= shopProfile.shop_id
+                        GROUP BY voucher.voucher_id
                         WHERE shopProfile.shop_id = '$shopId'
                         "; 
 
