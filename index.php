1c93210d6a1f89e626f48f57b873c152e55d05cf
easonying0723 <ey73210723@gmail.com>
2022-04-22 01:29:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -256,22 +256,22 @@
                                     <div class="tnccontainer m-2">
                                         <div class="container">
                                             <strong>Product</strong>
-                                            <p>
-                                                <?php 
+                                            <?php 
                                                     while ($r = $res->fetch_assoc()) {
                                                         $voucherid = $r['voucher_id'];
                                                         $voucherid2 = $row['voucher_id'];
 
-                                                        for($i = 0; $i < count($voucherid); $i++){
-                                                            for($x = 0; $x < count($voucherid2); $x++){
-                                                                if($voucherid[$i] === $voucherid2[$x]){
-                                                                    echo $r['product_name']; 
-                                                                }
+                                                        for($i = 0; $i < count($voucherid2); $i++){
+                                                            for($x = 0; $x < count($voucherid); $x++){
+                                                                if($voucherid2[$i] === $voucherid[$x]){
+                                            ?>
+                                            <p><?php echo $r['product_name'];?>, </p>
+                                            <?php
                                                             }
                                                         }
                                                     }
-                                                ?>
-                                            </p>
+                                                }
+                                            ?>
                                         </div>
                                         <div class="container">
                                             <strong>More Details</strong>
