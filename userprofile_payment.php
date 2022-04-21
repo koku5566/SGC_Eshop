5130a362c1a94d7f1533324fc9cee1f0fd82e484
Kyurem <40711092+KyuremEX@users.noreply.github.com>
2022-04-21 18:05:38 +0800

    Update userprofile_payment.php

--- userprofile_payment.php
+++ userprofile_payment.php
@@ -95,7 +95,7 @@
 	<div class="col2" style="background-color: #a31f37;">
 		<div class="container-col2">
 			<div class="container-left-col2"><h3 style="color: white;">Bank Account</h3></div>
-			<div class="container-right-col2"><button class="btn btn-primary"><a style="color:white;" href="../userAddBank.php">+</a></button></div>
+			<div class="container-right-col2"><a class="btn btn-primary" style="color:white;" href="../userAddBank.php">+</a></div>
 		</div>
 	</div>
 </div>
