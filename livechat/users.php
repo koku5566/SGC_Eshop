aaa0cce5f51f93f68dfdf57c3277292e269db5f3
Maverick <maverickleeweilin88@gmail.com>
2022-04-22 19:04:07 +0800

    Update users.php

--- livechat/users.php
+++ livechat/users.php
@@ -4,7 +4,6 @@
     $outgoing_id = $_SESSION['userid'];
     $sql = "SELECT * FROM user WHERE userID != '$outgoing_id' AND (role = 'SELLER' OR role = 'ADMIN') ORDER BY user_id DESC";
     $query = mysqli_query($conn, $sql);
-    echo($sql);
     $output = "";
     if(mysqli_num_rows($query) == 0){
         $output .= "No users are available to chat";
