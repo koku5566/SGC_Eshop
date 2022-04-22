83952992ae079e1de72cbb64ff1841fe1006c49f
  <40711092+KyuremEX@users.noreply.github.com>
2022-04-22 17:54:48 +0800

    Update adminAddUser.php

--- adminAddUser.php
+++ adminAddUser.php
@@ -38,6 +38,8 @@ if(isset($_POST['signup']))
 					if (mysqli_query($conn, $sql)) {
 						if ($_POST['role'] == "SELLER"){
 							$sql = "INSERT INTO shopProfile (shop_id, shop_name) VALUES ((SELECT CONCAT('U',(SELECT LPAD('$userid', 6, 0)))),'$username')";
+						}else{
+							echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 						}
 
 						$passwordE=$_POST['password'];
