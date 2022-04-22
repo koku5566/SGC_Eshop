f770bb73ff4d82655719fe42bc285e9dadf11fdc
Maverick <maverickleeweilin88@gmail.com>
2022-04-22 19:31:31 +0800

    as

--- livechat/users.php
+++ livechat/users.php
@@ -19,8 +19,7 @@
                     (outgoing_msg_id = '$outgoing_id'  OR incoming_msg_id = '$outgoing_id') ORDER BY msg_id DESC LIMIT 1";
                     
             $query2 = mysqli_query($conn, $sql2);
-            echo($sql2);
-            
+
             while($row2 = mysqli_fetch_assoc($query2)){
                 (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
                 (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
