332e961073ade4c5db7c7894af647386eb64cc71
easonying0723 <ey73210723@gmail.com>
2022-04-22 19:41:21 +0800

    Update userprofile_voucher.php

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -51,132 +51,134 @@
                                        <div class="h1 text-gray-900 mb-4 container-left-col2">My Voucher</div>
                                     </div>
                                     <hr>
-                              <?php
+                                    <?php
 
-                              $uid = $_SESSION['uid'];
+                                    $uid = $_SESSION['uid'];
 
-                              $sql_voucherR =
-                              "SELECT 
-                              voucherRedemption.voucher_id,
-                              voucherRedemption.user_id,
-                              voucher.voucher_id,
-                              voucher.voucher_code,
-                              voucher.voucher_type,
-                              voucher.discount_amount,
-                              voucher.voucher_display,
-                              voucher.voucher_limit,
-                              voucher.voucher_startdate,
-                              voucher.voucher_expired,
-                              voucher.voucher_details,
-                              shopProfile.shop_name,
-                              shopProfile.shop_id,
-                              shopProfile.shop_profile_image
+                                    $sql_voucherR =
+                                    "SELECT 
+                                    voucherRedemption.voucher_id,
+                                    voucherRedemption.user_id,
+                                    voucher.voucher_id,
+                                    voucher.voucher_code,
+                                    voucher.voucher_type,
+                                    voucher.discount_amount,
+                                    voucher.voucher_display,
+                                    voucher.voucher_limit,
+                                    voucher.voucher_startdate,
+                                    voucher.voucher_expired,
+                                    voucher.voucher_details,
+                                    shopProfile.shop_name,
+                                    shopProfile.shop_id,
+                                    shopProfile.shop_profile_image
 
-                              FROM voucherRedemption
-                              JOIN voucher ON voucherRedemption.voucher_id = voucher.voucher_id
-                              JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id
-                              JOIN product ON productVoucher.product_id = product.product_id
-                              JOIN shopProfile ON product.shop_id = shopProfile.shop_id
-                              WHERE voucherRedemption.user_id = $uid";
+                                    FROM voucherRedemption
+                                    JOIN voucher ON voucherRedemption.voucher_id = voucher.voucher_id
+                                    JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id
+                                    JOIN product ON productVoucher.product_id = product.product_id
+                                    JOIN shopProfile ON product.shop_id = shopProfile.shop_id
+                                    WHERE voucherRedemption.user_id = $uid";
 
-                              $stmt = $conn->prepare($sql_voucherR);
-                              $stmt->execute();
-                              $result = $stmt->get_result();
+                                    $stmt = $conn->prepare($sql_voucherR);
+                                    $stmt->execute();
+                                    $result = $stmt->get_result();
 
-                              while ($row = $result->fetch_assoc()) {
+                                    while ($row = $result->fetch_assoc()) {
 
-                              ?>
+                                    ?>
 
-                              <div class="card m-2" id="vouchercard2">
-                                 <div class="card-body">
-                                    <div class="row">
-                                       <div class="col-mb-3 m-2">
-                                          <img class="m-2" src="../img/shop_logo/<?php echo $row['shop_profile_image']; ?>" id="voucherlogo">
-                                       </div>
-                                       <div class="col-mb-7 m-2">
-                                          <h6 class="card-title"><strong><?php echo $row['shop_name']; ?></strong></h6>
-                                          <h5 class="card-subtitle text-muted"><?php echo $row['discount_amount']; ?> <?php echo $row['voucher_type']; ?> off</h5>
-                                          <small>Expired:<?php echo $row['voucher_expired']; ?></small><br>
-                                          <u>
-                                             <a type="" class="" data-toggle="modal" data-target="#termsv2Modal<?php echo $row['voucher_id']; ?>">
-                                             T&C applied.
-                                             </a>
-                                          </u>
+                                    <div class="col-sm-12">
+                                       <div class="card m-2">
+                                          <div class="card-body">
+                                             <div class="row">
+                                                <div class="col-mb-3 m-2">
+                                                   <img class="m-2" src="../img/shop_logo/<?php echo $row['shop_profile_image']; ?>" id="voucherlogo">
+                                                </div>
+                                                <div class="col-mb-7 m-2">
+                                                   <h6 class="card-title"><strong><?php echo $row['shop_name']; ?></strong></h6>
+                                                   <h5 class="card-subtitle text-muted"><?php echo $row['discount_amount']; ?> <?php echo $row['voucher_type']; ?> off</h5>
+                                                   <small>Expired:<?php echo $row['voucher_expired']; ?></small><br>
+                                                   <u>
+                                                      <a type="" class="" data-toggle="modal" data-target="#termsv2Modal<?php echo $row['voucher_id']; ?>">
+                                                      T&C applied.
+                                                      </a>
+                                                   </u>
+                                                </div>
+                                             </div>
+                                          </div>
                                        </div>
                                     </div>
-                                 </div>
-                              </div>
 
-                              <!-- Modal -->
-                              <div class="modal fade" id="termsModal<?php echo $row['voucher_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="termsModalTitle" aria-hidden="true">
-                                 <div class="modal-dialog modal-dialog-centered" role="document">
-                                    <div class="modal-content">
-                                          <div class="modal-header">
-                                          <h5 class="modal-title" id="termsModalLongTitle">Terms and Conditions.</h5>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                             <span aria-hidden="true">&times;</span>
-                                          </button>
-                                          </div>
-                                          <div class="modal-body">
-                                             <div class="d-flex justify-content-center">
-                                                <div class="card m-2" id="termsvouchercard">
-                                                <div class="container">
-                                                      <img class="mt-3" src="../img/shop_logo/<?php echo $row['shop_profile_image']; ?>" id="voucherlogo">
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="termsModal<?php echo $row['voucher_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="termsModalTitle" aria-hidden="true">
+                                       <div class="modal-dialog modal-dialog-centered" role="document">
+                                          <div class="modal-content">
+                                                <div class="modal-header">
+                                                <h5 class="modal-title" id="termsModalLongTitle">Terms and Conditions.</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                   <span aria-hidden="true">&times;</span>
+                                                </button>
                                                 </div>
-                                                <div class="card-body">
-                                                      <h6 class="card-title"><strong><?php echo $row['shop_name']; ?></strong></h6>
-                                                      <h5 class="card-subtitle text-muted"><?php echo $row['discount_amount']; ?> <?php echo $row['voucher_type']; ?> off</h5>
-                                                      <small>Used : <?php echo $row['voucher_startdate']; ?> ~ <?php echo $row['voucher_expired']; ?></small><br>
+                                                <div class="modal-body">
+                                                   <div class="d-flex justify-content-center">
+                                                      <div class="card m-2" id="termsvouchercard">
+                                                      <div class="container">
+                                                            <img class="mt-3" src="../img/shop_logo/<?php echo $row['shop_profile_image']; ?>" id="voucherlogo">
+                                                      </div>
+                                                      <div class="card-body">
+                                                            <h6 class="card-title"><strong><?php echo $row['shop_name']; ?></strong></h6>
+                                                            <h5 class="card-subtitle text-muted"><?php echo $row['discount_amount']; ?> <?php echo $row['voucher_type']; ?> off</h5>
+                                                            <small>Used : <?php echo $row['voucher_startdate']; ?> ~ <?php echo $row['voucher_expired']; ?></small><br>
+                                                      </div>
+                                                      </div>
+                                                   </div>
                                                 </div>
+                                                <div class="tnccontainer m-5 p-3">
+                                                   <div class="container">
+                                                      <strong>Product</strong>
+                                                      <?php 
+
+                                                      $voucher_id = $row['voucher_id'];
+
+                                                      $sql_pn=
+                                                      "SELECT
+                                                      product.product_name,
+                                                      voucher.voucher_id
+                                                                           
+                                                      FROM voucher
+                                                      JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id
+                                                      JOIN product ON productVoucher.product_id = product.product_id
+                                                      JOIN shopProfile ON product.shop_id = shopProfile.shop_id
+                                                      WHERE voucher.voucher_id = $voucher_id
+                                                      ";
+                           
+                                                      $sm = $conn->prepare($sql_pn);
+                                                      $sm->execute();
+                                                      $res = $sm->get_result();
+
+                                                            while ($r = $res->fetch_assoc()) {
+                                                      ?>
+
+                                                      <p><?php echo $r['product_name'];?>, </p>
+                                                      <?php 
+
+                                                   }?>
+                                                   </div>
+                                                   <div class="container">
+                                                      <strong>More Details</strong>
+                                                      <p> <?php echo $row['voucher_details']; ?> </p>
+                                                   </div>
+                                                   <div class="container">
+                                                      <strong>Usage Period</strong>
+                                                      <p><?php echo $row['voucher_startdate']; ?> ~ <?php echo $row['voucher_expired']; ?></p>
+                                                   </div>
                                                 </div>
-                                             </div>
                                           </div>
-                                          <div class="tnccontainer m-5 p-3">
-                                             <div class="container">
-                                                <strong>Product</strong>
-                                                <?php 
-
-                                                $voucher_id = $row['voucher_id'];
-
-                                                $sql_pn=
-                                                "SELECT
-                                                product.product_name,
-                                                voucher.voucher_id
-                                                                     
-                                                FROM voucher
-                                                JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id
-                                                JOIN product ON productVoucher.product_id = product.product_id
-                                                JOIN shopProfile ON product.shop_id = shopProfile.shop_id
-                                                WHERE voucher.voucher_id = $voucher_id
-                                                ";
-                     
-                                                $sm = $conn->prepare($sql_pn);
-                                                $sm->execute();
-                                                $res = $sm->get_result();
-
-                                                      while ($r = $res->fetch_assoc()) {
-                                                ?>
-
-                                                <p><?php echo $r['product_name'];?>, </p>
-                                                <?php 
-
-                                             }?>
-                                             </div>
-                                             <div class="container">
-                                                <strong>More Details</strong>
-                                                <p> <?php echo $row['voucher_details']; ?> </p>
-                                             </div>
-                                             <div class="container">
-                                                <strong>Usage Period</strong>
-                                                <p><?php echo $row['voucher_startdate']; ?> ~ <?php echo $row['voucher_expired']; ?></p>
-                                             </div>
-                                          </div>
+                                       </div>
                                     </div>
-                                 </div>
-                              </div>
                             
-                              <?php 
-                              }?>
+                                    <?php 
+                                    }?>
                               
                                  </div>
                               </div>
