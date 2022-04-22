9aea343a4efbbe85cbe83b4b4c3f46924ad19e29
Rycdnh <100915516+Rycdnh@users.noreply.github.com>
2022-04-23 01:36:30 +0800

    ss

---
+++ livechat.php
@@ -0,0 +1,24 @@
+<?php
+    require __DIR__ . '/header.php'
+?>
+
+<body>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6262e5edb0d10b6f3e6ee72b/1g1954a5a';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+</body>
+</html>
+<br>
+
+<?php
+    require __DIR__ . '/footer.php'
+?>
