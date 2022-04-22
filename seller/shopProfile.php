3c2e2ba8a0356a7cb4bb38dc8aaeaf2fac2f0a8b
Marcheal <marchealyong@gmail.com>
2022-04-22 15:53:54 +0800

    Update shopProfile.php

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -26,7 +26,7 @@
 
 <!-- Select Data -->
 <?php
-  $sql = "SELECT * FROM shopProfile WHERE shop_id = '5'";
+  $sql = "SELECT * FROM shopProfile WHERE shop_id = '10'";
   $result1 = mysqli_query($conn, $sql); 
 ?>
 
