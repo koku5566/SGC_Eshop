f768493078d9400b4bbac8404835605c6d1ce32d
koku5566 <koku5566@gmail.com>
2022-04-22 08:44:20 +0800

    Update eventDetails.php

--- eventDetails.php
+++ eventDetails.php
@@ -11,6 +11,89 @@ require __DIR__ . '/header.php'
     }
 ?>
 
+<?php
+    if(isset($_POST['resendTicket']))
+    {
+        $tEmail = mysqli_real_escape_string($conn, SanitizeString($_POST["emailTicket"]));
+        $sql1 = "SELECT * FROM `ticket` 
+        INNER JOIN ticketTransaction ON `ticket`.`transaction_id` = `ticketTransaction`.`ticketOrder_id`
+        INNER JOIN `event` ON `event`.`event_id` = `ticket`.`event_id` 
+        INNER JOIN `ticketType` ON `ticket`.`ticketType_id` = `ticketType`.`ticketType_id`
+        WHERE `ticketTransaction`.`buyer_email` = $tEmail";
+        $result1 = mysqli_query($conn, $sql1);
+
+        if (mysqli_num_rows($result1) > 0) {
+            while ($row1 = mysqli_fetch_assoc($result1)) {
+                $buyerEmail = $row1['buyer_email'];
+                $eventName = $row1['event_name'];
+                $ticketOrderID = $row1['ticketOrder_id'];
+                $buyerName = $row1['buyer_name'];
+                $ticketType = $row1['ticket_name'];
+                $contact = $row1['buyer_contact'];
+                $ticketString = $row1['ticket_id'];
+                    $to = $buyerEmail;
+                    $subject = "Event Regisration Completed - " . $eventName;
+                    $from = "event@sgcprototype2.com";
+                    $from2 = "event@sgcprototype2.com";
+                    $fromName = "SGC E-Shop";
+
+                    $headers =  "From: $fromName <$from> \r\n";
+                    $headers .= "MIME-Version: 1.0\r\n";
+                    $headers .= "Content-Type: multipart/mixed;\r\n";
+
+
+                    $message = "
+                    <link href='https://fonts.googleapis.com/css?family=Libre Barcode 128' rel='stylesheet'>
+                    <style>
+                    h2 {
+                        font-family: 'Libre Barcode 128';font-size: 22px;
+                    }
+                    </style>
+                    <h3>Thank you for registering in $eventName</h3>
+                    <h5>Your Transaction Summary</h5>
+                    <p>Transaction ID: $ticketOrderID</p>
+                    <p>Buyer Name: $buyerName</p>
+                    <p>Buyer Email: $buyerEmail</p>
+                    <p>Buyer Contact: $contact</p>
+                    <p>Total Price: $price</p>
+                    <p>Register Event: $eventName</p>
+                    <p>Ticket: $ticketType</p>
+                    <h5>Barcode below is your ticket for organizer check in purposes. Kindly bookmark this email and keep it safely</h5>
+                    <h2>$ticketString</h2>
+                    <h4>Thank you</h4>
+                    <h4>Best Regards</h4>
+                    <h4>SGC Eshop</h4>
+			        ";
+
+                    $HTMLcontent = "<p><b>Dear $buyerName</b>,</p><p>$message</p>";
+
+                    $boundary = md5(time());
+                    $headers .= " boundary=\"{$boundary}\"";
+                    $message = "--{$boundary}\r\n";
+                    $message .= "Content-Type: text/html; charset=\"UTF-8\"\r\n";
+                    $message .= "Content-Transfer-Encoding: 7bit\r\n";
+                    $message .= $HTMLcontent . "\r\n";
+                    $message .= "--{$boundary}\r\n";
+                    $returnPath = "-f" . $from2;
+
+                    if (@mail($to, $subject, $message, $headers, $returnPath)) {
+                        echo "<script>alert('A purchase confirmation email has been sent to $buyerEmail')</script>";
+                    } else {
+                        echo "<script>alert('Error')</script>";
+                    }
+            }
+        }
+        else
+        {
+            echo("
+            <script>alert(\"No Ticket Found, Please check your ticket ID again\") </script>
+            ");
+        }
+
+    }
+
+?>
+
 <title>Event Details</title>
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/suneditor@latest/dist/css/suneditor.min.css">
@@ -71,7 +154,8 @@ require __DIR__ . '/header.php'
                                 <h1 class=\"text-center\">".$row['event_name']."</h1>
                                 <p class=\"text-center\"><span class=\"by\">Organized by</span> <a href=\"#\">".$row['name']."</a></p>
                                 <div class=\"row\">
-                                    <div class=\"col-12\"><a href = \"selectTicket.php?eventID=" . $row['event_id'] . "\"><button class=\"btn btn-primary\" type=\"button\" style=\"background: rgb(163, 31, 55);\">Buy Ticket</button></a><button class=\"btn btn-primary\" type=\"button\" style=\"background: rgb(30,79,204);margin-left: 10px;\">Resend Ticket</button></div>
+                                    <div class=\"col-12\"><a href = \"selectTicket.php?eventID=" . $row['event_id'] . "\"><button class=\"btn btn-primary\" type=\"button\" style=\"background: rgb(163, 31, 55);\">Buy Ticket</button></a>
+                                    <button class=\"btn btn-primary\" type=\"button\" style=\"background: rgb(30,79,204);margin-left: 10px;\" data-bs-toggle=\"modal\" data-bs-target=\"#ticketType_modal\">Resend Ticket</button></div>
                                     <div class=\"row\">
                                         <div class=\"col col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 col-12\"><img style=\"width: 100%;\" src=\"$picLocation\" \"></div>
                                         <div class=\"col col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 col-12\">
@@ -131,6 +215,28 @@ require __DIR__ . '/header.php'
             }
         }
         ?>
+        <!-- Resend ticket modal -->
+        <div class="modal fade modal-dialog-scrollable" role="dialog" tabindex="-1" id="resendTicket_modal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Resend Ticket</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method = "POST" enctype="multipart/form-data">
+                            <div style="margin-bottom: 20px;">
+                                <h5>Buyer Email</h5><input class="form-control" type="email" placeholder="Email" name="emailTicket">
+                            </div>
+                            <div class="modal-footer">
+                                <button class="btn btn-light" type="button" data-bs-dismiss="modal">Close</button>
+                                <button class="btn btn-primary" type="submit" style="background: rgb(163, 31, 55);" name="resendTicket">Resend Ticket</button>
+                            </div>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
 
 
 
