4d221fe6555eaddd5524567479a1171d7e328493
easonying0723 <ey73210723@gmail.com>
2022-04-22 15:55:28 +0800

    Update promotion.php

--- promotion.php
+++ promotion.php
@@ -284,29 +284,18 @@
         }
         else{
 
-            $claimedcheckv = $pdo->prepare("SELECT voucher_id FROM `voucherRedemption`");
-            $claimedchecku = $pdo->prepare("SELECT user_id FROM `voucherRedemption`");
             $uid = $_SESSION['uid'];
+            $voucher_id = $_POST['voucher_id'];
 
-            if($row['voucher_id'] = $claimedcheck &&  $uid = $claimedchecku){
-
-                echo '<script>alert("Voucher has been claimed.")</script>';
-
-            }else{
-
-                $uid = $_SESSION['uid'];
-                $voucher_id = $_POST['voucher_id'];
-
-                $sqlc = "INSERT INTO voucherRedemption (voucher_id, user_id)
-                        VALUES ('$voucher_id','$uid')";
-
-                if($conn->query($sqlc))
-                {
-                    echo '<script>alert("Voucher claimed succesfully.")</script>';
-                }
-                else{
-                    echo '<script>alert("Voucher claimed failed.")</script>';
-                }
+            $sqlc = "INSERT INTO voucherRedemption (voucher_id, user_id)
+                    VALUES ('$voucher_id','$uid')";
+                    
+            if($conn->query($sqlc))
+            {
+                echo '<script>alert("Voucher claimed succesfully.")</script>';
+            }
+            else{
+                echo '<script>alert("Voucher claimed failed.")</script>';
             }
         }
 
