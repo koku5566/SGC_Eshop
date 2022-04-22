2a325bce3a032f59e8ef1e9660b95cbfa2002014
Marcheal <marchealyong@gmail.com>
2022-04-22 15:47:02 +0800

    Update shopProfile.php

--- seller/shopProfile.php
+++ seller/shopProfile.php
@@ -26,7 +26,7 @@
 
 <!-- Select Data -->
 <?php
-  $sql = "SELECT * FROM shopProfile WHERE shop_id = '6'";
+  $sql = "SELECT * FROM shopProfile WHERE shop_id = '5'";
   $result1 = mysqli_query($conn, $sql); 
 ?>
 
@@ -127,7 +127,7 @@
      $shopName = $_POST['name'];
      $shopDescription = $_POST['description'];
      $shopMedia = $_POST['mediaContainer'];
-     $update = "UPDATE shopProfile SET shop_profile_cover='$shopProfileCover', shop_profile_image='$shopProfilePic', shop_name='$shopName', shop_description='$shopDescription', shop_media='$shopMedia' WHERE shop_id = '6'";
+     $update = "UPDATE shopProfile SET shop_profile_cover='$shopProfileCover', shop_profile_image='$shopProfilePic', shop_name='$shopName', shop_description='$shopDescription', shop_media='$shopMedia' WHERE shop_id = '5'";
      $result = mysqli_query($conn, $update);
    }
 ?>
