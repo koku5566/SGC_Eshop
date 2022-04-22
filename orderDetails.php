001ebf37d47511223ea28532160d909d0e9be19e
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-22 13:59:41 +0800

    tesst

--- orderDetails.php
+++ orderDetails.php
@@ -38,50 +38,10 @@ $resultod = $stmt_2->get_result();
                             <h2 class="font-weight-bold text-center">ORDER DETAILS</h2>
                             <hr class="mx-auto">
                         </div>
-                        <div class="card">
-                                
-                                <div class="card-header">
-                                    
-                                   <div class="order-list-panel">
-                                        <div class="row">
-                                          
-                                            <div class="col-1">No.</div>
-                                            <div class="col-5">Product(s)</div>
-                                            <div class="col-2">Unit Price</div>
-                                            <div class="col-1">Quantity</div>
-                                            <div class="col-3">Total Amount</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                  <?php
-                                        $count=1;
-                                        while($rowod = $resultod->fetch_assoc()){
-                                        ?>
-                                        <div class="order-list-panel">
-                                            <div class="row">
-                                                <div class="col-1"><?php echo $count++; ?>.</div>
-                                                <div class="col-5"><img src="/img/product/<?php echo $row['product_cover_picture']?>">&nbsp;&nbsp;<?php echo $row["product_name"]; ?></div>
-                                                <div class="col-2"><?php echo $row["product_price"]; ?></div>
-                                                <div class="col-1"><?php echo $row["quantity"]; ?></div>
-                                                <div class="col-3"><?php echo $row["price"]; ?></div>
-                                            </div>
-                                        </div>
-                                                
-                                                
-                                            
-                                        <hr>
-                                        <?php } ?>
+                    <?php while($rowod = $resultod ->fetch_assoc()){?>
+                        <div class="card-header"><?php echo $rowod['order_id']?></div>
 
-                                        <br>
-                                        <a href="orderDetails.php?cancel&id=<?php echo $order_id?>" onclick="return confirm_click();"><button type="button" class="btn btn-primary">Cancel Order</button></a>
-                                        <a href="orderDetails.php?confirm&id=<?php echo $order_id?>" onclick="return confirm_click();"><button type="button" class="btn btn-primary">Confirm Order</button></a>
-                                        
-                                        <br><br>
-                                   
-                                </div>
-                              
-                    </section>
-                </div>
+                        <?php }?>
                 <!-- /.container-fluid -->
 
 <?php
