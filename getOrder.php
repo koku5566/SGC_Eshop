4bccf7851e103c5c514caf7274c608c4ef1dd0e2
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-22 10:45:08 +0800

    Update getOrder.php

--- getOrder.php
+++ getOrder.php
@@ -21,7 +21,7 @@ JOIN orderDetails ON myOrder.order_id = orderDetails.order_id
 JOIN product ON orderDetails.product_id = product.product_id
 JOIN shopProfile ON product.shop_id = shopProfile.shop_id
 
-ORDER BY myOrder.order_id
+ORDER BY myOrder.order_id DESC
 ";
 $stmt_2 = $conn->prepare($sql_2);
 $stmt_2->execute();
