549b0706aa5a4460c7575a1eec995642344e20bf
Maverick <maverickleeweilin88@gmail.com>
2022-04-22 19:34:06 +0800

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,6 +1,6 @@
-const searchBar = document.querySelector(".search input"),
-searchIcon = document.querySelector(".search button"),
-usersList = document.querySelector(".users-list");
+const searchBar = document.querySelector(".search input");
+const searchIcon = document.querySelector(".search button");
+const usersList = document.querySelector(".users-list");
 
 searchIcon.onclick = ()=>{
   searchBar.classList.toggle("show");
