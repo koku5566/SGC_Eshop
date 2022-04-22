202b22346fec7e092fe0ade49faf770213f99f27
chrischom02 <chrischom02@gmail.com>
2022-04-23 00:17:37 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -180,7 +180,7 @@ $shippingfee = 8.6;
                                         $product_price = $rowKL['P_price'];
                                         $product_stock = $rowKL['product_stock'];
         
-                                        $variation_message = "<option selected>Not Variation</option>";
+                                        $variation_message = " ";
                                     }
                                     else if ($rowKL['variation_id'] != "") {
                                         
