ff6921ecb771d6c20d4a82ebe634668273608edd
easonying0723 <ey73210723@gmail.com>
2022-04-22 19:42:27 +0800

    Update userprofile_voucher.php

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -110,7 +110,7 @@
                                     </div>
 
                                     <!-- Modal -->
-                                    <div class="modal fade" id="termsModal<?php echo $row['voucher_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="termsModalTitle" aria-hidden="true">
+                                    <div class="modal fade" id="termsv2Modal<?php echo $row['voucher_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="termsModalTitle" aria-hidden="true">
                                        <div class="modal-dialog modal-dialog-centered" role="document">
                                           <div class="modal-content">
                                                 <div class="modal-header">
