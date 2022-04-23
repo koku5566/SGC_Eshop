2d486f5793775b2a28f5cdfc135ba5673610c5ca
Maverick <maverickleeweilin88@gmail.com>
2022-04-23 10:40:00 +0800

    Update promotion.php

--- seller/promotion.php
+++ seller/promotion.php
@@ -1,10 +1,10 @@
 <?php
     require __DIR__ . '/header.php';
 
-    if ($_SESSION['login'] == "" || $_SESSION['uid'] == ""){
+    if (!isset($_SESSION['login']) || !isset($_SESSION['userid'])){
         ?>
             <script type="text/javascript">
-                //window.location.href = window.location.origin + "/login.php";
+                window.location.href = window.location.origin + "/login.php";
             </script>
         <?php
         exit;
