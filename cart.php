a9182e1b2a25badac75bf37db838cc8e31538eb2
Joelteoh <teohlixuan@gmail.com>
2022-04-23 10:47:12 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -8,7 +8,7 @@
                 FROM cart 
                 JOIN shopProfile
                 ON cart.shop_id = shopProfile.shop_id
-                WHERE `user_ID` = $userID AND cart.remove_Product = '0'";
+                WHERE `user_ID` = '$userID' AND cart.remove_Product = '0'";
     $query_shop = mysqli_query($conn, $sql_shop);
 
 ?>
