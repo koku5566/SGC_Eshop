e12a80fcc8b88d5ffbcefafdd5206d558eadca25
chrischom02 <chrischom02@gmail.com>
2022-04-23 14:58:01 +0800

    Update PaypalSuccess1.php

--- PaypalSuccess1.php
+++ PaypalSuccess1.php
@@ -162,12 +162,14 @@ $queryKL = mysqli_query($conn, $sql);
      date_default_timezone_set("Asia/Kuala_Lumpur");
     $date = date("Y-m-d");
     $paid = "Paid";
+    $emptyint = 0;
+    $emptystring  ='0';
     $shippingMethod = $_SESSION['shippingMethod'];  
 
-    echo(" 
+/*     echo(" 
     <span>".$date."</span>
     <span>".$paid."</span>
-    ");
+    "); */
 
     
     $sql2 = "INSERT INTO `productTransaction`(`invoice_id`, `user_id`, `product_id`, `variation_id`, `payment_status`, `address_id`, `shop_id`, `createdtime`, `quantity`) VALUES (?,?,?,?,?,?,?,?,?)";
@@ -177,7 +179,7 @@ $queryKL = mysqli_query($conn, $sql);
     }
            $sql3 = "INSERT INTO `myOrder`(`user_id`, `userID` `address_id`, `delivery_method`, `return_id`, `reason_type`, `sku`, `order_date`, `order_status`, `tracking_number`, `cancellation_id`, `invoice_id`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
         if ($stmt4 = mysqli_prepare($conn, $sql3)) {
-            $bp3 = mysqli_stmt_bind_param($stmt4, "ssisisssssis", '0',$uid, $user_address,$shippingMethod, '0','0','0',$date, $paid,'0','0', $invoice_id );
+            $bp3 = mysqli_stmt_bind_param($stmt4, "ssisisssssis", $emptystring ,$uid, $user_address,$shippingMethod, $emptyint,$emptystring,$emptystring,$date, $paid,$emptystring,$emptyint, $invoice_id );
             $bp3 = mysqli_stmt_execute($stmt4);
     }   
     
