cb5ce0aca30077d7695ef75c93b1ac7e86e96027
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-23 22:52:23 +0800

    Update orderDetails.php

--- orderDetails.php
+++ orderDetails.php
@@ -94,8 +94,13 @@ $order_id = $_GET['order_id'];
                         </div>
                         
                     </div>
+                        
+                       <?php if($row['order_status'] !='Completed'){?>
                         <a class="btn btn-primary " style="margin-left:10px;"  href="cancellation.php?order_id=<?php echo $row2['order_id'];?>">Cancel Order</a>
-                       <a class="btn btn-primary"style="margin-left:10px;" href="orderDetails.php?order_id=<?php echo $row2['order_id'];?>">Confirm Order</a>
+                        <a class="btn btn-primary"style="margin-left:10px;" href="orderDetails.php?order_id=<?php echo $row2['order_id'];?>">Confirm Order</a>
+                            <?php } else{ ?>
+                        <a class= "btn btn-primary" href="getOrder.php"></a>
+                        <?php }?>
                     </div>
                     <?php }?>
                 </div>
