d9a91b31bb2d01f143fe43cd1cbe9f5cbc0be8ef
chrischom02 <chrischom02@gmail.com>
2022-04-23 11:40:49 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -12,7 +12,7 @@
      $usersql ="SELECT user.email,userAddress.address_id,user.name,userAddress.user_id,userAddress.contact_name,userAddress.phone_number,userAddress.address,userAddress.postal_code,userAddress.area,userAddress.state,userAddress.country 
      FROM `userAddress`
      JOIN user ON userAddress.user_id = user.user_id
-     WHERE userAddress.user_id= '$userID'";
+     WHERE userAddress.user_id = '$_SESSION[uid]'";
 
 
 //Username and address
