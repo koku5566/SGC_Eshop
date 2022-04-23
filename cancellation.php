f6e3ec1521eb77a1a0c215b028b62fc4f80d0da4
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-23 17:16:53 +0800

    Update cancellation.php

--- cancellation.php
+++ cancellation.php
@@ -2,73 +2,89 @@
     require __DIR__ . '/header.php'
 ?>
 <?php
-    $orderid = $_GET['order_id'];
-    $cancelsql ="SELECT
-    myOrder.order_id,
-    myOrder.order_status,
-    myOrder.order_date,
-    orderDetails.quantity,
-    orderDetails.amount,
-    orderDetails.shop_id,
-    product.product_name,
-    product.product_price,
-    product.product_cover_picture,
-    shopProfile.shop_name,
-    shopProfile.shop_profile_image
-    FROM
-    myOrder
-    JOIN user ON myOrder.userID = user.user_id
-    JOIN orderDetails ON myOrder.order_id = orderDetails.order_id
-    JOIN product ON orderDetails.product_id = product.product_id
-    JOIN shopProfile ON orderDetails.shop_id = shopProfile.shop_id
-    WHERE myOrder.order_id = '$orderid';";
-    $stmt = $conn->prepare($cancelsql);
-    $stmt->execute();
-    $resultc = $stmt->get_result();
-    while ($rowc = $resultc->fetch_assoc()) {
-        $orderid = $rowc['order_id'];
-        $orderdate = $rowc['order_date'];
-        $qty = $rowc['quantity'];
-        $amt = $rowc['amount'];
-        $productname = $rowc['product_name'];
-        $productprice = $rowc['product_price'];
-        $productcover = $rowc['product_cover_picture'];
-        $shopname = $rowc['shop_name'];
-        $shopprofile = $rowc['shop_profile_image'];
-    }
+$order_id = $_GET['order_id'];
+
+
 ?>
+
+
 <!-- Begin Page Content -->
-<div class="container-fluid" style="width:80%">
-  <!---CANCEL ORDER----->
-  <h1 style="text-align:center; color: red ;">CANCELLATION</h1>
-  <a href="index.php" style="font-size:20px;">BACK</a>
-  <section id="orders" class="order container my-5 py-3 ">
-    <div class="container mt-2">
-      <h2 class="font-weight-bold text-center">ARE YOU SURE YOU WANT TO CANCEL YOUR ORDER?</h2>
-      <hr class="mx-auto">
-    </div>
-    <div class="card-body">
+    <div class="container-fluid" style="width:80%">
+        <h1 style="text-align:center; color: red ;">Cancellation</h1>
+        <a href="getOrder.php" style="font-size:20px;">BACK</a>
+        <section id="orders" class="order container my-5 py-3 ">
+            <div class="container mt-2">
+                <h2 class="font-weight-bold text-center">ARE YOU SURE YOU WANT TO CANCEL THE ORDER?</h2>
+                <hr class="mx-auto">
+            </div>
+            
+            <div class="card">
+                <div class="card-header">
+                    <div class="order-list-panel">
+                        <div class="row">
+                            <div class="col-1"></div>
+                            <div class="col-5">Product</div>
+                            <div class="col-2">Unit Price</div>
+                            <div class="col-2">Quantity</div>
+                            <div class="col-2">Total Price</div>
+                        </div>
+                    </div>
+                </div>
+                
+                <?php
+                    $shippingfee = 8.6;
+                    $sql2 = "SELECT * FROM myOrder 
+                    JOIN orderDetails ON myOrder.order_id = orderDetails.order_id
+                    JOIN product ON orderDetails.product_id = product.product_id
+                    JOIN shopProfile ON orderDetails.shop_id = shopProfile.shop_id
+                    WHERE myOrder.order_id = $order_id";
+                    
+                    $result2 = $conn->query($sql2);
+                    while($row2 = $result2->fetch_assoc()){
+                ?>
+                <div class="card">
+                <div class="card-body">
                     <div class="row">
                         
-                        <div class="col-1"><img src=/img/product/<?php echo $productcover?> style="object-fit:contain;width:100%;height:100%"></div>
+                        <div class="col-1"><img src=/img/product/<?php echo $row2['product_cover_picture']?> style="object-fit:contain;width:100%;height:100%"></div>
                         <div class="col-5">
-                            <?php echo $productname ?>
+                            <?php echo $row2['product_name']; ?>
                         </div>
                         <div class="col-2">RM
-                            <?php echo $productprice ?>.00
+                            <?php echo $row2['product_price']; ?>.00
                         </div>
                         <div class="col-2">X
-                            <?php echo $qty ?>
+                            <?php echo $row2['quantity']; ?>
                         </div>
                         <div class="col-2 red-text">RM
-                            <?php echo $amt ?>.00
+                            <?php echo $row2['amount']; ?>.00
                         </div>
                         
                     </div>
                 </div>
-  </section>    
-</div>
-
+                <br>
+                <div class="card-body">
+                  <h1 class="blue-text">Please tell us why you want to cancel your order</h1>
+                    <form method="post" action="getOrder.php" style="font-size:25px;">
+                      <input type="radio" id="id_1" name="reason" value="Regrets"  checked>
+                      <label for="id_1" >Regrets</label><br>
+                      <input type="radio" id="id_2" name="reason" value="Change Of Mind">
+                      <label for="id_2">Change of Mind</label><br>
+                      <input type="radio" id="id_3" name="reason" value="Change Color">
+                      <label for="id_3">Change Color</label><br>
+                      <input type="radio" id="id_4" name="reason" value="Others" checked>
+                      <label for="id_1">Others</label><br>
+                      
+                    </form> 
+                </div>
+                       
+                  </div>
+                    <?php }?>
+                </div>
+                <br>
+            </div>
+        </section>
+    </div>
 <!-- /.container-fluid -->
 
 <?php
@@ -78,3 +94,11 @@
 <style>
 
 </style>
+<script type="text/javascript">
+function confirm_click()
+{
+return confirm("Are you sure you want to complete the order?");
+}
+
+
+</script>
