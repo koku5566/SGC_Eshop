024bc8dcda357d1c0c3c44335d1e24fc8b92db63
Marcheal <marchealyong@gmail.com>
2022-04-25 00:14:03 +0800

    Update shopDetails.php

--- shopDetails.php
+++ shopDetails.php
@@ -163,14 +163,14 @@
       </section><br>
 
         <section class="text-center">
-          <h4 class="mb-5"><strong>Shop Voucher</strong></h4>
+          <h4 class="mb-5"><strong>Vouchers</strong></h4>
           <div class="d-flex align-items-center"> <!--<div class="voucherContainer d-flex align-items-center">-->
           <?php
                   if ($result2->num_rows > 0) {
                     // output data of each row
                     while($row2 = $result2->fetch_assoc()) {
           ?>
-            <div class="voucher">
+            <div class="voucher overflow-hidden">
               <div class="coupon-card">
                 <!--<img src="https://cdn.mos.cms.futurecdn.net/tQxVwcJSowYD7xwWDYidd9.jpg" class="logo">-->
                 <!--<h3>20% flat off on all rides within the city <br> using HDFC Credit Card</h3>-->
@@ -205,7 +205,7 @@
 
         <!--Section: Content-->
         <section class="text-center">
-          <h4 class="mb-5"><strong>Best Sales</strong></h4>
+          <h4 class="mb-5"><strong>Products</strong></h4>
           <div class="row">
             <?php
               if ($result1->num_rows > 0) {
