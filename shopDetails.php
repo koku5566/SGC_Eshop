90b38974f37df23f006794ce90a8431ff7d7af1e
Qxlim12 <qxionglim@yahoo.com>
2022-04-24 16:50:20 +0800

    tt

--- shopDetails.php
+++ shopDetails.php
@@ -6,8 +6,9 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    $sql1 = "SELECT product_name, product_description, product_price, product_cover_picture FROM product";
-    $sql2 = "SELECT discount_amount, voucher_code, voucher_startdate, voucher_expired FROM voucher"; 
+    $userId = $_GET['id'];
+    $sql1 = "SELECT product_name, product_description, product_price, product_cover_picture FROM product WHERE shop_id='$userId'";
+    $sql2 = "SELECT discount_amount, voucher_code, voucher_startdate, voucher_expired FROM voucher WHERE shop_id='$userId'"; 
     $result1 = $conn->query($sql1);
     $result2 = $conn->query($sql2);
 ?>
