35328c1fb363b6ed851b6e1c690a2b7389f2fd2c
easonying0723 <ey73210723@gmail.com>
2022-04-24 21:45:27 +0800

    Update userprofile_voucher.php

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -75,7 +75,7 @@
                                     JOIN product ON productVoucher.product_id = product.product_id
                                     JOIN shopProfile ON product.shop_id = shopProfile.shop_id
                                     -- WHERE voucherRedemption.id = $userid
-                                    GROUP BY voucher.voucher_id, shopProfile.shop_name, shopProfile.shop_profile_image, shopProfile.shop_id, voucherRedemption.voucher_id, voucherRedemption.user_id
+                                    -- GROUP BY voucher.voucher_id, shopProfile.shop_name, shopProfile.shop_profile_image, shopProfile.shop_id, voucherRedemption.voucher_id, voucherRedemption.user_id
                                     ";
 
                                     $stmt = $conn->prepare($sql_voucherR);
