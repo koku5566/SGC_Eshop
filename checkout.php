38c2206130b805bcb15e254e5f9ffdd632e685e4
chrischom02 <chrischom02@gmail.com>
2022-04-24 10:54:41 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -111,7 +111,7 @@ $shippingfee = 8.6;
     </div>
   </div>
   
-    <div class="container-fluid" style="width:80%">
+    <div class="container-fluid" style="width:80% background-color:white;">
 <div>
     <div class="container" style="padding: 24px;margin-top: 30px;">
         <div style="padding: 12px;background: var(--bs-body-bg);border-width: 1px;box-shadow: 0px 0px 1px var(--bs-gray-500);"><label class="form-label" style="font-size: 20px;"><i class="fa fa-map-marker" style="width: 19.4375px;"></i><strong>Delivery Address</strong></label>
