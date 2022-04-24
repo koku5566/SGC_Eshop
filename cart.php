a25ffadffa67fcac19e9ad78382b4dd2e300b761
Joelteoh <teohlixuan@gmail.com>
2022-04-25 01:07:50 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -724,7 +724,7 @@ select.form-control {
     function discountAmount()
     {
         var afterDiscount = parseFloat(document.getElementById('discount').innerText);
-        var beforeDiscount = parseFloat(document.getElementById('subtotal_kl').innerText);
+        var beforeDiscount = parseFloat(document.getElementById('subtotal_kl').innerHTML);
 
         var discountTotal = afterDiscount - beforeDiscount;
         document.getElementById('discount').innerHTML = (Math.round((discountTotal + Number.EPSILON) * 100) / 100).toFixed(2);
