397408423ca58e98acc2f296bb6246d3d592d5f0
Joelteoh <teohlixuan@gmail.com>
2022-04-24 18:46:29 +0800

    Update voucherModal.php

--- voucherModal.php
+++ voucherModal.php
@@ -238,7 +238,7 @@
          console.log("type: " + type);
 
          //get tr element id
-         var current_price = parseFloat(document.getElementById(values[i]).innerHTML).toFixed(2);
+         var current_price = document.getElementById(values[i]).innerHTML;
 
          console.log("current price: " + current_price);
 
