62a1403a13527730a6393886c066664c5b2c4a48
easonying0723 <ey73210723@gmail.com>
2022-04-26 01:37:46 +0800

    Update myVoucher.php

--- seller/myVoucher.php
+++ seller/myVoucher.php
@@ -79,7 +79,6 @@
                      while ($r = $res->fetch_assoc()) {
 
                         $vid = $r['voucher_id'];
-                        $return = $_SERVER['PHP_SELF'];
 
                   ?>
                   <tr>
@@ -93,6 +92,8 @@
                      <td>
                         <?php if ($r['voucher_list'] == 0 ){
 
+                           $return = $_SERVER['PHP_SELF'];
+
                            echo ("
 
                            <form action=\"<?php echo $return ?>\" method=\"POST\" enctype=\"multipart/form-data\">
@@ -102,39 +103,9 @@
 
                            ");
 
-                           if(isset($_POST['list'])){
-         
-                              $voucher_id = $_POST['voucher_id'];
-                        
-                              $sqll = "UPDATE voucher SET voucher_list = '0'
-                                       WHERE voucher_id = '$voucher_id'";
-                                          
-                              if($conn->query($sqll))
-                              {
-                                 echo '<script>alert("Your voucher has been delisted.")</script>';
-                              }
-                              else{
-                                 echo '<script>alert("Your voucher failed to be delisted.")</script>';
-                              }
-                           }
-                        
-                           if(isset($_POST['delist'])){
-                              
-                              $voucher_id = $_POST['voucher_id'];
-                        
-                              $sqldl = "UPDATE voucher SET voucher_list = '1'
-                                       WHERE voucher_id = '$voucher_id'";
-                                          
-                              if($conn->query($sqldl))
-                              {
-                                 echo '<script>alert("Your voucher has been listed.")</script>';
-                              }
-                              else{
-                                 echo '<script>alert("Your voucher failed to be listed.")</script>';
-                              }
-                           }
-
                         }else if($r['voucher_list'] == 1 ){
+
+                           $return = $_SERVER['PHP_SELF'];
 
                            echo ("
 
@@ -144,39 +115,6 @@
                            </form>
 
                            ");
-
-
-                           if(isset($_POST['list'])){
-         
-                              $voucher_id = $_POST['voucher_id'];
-                        
-                              $sqll = "UPDATE voucher SET voucher_list = '0'
-                                       WHERE voucher_id = '$voucher_id'";
-                                          
-                              if($conn->query($sqll))
-                              {
-                                 echo '<script>alert("Your voucher has been delisted.")</script>';
-                              }
-                              else{
-                                 echo '<script>alert("Your voucher failed to be delisted.")</script>';
-                              }
-                           }
-                        
-                           if(isset($_POST['delist'])){
-                              
-                              $voucher_id = $_POST['voucher_id'];
-                        
-                              $sqldl = "UPDATE voucher SET voucher_list = '1'
-                                       WHERE voucher_id = '$voucher_id'";
-                                          
-                              if($conn->query($sqldl))
-                              {
-                                 echo '<script>alert("Your voucher has been listed.")</script>';
-                              }
-                              else{
-                                 echo '<script>alert("Your voucher failed to be listed.")</script>';
-                              }
-                           }
 
                         }
                         ?>
@@ -196,37 +134,37 @@
 
 <?php
 
-   // if(isset($_POST['list'])){
+   if(isset($_POST['list'])){
          
-   //    $voucher_id = $_POST['voucher_id'];
+      $voucher_id = $_POST['voucher_id'];
 
-   //    $sqll = "UPDATE voucher SET voucher_list = '0'
-   //             WHERE voucher_id = '$voucher_id'";
+      $sqll = "UPDATE voucher SET voucher_list = '0'
+               WHERE voucher_id = '$voucher_id'";
                   
-   //    if($conn->query($sqll))
-   //    {
-   //       echo '<script>alert("Your voucher has been delisted.")</script>';
-   //    }
-   //    else{
-   //       echo '<script>alert("Your voucher failed to be delisted.")</script>';
-   //    }
-   // }
+      if($conn->query($sqll))
+      {
+         echo '<script>alert("Your voucher has been delisted.")</script>';
+      }
+      else{
+         echo '<script>alert("Your voucher failed to be delisted.")</script>';
+      }
+   }
 
-   // if(isset($_POST['delist'])){
+   if(isset($_POST['delist'])){
       
-   //    $voucher_id = $_POST['voucher_id'];
+      $voucher_id = $_POST['voucher_id'];
 
-   //    $sqldl = "UPDATE voucher SET voucher_list = '1'
-   //             WHERE voucher_id = '$voucher_id'";
+      $sqldl = "UPDATE voucher SET voucher_list = '1'
+               WHERE voucher_id = '$voucher_id'";
                   
-   //    if($conn->query($sqldl))
-   //    {
-   //       echo '<script>alert("Your voucher has been listed.")</script>';
-   //    }
-   //    else{
-   //       echo '<script>alert("Your voucher failed to be listed.")</script>';
-   //    }
-   // }
+      if($conn->query($sqldl))
+      {
+         echo '<script>alert("Your voucher has been listed.")</script>';
+      }
+      else{
+         echo '<script>alert("Your voucher failed to be listed.")</script>';
+      }
+   }
    
 ?>
 
