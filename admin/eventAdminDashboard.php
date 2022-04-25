6b2cb3fc0d3c2ea9252c01900e2cbdcab8c6b00f
Maverick <maverickleeweilin88@gmail.com>
2022-04-25 23:13:07 +0800

    aa

--- admin/eventAdminDashboard.php
+++ admin/eventAdminDashboard.php
@@ -2,6 +2,13 @@
 require __DIR__ . '/header.php'
 ?>
 
+<?php	
+	if($_SESSION['login'] == false || $_SESSION['role'] != "ADMIN")
+	{
+		?><script>window.location = '<?php echo("$domain/index.php");?>'</script><?php
+		exit;
+    }
+?>
 <!-- Begin Page Content -->
 <div class="container-fluid" style="width:100%;">
     <!-- Above Template -->
