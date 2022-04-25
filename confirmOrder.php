55ce494efbd719730b889dc7a6ab737db39c7249
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-25 15:43:41 +0800

    Update confirmOrder.php

--- confirmOrder.php
+++ confirmOrder.php
@@ -77,15 +77,6 @@ if(isset($_POST['confirm']))
       </div>
 
       <!--------------------END OF DETAILS---------------------->
-      
-      <div class="card-body">
-          
-          <form method="post" action="confirmOrder.php" style="font-size:25px;">
-                <input type="hidden" id="order_id" name="order_id" value="<?php echo $_GET['order_id']; ?>">
-                <input class="btn btn-primary" type="submit" name="confirm" value="Confirm Order" >
-                
-            </form>
-      </div>
     
 
     </div>
