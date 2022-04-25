b2bfca9d35c9b2e530c384c743052b0a7d711c36
  <40711092+KyuremEX@users.noreply.github.com>
2022-04-25 19:54:29 +0800

    Update header.php

--- seller/header.php
+++ seller/header.php
@@ -116,7 +116,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="dashboard.php">
+                <a class="nav-link" href="../seller/dashboard.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
