e54cfed23621ab239d12ba12c696f173f1e47a0c
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-25 16:05:43 +0800

    Update confirmOrder.php

--- confirmOrder.php
+++ confirmOrder.php
@@ -36,39 +36,7 @@ if(isset($_POST['confirm']))
       <div class="card-header">
         
       </div>
-      <!-----------------THIS IS THE DETAILS------------------->
-      <?php
-        $shippingfee = 8.6;
-        $sql2 = "SELECT * FROM myOrder 
-        JOIN orderDetails ON myOrder.order_id = orderDetails.order_id
-        JOIN product ON orderDetails.product_id = product.product_id
-        JOIN shopProfile ON orderDetails.shop_id = shopProfile.shop_id
-        WHERE myOrder.order_id = '$order_id' ";
-        $result2 = $conn->query($sql2);
-        while($row2 = $result2->fetch_assoc()){
-      ?>
-      <div class="card">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-1"><img src=/img/product/<?php echo $row2['product_cover_picture']?> style="object-fit:contain;width:100%;height:100%"></div>
-            <div class="col-5">
-              <?php echo $row2['product_name']; ?>
-            </div>
-            <div class="col-2">RM
-              <?php echo $row2['product_price']; ?>.00
-            </div>
-            <div class="col-2">X
-              <?php echo $row2['quantity']; ?>
-            </div>
-            <div class="col-2 red-text">RM
-              <?php echo $row2['amount']; ?>.00
-            </div>
-          </div>
-        </div>
-        <?php }?>
-      </div>
-
-      <!--------------------END OF DETAILS---------------------->
+     
       
       <div class="card-footer">
           
