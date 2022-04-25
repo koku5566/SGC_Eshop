cc0c1dafea54b3b0f8d2e0b350d7dd14399b4fe2
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-26 00:07:59 +0800

    Update orderDetails.php

--- orderDetails.php
+++ orderDetails.php
@@ -98,7 +98,7 @@ $order_id = $_GET['order_id'];
                         </div>
                         
                     </div>
-                    <?php if($row2['order_status'] !='Completed'){?>
+                    <?php if($row2['order_status'] !='Completed' && $row2['cancellation_status'] =='rejected'){?>
                         <a class="btn btn-primary " style="margin-left:10px;"  href="cancellation.php?order_id=<?php echo $row2['order_id'];?>">Cancel Order</a>
                         <?php }else{?>
                         <a class= "btn btn-primary" href="getOrder.php">Back</a>
