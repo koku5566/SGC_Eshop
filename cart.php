4b60e488ac3015b531a39397c4213fe74ba98b38
Joelteoh <teohlixuan@gmail.com>
2022-04-26 00:41:22 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -310,31 +310,31 @@
         
         var times = document.getElementsByClassName("form-check-input");
 
-        // if(document.getElementById(id).checked != true){
-        //     for (var i = 0;i < times.length; i++)
-        //     {
+        if(document.getElementById(id).checked != true){
+            // for (var i = 0;i < times.length; i++)
+            // {
                 
-        //         document.getElementById(i).disabled = false;
+            //     document.getElementById(i).disabled = false;
                 
-        //     }
-        //     document.getElementById(id).checked = false;
-        // }
-        // else{
+            // }
+            document.getElementById(id).checked = false;
+        }
+        else{
             for (var i = 0;i < times.length; i++)
             {
-                if(i == id)
-                {
-                    document.getElementById(i).disabled = false;
-                }
-                else{
+                // if(i == id)
+                // {
+                //     document.getElementById(i).disabled = false;
+                // }
+                //else{
                     document.getElementById(i).checked = false;
-                    document.getElementById(i).disabled = true;
-                }
+                    //document.getElementById(i).disabled = true;
+                //}
                 
             }
             document.getElementById(id).checked = true;
             
-        //}
+        }
 
     }
 
