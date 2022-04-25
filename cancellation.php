10bfd4dc90e359c9371bec0de2a718e3ed4b8686
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-25 17:50:05 +0800

    Update cancellation.php

--- cancellation.php
+++ cancellation.php
@@ -55,27 +55,7 @@ if(isset($_POST['cancel']))
         $result2 = $conn->query($sql2);
         while($row2 = $result2->fetch_assoc()){
       ?>
-      <div class="card">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-1"><img src=/img/product/<?php echo $row2['product_cover_picture']?> style="object-fit:contain;width:100%;height:100%"></div>
-            <div class="col-5">
-              <?php echo $row2['product_name']; ?>
-            </div>
-            <div class="col-2">RM
-              <?php echo $row2['product_price']; ?>.00
-            </div>
-            <div class="col-2">X
-              <?php echo $row2['quantity']; ?>
-            </div>
-            <div class="col-2 red-text">RM
-              <?php echo $row2['amount']; ?>.00
-            </div>
-          </div>
-        </div>
-        <?php }?>
-      </div>
-
+      <?php }?>
       <!--------------------END OF DETAILS---------------------->
       <!--------------------ASK REASON TO CANCEL---------------->
       <div class="card-body">
@@ -93,6 +73,7 @@ if(isset($_POST['cancel']))
                 <input class="btn btn-primary" type="submit" name="cancel" value="Confirm" >
                 
             </form>
+        </div>
           
       <!-----------------END OF ASK REASON TO CANCEL------------>
 
