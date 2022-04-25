a78a40f614378ddd2f5d41906d2541fc62cb2042
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-25 15:30:34 +0800

    Update orderDetails.php

--- orderDetails.php
+++ orderDetails.php
@@ -71,7 +71,44 @@ $order_id = $_GET['order_id'];
                     </div>
                 </div>
                 <br>
-               <?php }?>
+                <div class="card-footer">
+                    <div class="col-4" style="text-align:right; margin-left:60%">
+                        <div class="row p-2">
+                            <div class="col">Total:</div>
+                            <div class="col"> RM<?php echo $row2['amount']?>.00</div>
+                        </div>
+                        <div class="row p-2">
+                            <div class="col">Discounts:</div>
+                            <div class="col">-RM0.00</div>
+                        </div>
+                        <div class="row p-2">
+                            <div class="col">Delivery Fees:</div>
+                            <div class="col">
+                                    0.00
+                            </div>
+                        </div>
+                        <div class="row p-2">
+                            <div class="col">
+                                <h5>Order Total:</h5>
+                                
+                            </div>
+                            <div class="col red-text">
+                                <h5><strong>RM<?php echo $row2['amount']?>.00</strong></h5>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                        
+                       <?php if($row2['order_status'] !='Completed'){?>
+                        <a class="btn btn-primary " style="margin-left:10px;"  href="cancellation.php?order_id=<?php echo $row2['order_id'];?>">Cancel Order</a>
+                        <a class="btn btn-primary"style="margin-left:10px;" href="orderDetails.php?order_id=<?php echo $row2['order_id'];?>">Confirm Order</a>
+                            <?php } else{ ?>
+                        <a class= "btn btn-primary" href="getOrder.php">Back</a>
+                        <?php }?>
+                    </div>
+                    <?php }?>
+                </div>
                 <br>
             </div>
         </section>
