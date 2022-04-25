5ef75608d5c454179aab3b249b4e35d1b0a4d9bd
Joelteoh <teohlixuan@gmail.com>
2022-04-25 15:47:22 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -16,7 +16,7 @@
 <link rel="stylesheet" href="../css/cart.css">
 <!-- Begin Page Content -->
 <div class="container-fluid" style="width:80%">
-<div class="column"><button onclick="history.back()"><a class="btn btn-back" href="#"><i class="icon-arrow-left"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+<button onclick="history.back()"><div class="column"><a class="btn btn-back" href="#"><i class="icon-arrow-left"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
   <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
     </svg>  Back</a></div></button>
     <!-- Shopping Cart-->
