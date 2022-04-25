e67ca1d38af157164d23076008110bd4e4223e84
easonying0723 <ey73210723@gmail.com>
2022-04-25 18:48:25 +0800

    1

--- userprofile_voucher.php
+++ userprofile_voucher.php
@@ -20,42 +20,6 @@
 <link href="css/classic.css" rel="stylesheet">
 
 <br>
-
-<?php 
-  $userid = $_SESSION["userid"];
-
- $sql_voucherR =
- "SELECT 
- voucherRedemption.voucher_id,
- voucher.voucher_id,
- voucher.voucher_code,
- voucher.voucher_type,
- voucher.discount_amount,
- voucher.voucher_display,
- voucher.voucher_limit,
- voucher.voucher_startdate,
- voucher.voucher_expired,
- voucher.voucher_details,
- shopProfile.shop_name,
- shopProfile.shop_id,
- shopProfile.shop_profile_image
-
- FROM voucherRedemption
- JOIN voucher ON voucherRedemption.voucher_id = voucher.voucher_id
- JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id
- JOIN product ON productVoucher.product_id = product.product_id
- JOIN shopProfile ON product.shop_id = shopProfile.shop_id
- WHERE voucherRedemption.user_id = $userid
- GROUP BY voucher.voucher_id, shopProfile.shop_name, shopProfile.shop_profile_image, shopProfile.shop_id, voucherRedemption.voucher_id, voucherRedemption.user_id
- ";
-
-   //$stmt = $conn->prepare($sql_voucherR);
-   //$stmt->execute();
-   //$result = $stmt->get_result();
-
-   $result_upvoucher = mysqli_query($conn, $sql_voucherR);
-
-?>
    
 <div class="row">
    <?php require __DIR__ . '/userprofilenav.php' ?>
@@ -75,6 +39,35 @@
                                     </div>
                                     <hr>
                                     <?php
+                                      $userid = $_SESSION["userid"];
+
+                                      $sql_voucherR =
+                                      "SELECT 
+                                      voucherRedemption.voucher_id,
+                                      voucher.voucher_id,
+                                      voucher.voucher_code,
+                                      voucher.voucher_type,
+                                      voucher.discount_amount,
+                                      voucher.voucher_display,
+                                      voucher.voucher_limit,
+                                      voucher.voucher_startdate,
+                                      voucher.voucher_expired,
+                                      voucher.voucher_details,
+                                      shopProfile.shop_name,
+                                      shopProfile.shop_id,
+                                      shopProfile.shop_profile_image
+                                     
+                                      FROM voucherRedemption
+                                      JOIN voucher ON voucherRedemption.voucher_id = voucher.voucher_id
+                                      JOIN productVoucher ON voucher.voucher_id = productVoucher.voucher_id
+                                      JOIN product ON productVoucher.product_id = product.product_id
+                                      JOIN shopProfile ON product.shop_id = shopProfile.shop_id
+                                      WHERE voucherRedemption.user_id = $userid
+                                      GROUP BY voucher.voucher_id, shopProfile.shop_name, shopProfile.shop_profile_image, shopProfile.shop_id, voucherRedemption.voucher_id, voucherRedemption.user_id
+                                      ";
+                                     
+                                       $result_upvoucher = mysqli_query($conn, $sql_voucherR);
+
                                        while ($row = mysqli_fetch_assoc($result_upvoucher)) {
                                     ?>
                                     <div class="col-sm-12">
