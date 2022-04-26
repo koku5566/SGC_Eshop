97a13a97f8468f23df1a737ab96ec8f91c88ba7c
Marcheal <marchealyong@gmail.com>
2022-04-26 11:45:55 +0800

    Update sellerRating.php

--- seller/sellerRating.php
+++ seller/sellerRating.php
@@ -45,6 +45,17 @@
       </div>
 
       <div class="row reviewContent">
+      <?php
+          $sql_user = "SELECT username FROM user
+          INNER JOIN reviewRating
+          ON user.user_id = reviewRating.user_id";
+          $user_result = mysqli_query($conn, $sql_user);
+          
+          //if ($user_result->num_rows > 0) {
+          // output data of each row
+          while($row = $user_result->fetch_assoc()) {
+        ?>
+
         <?php
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
@@ -55,17 +66,6 @@
           if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-        ?>
-
-        <?php
-          //$sql_user = "SELECT username FROM user
-          //INNER JOIN reviewRating
-          //ON user.user_id = reviewRating.user_id";
-          //$user_result = mysqli_query($conn, $sql_user);
-          //
-          //if ($user_result->num_rows > 0) {
-          //  // output data of each row
-          //  while($row = $user_result->fetch_assoc()) {
         ?>
         <div class="col-lg-6 col-md-12">
           <p>
@@ -80,6 +80,9 @@
             echo "<br> 0 results";
           }
           $conn->close();
+        ?>
+        <?php
+          }
         ?>
       </div>
     </div>
