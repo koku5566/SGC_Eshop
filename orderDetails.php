970d0b565951072e0b8aec9fc2a96dd3b8b29a3a
Aislinn98 <100279067+Aislinn98@users.noreply.github.com>
2022-04-29 15:30:30 +0800

    Update orderDetails.php

--- orderDetails.php
+++ orderDetails.php
@@ -87,46 +87,7 @@ $order_id = $_GET['order_id'];
                     </div>
                 </div>
                 <br>
-                <div class="card-footer">
-                    <div class="col-4" style="text-align:right; margin-left:60%">
-                        <div class="row p-2">
-                            <div class="col">Total:</div>
-                            <div class="col"> 
-                            <?php  echo $amount * $row2['quantity']?>
-                            </div>
-                        </div>
-                        <div class="row p-2">
-                            <div class="col">Discounts:</div>
-                            <div class="col">-RM0.00</div>
-                        </div>
-                        <div class="row p-2">
-                            <div class="col">Delivery Fees:</div>
-                            <div class="col">
-                                   <?php echo $shippingfee?>
-                            </div>
-                        </div>
-                        <div class="row p-2">
-                            <div class="col">
-                                <h5>Order Total:</h5>
-                                
-                            </div>
-                            <div class="col red-text">
-                                <h5><strong>RM<?php  echo $amount * $row2['quantity'] + $shippingfee?></strong></h5>
-                            </div>
-                            
-                        </div>
-                        
-                    </div>
-                    <?php if($row2['order_status'] !='Completed' || $row2['cancellation_status'] =='rejected' || $row2['order_status'] =='Paid' ){?>
-                        <a class="btn btn-primary " style="margin-left:10px;"  href="cancellation.php?order_id=<?php echo $row2['order_id'];?>">Cancel Order</a>
-                        <?php }else{?>
-                        <a class= "btn btn-primary" href="getOrder.php">Back</a>
-                    <?php }?>
-                        
-                       
-                    </div>
-                    <?php }?>
-                </div>
+                
                 <br>
             </div>
         </section>
