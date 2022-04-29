c6f6f98ff63dda00f59e07eee70294273edd0b1c
chrischom02 <chrischom02@gmail.com>
2022-04-29 13:59:10 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -244,9 +244,7 @@ $shippingfee = 10;
                             </td>
                             
                                 <td>
-                                <div class='product-item'>
-                                <a href='#'><img src='/img/product/$product_image' alt='Product' style='display: table-cell !important;'></a>
-                                </div>
+                                <a href='#'><img src='/img/product/$product_image' alt='Product' style='display: table-cell !important; display: none !important;display: table-cell; vertical-align: top;width: 130px;padding-right: 20px; display: block; width: 100% display: none;'></a>
                                 </td>
                                 <td>
                                 <span>".$product_name."</span>
