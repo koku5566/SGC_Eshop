66a5dca04cf25b2e2e21c2c44867eb74402ddbb1
Joelteoh <99790374+Joelteoh@users.noreply.github.com>
2022-04-29 11:32:24 +0800

    Update cart_manage.php

--- cart_manage.php
+++ cart_manage.php
@@ -1,7 +1,7 @@
 <?php
     require __DIR__ . '/header.php';
-    require_once __DIR__ . '/mysqli_connect.php'
-    session_start();
+    include __DIR__ . '/mysqli_connect.php'
+    //session_start();
 
     //remove product from cart table
     if (isset($_POST['removeItemBtn'])) {
