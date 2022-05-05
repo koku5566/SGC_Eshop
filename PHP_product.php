5bf687acdba401d123b35cf98f1e57632db3a068
Joelteoh <99790374+Joelteoh@users.noreply.github.com>
2022-05-05 11:56:05 +0800

    Update PHP_product.php

--- PHP_product.php
+++ PHP_product.php
@@ -57,9 +57,9 @@
             
             if ($userId == "")
             {
-                echo "<script type="text/javascript">
-                    alert("Voucher claimed failed. Login to claimed voucher.")
-                    window.location.href = window.location.origin + "/login.php";
+                echo "<script type='text/javascript'>
+                    alert('Voucher claimed failed. Login to claimed voucher.')
+                    window.location.href = window.location.origin + '/login.php';
                 </script>"
             }
             
