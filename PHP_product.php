a12ea62bb6924be655cd44274b45bbcf4f3107b1
Joelteoh <99790374+Joelteoh@users.noreply.github.com>
2022-05-05 11:53:20 +0800

    Update PHP_product.php

--- PHP_product.php
+++ PHP_product.php
@@ -57,13 +57,10 @@
             
             if ($userId == "")
             {
-                ?>
-                    <script type="text/javascript">
-                        alert("Voucher claimed failed. Login to claimed voucher.")
-                        window.location.href = window.location.origin + "/login.php";
-                    </script>
-                <?php
-                exit;
+                echo "<script type="text/javascript">
+                    alert("Voucher claimed failed. Login to claimed voucher.")
+                    window.location.href = window.location.origin + "/login.php";
+                </script>"
             }
             
             //Joel you can use this 
